1d4c5a605ffac0d154dbe88f6cf428f00c8464e9
Ayumi Yu <ayumi@ayumiyu.com>
2016-10-14 14:31:28 -0700

    Filter params: add bitcoin_address, verification_token

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i(authentication_token password token)
+Rails.application.config.filter_parameters += %i(authentication_token bitcoin_address password token verification_token)
