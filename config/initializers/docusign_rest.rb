cdc801afdd9dde9b83836d726f4f3b8bb4080fac
Ayumi Yu <ayumi@ayumiyu.com>
2016-10-17 11:27:24 -0700

    Configurable docusign_api_endpoint and _version

--- config/initializers/docusign_rest.rb
+++ config/initializers/docusign_rest.rb
@@ -1,7 +1,15 @@
 DocusignRest.configure do |config|
+  # Discover endpoint with DocusignRest::Client #get_login_information
+  # Default: "https://demo.docusign.net/restapi"
+  if Rails.application.secrets[:docusign_api_endpoint]
+    config.endpoint = Rails.application.secrets[:docusign_api_endpoint]
+  end
+  # Default: "v2"
+  if Rails.application.secrets[:docusign_api_version]
+    config.api_version = Rails.application.secrets[:docusign_api_version]
+  end
+  # e.g. 12345678
   config.account_id = Rails.application.secrets[:docusign_api_account_id]
-  config.api_version = "v2"
-  # config.endpoint       = 'https://www.docusign.net/restapi'
   config.username = Rails.application.secrets[:docusign_api_username]
   config.password = Rails.application.secrets[:docusign_api_password]
   config.integrator_key = Rails.application.secrets[:docusign_integrator_key]
