1945d3898ab2abcf0cee5044eb553f526708195b
Ayumi Yu <ayumi@ayumiyu.com>
2016-10-25 14:42:10 -0700

    Add API_IP_WHITELIST

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,14 @@
+# To set authorization for the API, configure ENV["API_AUTH_TOKEN"] and
+# api_auth_token ENV["API_IP_WHITELIST"] (see secrets.yml)
 class Api::BaseController < ActionController::API
+  API_AUTH_TOKEN = Rails.application.secrets[:api_auth_token].freeze
+
+  if Rails.application.secrets[:api_ip_whitelist]
+    API_IP_WHITELIST = Rails.application.secrets[:api_ip_whitelist].split(",").map { |ip_cidr| IPAddr.new(ip_cidr) }.freeze
+  else
+    API_IP_WHITELIST = [].freeze
+  end
+
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   before_action :authenticate
@@ -7,16 +17,22 @@ class Api::BaseController < ActionController::API
 
   # before_action filter to protect API controller actions.
   def authenticate
-    authenticate_token || render_unauthorized
+    puts "request ip: #{request.remote_ip}"
+    (authenticate_ip && authenticate_token) || render_unauthorized
+  end
+
+  def authenticate_ip
+    return true if API_IP_WHITELIST.blank?
+    API_IP_WHITELIST.any? { |ip_addr| ip_addr.include?(request.remote_ip) }
   end
 
   def authenticate_token
-    return true if Rails.application.secrets[:api_auth_token].blank?
+    return true if API_AUTH_TOKEN.blank?
     authenticate_with_http_token do |token, _options|
       # Compare the tokens in a time-constant manner, to mitigate timing attacks.
       ActiveSupport::SecurityUtils.secure_compare(
         ::Digest::SHA256.hexdigest(token),
-        ::Digest::SHA256.hexdigest(Rails.application.secrets[:api_auth_token])
+        ::Digest::SHA256.hexdigest(API_AUTH_TOKEN)
       )
     end
   end
