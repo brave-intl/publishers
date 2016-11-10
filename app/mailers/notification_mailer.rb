ca6b7e6e45a97f21db99218ede28a370fd5c1166
Ayumi Yu <ayumi@ayumiyu.com>
2016-11-10 15:15:41 -0800

    Add publisher notification to retry legal form

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,6 +3,30 @@ class NotificationMailer < ApplicationMailer
   include PublishersHelper
   add_template_helper(PublishersHelper)
 
+  def publisher_form_retry(publisher, params = {})
+    @message = params[:message] if params.present?
+    @publisher = publisher
+    @private_reauth_url = publisher_private_reauth_url(@publisher)
+    mail(
+      to: @publisher.email,
+      subject: default_i18n_subject(brave_publisher_id: @publisher.brave_publisher_id)
+    )
+  end
+
+  # TODO: Refactor
+  def publisher_form_retry_internal(publisher, _params = {})
+    raise if !self.class.should_send_internal_emails?
+    @publisher = publisher
+    @private_reauth_url = "{redacted}"
+
+    mail(
+      to: INTERNAL_EMAIL,
+      reply_to: @publisher.email,
+      subject: "<Internal> #{I18n.t(:subject, brave_publisher_id: @publisher.brave_publisher_id, scope: %w(notification_mailer publisher_form_retry))}",
+      template_name: "publisher_form_retry"
+    )
+  end
+
   def publisher_payments_activated(publisher, _params = {})
     @publisher = publisher
     @bitcoin_address = @publisher.bitcoin_address
--- app/services/publisher_notifier.rb
+++ app/services/publisher_notifier.rb
@@ -3,7 +3,7 @@ class PublisherNotifier
   attr_reader :notification_params, :notification_type, :publisher
 
   # Should match methods in NotificationMailer starting with #publisher_*
-  NOTIFICATION_TYPES = %w(payments_activated)
+  NOTIFICATION_TYPES = %w(form_retry payments_activated)
 
   def initialize(notification_params: {}, notification_type:, publisher:)
     raise ":publisher invalid" if !publisher.is_a?(Publisher)
@@ -11,7 +11,7 @@ class PublisherNotifier
     if !NOTIFICATION_TYPES.include?(@notification_type)
       raise InvalidNotificationTypeError.new("#{notification_type} is an invalid notification_type")
     end
-    @notification_params = notification_params
+    @notification_params = notification_params.to_hash.symbolize_keys
     @publisher = publisher
   end
 
