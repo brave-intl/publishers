caf8cfc531076798d9a3a9fac3a2752ad1f2e8fa
Ayumi Yu <ayumi@ayumiyu.com>
2016-11-17 00:08:14 -0800

    Optional HTTP Basic Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  if Rails.application.secrets[:basic_auth_user] && Rails.application.secrets[:basic_auth_password]
+    http_basic_authenticate_with(
+      name: Rails.application.secrets[:basic_auth_user],
+      password: Rails.application.secrets[:basic_auth_password]
+    )
+  end
+
   protect_from_forgery with: :exception
 
   before_action :set_locale
