213527c868498cf5c98f41e5491b8355b44429ae
Ayumi Yu <ayumi@ayumiyu.com>
2016-11-17 11:37:12 -0800

    Update README with development guide

--- README.md
+++ README.md
@@ -1,9 +1,43 @@
-## Dependencies
+App for [publishers.brave.com](https://publishers.brave.com).
 
-- Ruby 2.3.1
-  - Try [rbenv](https://github.com/rbenv/rbenv)
-- Postgresql 9.5+
+## Development
 
-#### Test emails locally
+### Dependencies
 
-- [mailcatcher](https://github.com/sj26/mailcatcher) runs a local SMTP server and web UI. it's sweet
+1. Ruby 2.3.2. For a Ruby version manager try [rbenv](https://github.com/rbenv/rbenv).
+2. Postgresql 9.5+: `brew install postgresql`
+3. Redis: `brew install redis`
+4. (ruby) bundler: `gem install bundler`
+5. Install project dependencies: `bundle install --jobs=$(nproc)`
+  - Possible error: Nokogiri, with libxml2. Try installing a system libxml2 with `brew install libxml2` and then `bundle config build.nokogiri --use-system-libraries` then again `bundle install`.
+
+### Config
+
+Most configuration is set in [config/secrets.yml](https://github.com/brave/publishers/blob/master/config/secrets.yml) via environment variables.
+
+It might be useful to maintain a local bash script with a list of env vars. For an example see [config/secrets.yml](https://github.com/brave/publishers/blob/master/docs/publishers-secrets.example.sh).
+
+#### Automagic addon vars
+
+Some variables are set automagically with Heroku addons:
+
+- FIXIE_URL - Proxy provider. For outbound API requests.
+- MAILGUN_* - For sending emails.
+- NEW_RELIC_APP_NAME, NEW_RELIC_LICENSE_KEY - New Relic app monitoring.
+- REDIS_URL - For Sidekiq and rack-attack
+
+#### Other vars
+
+A few variables are not configured in secrets.yml:
+
+- GPG_PUBKEY - GPG pubkey for encrypting legal forms before uploading to S3.
+
+### Run app
+
+`foreman start`
+
+Starts web and workers. Visit http://localhost:5000 to check it out!
+
+### Testing emails locally
+
+- [mailcatcher](https://github.com/sj26/mailcatcher) runs a local SMTP server and web UI. `gem install mailcatcher` then run `mailcatcher`.
---
+++ docs/publishers-secrets.example.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# A list of env vars for running Publishers.
+# You may want to copy this file and fill in the blanks.
+# When running locally you can source your own file to populate env vars.
+
+#export API_AUTH_TOKEN="" # Enable local API token auth.
+#export API_IP_WHITELIST="1.2.3.4,5.6.7.8" # Enable local API whitelisting.
+#export API_EYESHADE_KEY=""
+#export API_EYESHADE_BASE_URI="" # e.g. https://eyeshade-server.example.com/ or run with API_EYESHADE_OFFLINE_1
+export API_EYESHADE_OFFLINE=1
+#export API_LEDGER_BASE_URI="" # e.g. https://ledger-server.example.com/ or run with API_LEDGER_OFFLINE=1
+export API_LEDGER_OFFLINE=1
+#export ATTR_ENCRYPTED_KEY="" # Encrypt sensitive things in the DB at rest.
+#export BASIC_AUTH_USER="" # Enable HTTP basic auth for the whole app.
+#export BASIC_AUTH_PASSWORD="" # see above
+export AWS_ACCESS_KEY_ID="" # For uploading signed LegalForm PDFs.
+export AWS_BUCKET="" # Target where signed LegalForm PDFs get uploaded.
+export AWS_REGION="" # e.g. us-west-2
+export AWS_SECRET_ACCESS_KEY="" # For uploading signed LegalForm PDFs.
+export DOCUSIGN_API_ACCOUNT_ID="" # For signing LegalForms
+export DOCUSIGN_API_USERNAME=""
+export DOCUSIGN_API_PASSWORD=""
+export DOCUSIGN_INTEGRATOR_KEY=""
+export DOCUSIGN_TEMPLATE_ID_IRS_W_8BEN=""
+export DOCUSIGN_TEMPLATE_ID_IRS_W_8BEN_E=""
+export DOCUSIGN_TEMPLATE_ID_IRS_W_9=""
+export GPG_PUBKEY=$(cat "~/publishers/publishers-public.asc") # Used to encrypt signed LegalForms when uploading to S3
+export INTERNAL_EMAIL="admin@publishers.local" # Admin notifications get sent here.
+#export MAILER_SENDER="" # The From: header in emails sent to users.
+export RECAPTCHA_PUBLIC_KEY="" # For recaptcha for rate limiting.
+export RECAPTCHA_PRIVATE_KEY=""
+#export SENTRY_DSN="" # Exception handling
+#export SIDEKIQ_USERNAME= # Sidekiq admin UI.
+#export SIDEKIQ_PASSWORD=
+export SLACK_WEBHOOK_URL="" # Admin notifications to your Slack.
+export SUPPORT_EMAIL="" # e.g. support@example.com
