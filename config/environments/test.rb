6984ffdf6e6265778a084b604f29159169a551dd
ａｙｕｍｉ  ｙｕ <ayumi@ayumiyu.com>
2017-03-02 00:23:46 +0000

    Test PublishersController: publisher registration
    Related: #3
    
    Auditors: @bsclifton

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: "www.example.com" }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
