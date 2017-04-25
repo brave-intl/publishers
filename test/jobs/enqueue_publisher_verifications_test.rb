4591b06207a56dd502dda229075a5fd2c03833dc
ａｙｕｍｉ  ｙｕ <ayumi@ayumiyu.com>
2017-04-25 23:35:41 +0000

    Stop domain verification checks after 6 weeks
    Fix #80
    
    Auditors: @bsclifton @mrose17

---
+++ test/jobs/enqueue_publisher_verifications_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class EnqueuePublisherVerificationsTest < ActiveJob::TestCase
+  test "#perform enqueue verifications" do
+    publisher = publishers(:default)
+    # Hack to clean DB
+    Publisher.where.not(id: publisher.id).destroy_all
+    assert_enqueued_with(job: VerifyPublisher, args: [{ brave_publisher_id: publisher.brave_publisher_id }]) do
+      EnqueuePublisherVerifications.perform_now
+    end
+  end
+
+  test "#perform doesn't enqueue verifications for old publishers" do
+    publisher = publishers(:stale)
+    # Hack to clean DB
+    Publisher.where.not(id: publisher.id).destroy_all
+    assert_no_enqueued_jobs do
+      EnqueuePublisherVerifications.perform_now
+    end
+  end
+end
