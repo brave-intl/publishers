5efecd7ab7d14840fa3077206d78c0a53080e039
Dan Gebhardt <dan@cerebris.com>
2017-08-17 16:11:32 -0400

    Remove unused BitcoinAddressValidator

--- app/validators/bitcoin_address_validator.rb
+++
@@ -1,9 +0,0 @@
-require "bitcoin"
-class BitcoinAddressValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    require "bitcoin"
-    if !Bitcoin.valid_address?(value)
-      record.errors[attribute] << (options[:message] || "is invalid")
-    end
-  end
-end
