4262e8297149fd15150bdd56e788b1219141203a
Larry Gebhardt <larry@cerebris.com>
2017-09-28 11:14:43 -0400

    Adds offline verification support for .well-known public file
    Separates the HTTP fetch logic form the PublisherHostInspector to its
    own module to share with the PublisherVerifier.

--- app/services/publisher_host_inspector.rb
+++ app/services/publisher_host_inspector.rb
@@ -1,9 +1,8 @@
-require 'net/http'
+require 'publishers/fetch'
 
 # Inspect a brave_publisher_id's host for web_host and HTTPS support
 class PublisherHostInspector
-  class RedirectError < StandardError; end
-  class ConnectionFailedError < StandardError; end
+  include Publishers::Fetch
 
   attr_reader :brave_publisher_id, :follow_local_redirects, :follow_all_redirects, :require_https, :check_web_host
 
@@ -19,45 +18,12 @@ class PublisherHostInspector
     @require_https = require_https
   end
 
-  # Fetch URI, following redirects per options
-  def fetch(uri, limit = 10)
-    raise RedirectError.new('too many HTTP redirects') if limit == 0
-
-    begin
-      response = Net::HTTP.get_response(uri)
-
-      case response
-        when Net::HTTPSuccess then
-          response
-        when Net::HTTPRedirection then
-          raise RedirectError.new('redirects prohibited') unless follow_all_redirects || follow_local_redirects
-
-          location = response['location']
-          new_uri = URI.parse(location)
-
-          # Tests if redirect is relative or if it's to a new host or page in the same domain
-          local_redirect = new_uri.relative? || new_uri.host.end_with?(uri.host)
-
-          if local_redirect && follow_local_redirects
-            new_uri = URI(uri + location)
-          elsif !follow_all_redirects
-            raise RedirectError.new('non local redirects prohibited')
-          end
-
-          fetch(new_uri, limit - 1)
-        else
-          response.value
-      end
-
-    rescue => e
-      raise ConnectionFailedError.new(e)
-    end
-  end
-
   # ToDo: github pages can be hosted at custom domains. We should detect them, if possible.
   # ToDo: perform better https test than just raising when the connection is refused?
   def inspect_uri(uri)
-    response = fetch(uri)
+    response = fetch(uri: uri,
+                     follow_all_redirects: follow_all_redirects,
+                     follow_local_redirects: follow_local_redirects)
 
     if check_web_host
       web_host = if brave_publisher_id.include?(".github.io")
@@ -70,7 +36,7 @@ class PublisherHostInspector
     end
 
     { response: response, web_host: web_host }
-  rescue RedirectError, ConnectionFailedError => e
+  rescue Publishers::Fetch::RedirectError, Publishers::Fetch::ConnectionFailedError => e
     { response: e }
   end
 
