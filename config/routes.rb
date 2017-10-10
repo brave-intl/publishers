e73e229c029017ca93d73529bd385e3dcb505d9f
Larry Gebhardt <larry@cerebris.com>
2017-10-10 12:25:07 -0400

    Remove verification_done route
    Replaced by dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
       get :email_verified
       get :verification_choose_method
       get :verification_dns_record
-      get :verification_done
       get :verification_public_file
       get :verification_github
       get :verification_wordpress
