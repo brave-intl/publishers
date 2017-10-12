7569ebc26065d736f66ae9a321be595f71f0ac4e
ａｙｕｍｉ  ｙｕ <ayumi@ayumiyu.com>
2017-10-12 19:56:32 +0000

    redirect all requests using TARGET_APP_DOMAIN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  if ENV["TARGET_APP_DOMAIN"]
+    TARGET_APP_DOMAIN = ENV["TARGET_APP_DOMAIN"].freeze
+    Rails.logger.info("Redirecting all requests to #{TARGET_APP_DOMAIN}")
+    before_action :ensure_domain
+    def ensure_domain
+      Rails.logger.info(request.env["HTTP_HOST"])
+      return true if request.env["HTTP_HOST"] == TARGET_APP_DOMAIN
+      redirect_to "https://#{TARGET_APP_DOMAIN}#{request.path}", status: :moved_permanently
+    end
+  end
+
   if Rails.application.secrets[:basic_auth_user] && Rails.application.secrets[:basic_auth_password]
     http_basic_authenticate_with(
       name: Rails.application.secrets[:basic_auth_user],
