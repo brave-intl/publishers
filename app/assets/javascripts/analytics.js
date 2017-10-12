6c907537ba0465e904371971e949d0523d52cef1
Alex Wykoff <alex@alexwykoff.com>
2017-10-12 11:08:33 -0500

    cleaned up analytics.js

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -2,7 +2,7 @@
 //data is sent to https://analytics.brave.com
 
 <!-- Piwik -->
-<script type="text/javascript">
+
   var _paq = _paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
@@ -14,17 +14,16 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
-</script>
+
 <!-- End Piwik Code -->
 
 
-document.addEventListener('click', function(e) {
+//trying to track if a user clicked on the verify button for a Wordpress flow
+$(document).addEventListener('click', function(e) {
     e = e || window.event;
     var target = e.target || e.srcElement,
         text = target.textContent || text.innerText;
     if (target.dataset.trackWordpress === 1){
        _paq.push(['trackEvent', 'WordpressVerificationClicked', 'Clicked', 'WordpressFlow'])
     }
-
-
 }, false);
