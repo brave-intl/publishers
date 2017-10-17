3ee0c9543f582794117b3ce98832da339285429d
ａｙｕｍｉ  ｙｕ <ayumi@ayumiyu.com>
2017-10-17 19:14:59 +0000

    Add Publisher sentry context to ErrorHandler

--- lib/error_handler.rb
+++ lib/error_handler.rb
@@ -19,9 +19,24 @@ module ErrorHandler
   def handle_standard_error(exception)
     if %w(production staging).include?(Rails.env)
       require "sentry-raven"
+      if publisher = introspect_publisher
+        Raven.user_context(
+          publisher_id: publisher.id,
+          brave_publisher_id: publisher.brave_publisher_id
+        )
+      end
       Raven.capture_exception(exception)
     else
       Rails.logger.warn(exception)
+    end
+  end
+
+  def introspect_publisher
+    if defined?(@publisher) && @publisher
+      return @publisher
+    end
+    if defined?(current_publisher) && current_publisher
+      return current_publisher
     end
   end
 end
