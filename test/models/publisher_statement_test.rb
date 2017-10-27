705ed873cb203032c1c2568b67d6707e0577e047
Dan Gebhardt <dan@cerebris.com>
2017-10-27 08:58:23 -0400

    Introduce PublisherStatement.expired scope

--- test/models/publisher_statement_test.rb
+++ test/models/publisher_statement_test.rb
@@ -14,4 +14,22 @@ class PublisherStatementTest < ActiveSupport::TestCase
     assert statement.expires_at > Time.zone.now
     refute statement.expired?
   end
+
+  test "`expired` scope includes all expired statements" do
+    publisher = publishers(:verified)
+    statement1 = PublisherStatement.new(publisher: publisher, period: 'past_7_days')
+    statement1.save
+    statement2 = PublisherStatement.new(publisher: publisher, period: 'past_7_days')
+    statement2.save
+    statement3 = PublisherStatement.new(publisher: publisher, period: 'past_7_days')
+    statement3.save
+
+    statement2.expires_at = 2.days.ago
+    statement2.save
+
+    statement3.expires_at = 1.minute.ago
+    statement3.save
+
+    assert_equal 2,PublisherStatement.expired.count
+  end
 end
