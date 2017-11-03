5c25cbeec3ae97e228d4ecaaed9f08425d4fdd28
Dan Gebhardt <dan@cerebris.com>
2017-11-03 14:59:52 -0400

    Add omniauth fields to publishers table

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,10 @@ ActiveRecord::Schema.define(version: 20171025114507) do
     t.boolean  "host_connection_verified"
     t.string   "detected_web_host"
     t.string   "default_currency"
+    t.string   "auth_provider"
+    t.string   "auth_user_id"
+    t.string   "auth_name"
+    t.string   "auth_email"
     t.index ["brave_publisher_id"], name: "index_publishers_on_brave_publisher_id", using: :btree
     t.index ["created_at"], name: "index_publishers_on_created_at", using: :btree
     t.index ["verification_token"], name: "index_publishers_on_verification_token", using: :btree
