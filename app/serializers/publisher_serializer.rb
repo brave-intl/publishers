471e26f710dd18b5a96b7ac233e3074058d79e7f
Kevin Pfefferle <kevin@pfefferle.co>
2017-11-07 13:40:36 -0500

    Add show_verification_status and verification_method to publishers API endpoint (#237)

--- app/serializers/publisher_serializer.rb
+++ app/serializers/publisher_serializer.rb
@@ -1,3 +1,3 @@
 class PublisherSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :phone, :phone_normalized, :verified, :verification_token, :created_at, :updated_at, :last_sign_in_at
+  attributes :id, :email, :name, :phone, :phone_normalized, :verified, :verification_method, :verification_token, :show_verification_status, :created_at, :updated_at, :last_sign_in_at
 end
