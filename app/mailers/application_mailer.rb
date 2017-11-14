15ac507f9e4014906c79fb5d13812ba4c51a834c
Larry Gebhardt <larry@cerebris.com>
2017-11-13 21:36:26 -0500

    Embed images in styles
    Compressed large images

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,18 +5,12 @@ class ApplicationMailer < ActionMailer::Base
   layout "mailer"
 
   before_action :require_premailer
-  before_action :add_logo
 
   def self.should_send_internal_emails?
     INTERNAL_EMAIL.present?
   end
 
   private
-
-  def add_logo
-    path = Rails.root.join("app/assets/images/brave_logo_horz_h30.png")
-    attachments.inline["brave_logo_horz_h30.png"] = File.read(path)
-  end
 
   def require_premailer
     require "premailer/rails"
