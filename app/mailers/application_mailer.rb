ca64bde6cb225deb023da879b53d45cedfb894a2
Larry Gebhardt <larry@cerebris.com>
2017-11-15 21:17:28 -0500

    Inline some images to stop gmail from eating the embedded images
    Renames images for prettier names as attachments

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,12 +5,26 @@ class ApplicationMailer < ActionMailer::Base
   layout "mailer"
 
   before_action :require_premailer
+  before_action :add_images
 
   def self.should_send_internal_emails?
     INTERNAL_EMAIL.present?
   end
 
   private
+
+  def add_image(image_file_name)
+    path = Rails.root.join("app/assets/images/mailer/#{image_file_name}")
+    attachments.inline[image_file_name] = File.read(path)
+  end
+
+  def add_images
+    add_image("brave-payments.png")
+    add_image("logo-medium.png")
+    add_image("logo-reddit.png")
+    add_image("logo-rocketchat.png")
+    add_image("logo-twitter.png")
+  end
 
   def require_premailer
     require "premailer/rails"
