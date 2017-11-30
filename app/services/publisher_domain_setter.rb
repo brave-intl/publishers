27ddfcaf38ebe3eb1579a6c53034a0d1f89255df
Dan Gebhardt <dan@cerebris.com>
2017-11-29 20:04:08 -0500

    Introduce PublisherDomainSetter service
    This service takes a publisher and a domain, normalizes the domain,
    inspects the host, and then sets `publisher.brave_publisher_id` on
    success.
    
    This processing was formerly performed in the Publisher model.
    Separating this out into a service allows it to happen synchronously
    or asynchronously (via a job).

---
+++ app/services/publisher_domain_setter.rb
@@ -0,0 +1,46 @@
+class PublisherDomainSetter < BaseService
+  attr_reader :publisher
+
+  def initialize(publisher:)
+    @publisher = publisher
+  end
+
+  def perform
+    normalize_domain
+    inspect_host
+  end
+
+  private
+
+  def normalize_domain
+    require "faraday"
+
+    @publisher.brave_publisher_id = nil
+    @publisher.brave_publisher_id_error_code = nil
+
+    @publisher.brave_publisher_id = PublisherDomainNormalizer.new(domain: @publisher.brave_publisher_id_unnormalized).perform
+    @publisher.brave_publisher_id_unnormalized = nil
+
+  rescue PublisherDomainNormalizer::DomainExclusionError
+    @publisher.brave_publisher_id_error_code = :exclusion_list_error
+  rescue Faraday::Error
+    @publisher.brave_publisher_id_error_code = :api_error_cant_normalize
+  rescue URI::InvalidURIError
+    @publisher.brave_publisher_id_error_code = :invalid_uri
+  end
+
+  def inspect_host
+    return unless @publisher.brave_publisher_id
+
+    result = PublisherHostInspector.new(brave_publisher_id: @publisher.brave_publisher_id).perform
+    if result[:host_connection_verified]
+      @publisher.supports_https = result[:https]
+      @publisher.detected_web_host = result[:web_host]
+      @publisher.host_connection_verified = true
+    else
+      @publisher.supports_https = false
+      @publisher.detected_web_host = nil
+      @publisher.host_connection_verified = false
+    end
+  end
+end
