1365d331a3c74e45035fb07edfd9ae7c67ff8e8c
Nick von Pentz <nickvonpentz@gmail.com>
2017-12-08 09:35:15 -0500

    Use sentry to log messages
    * Add sentry logs
    
    * Log `publisher.id` instead of `publisher.brave_publisher.id`

--- app/jobs/clean_stale_uphold_data_job.rb
+++ app/jobs/clean_stale_uphold_data_job.rb
@@ -2,6 +2,7 @@ class CleanStaleUpholdDataJob < ApplicationJob
   queue_as :scheduler
 
   def perform
+    require "sentry-raven"
     # clear uphold codes sitting for over 5 minutes   
     publishers = Publisher.has_stale_uphold_code
     n = 0
@@ -11,9 +12,11 @@ class CleanStaleUpholdDataJob < ApplicationJob
       publisher.uphold_updated_at = Time.now
       publisher.save!
       n += 1
-      Rails.logger.info("Cleaned stalled uphold code for #{publisher.brave_publisher_id}.")
+      Rails.logger.info("Cleaned stalled uphold code for #{publisher.id}.")
+      Raven.capture_message("Cleaned stalled uphold code for #{publisher.id}.")
     end
     Rails.logger.info("CleanStaleUpholdDataJob cleared #{n} stalled uphold codes.")
+    Raven.capture_message("CleanStaleUpholdDataJob cleared #{n} stalled uphold codes.")
 
     # clear uphold access params sitting for over 2 hours
     publishers = Publisher.has_stale_uphold_access_parameters
@@ -24,8 +27,10 @@ class CleanStaleUpholdDataJob < ApplicationJob
       publisher.uphold_updated_at = Time.now
       publisher.save!
       n += 1
-      Rails.logger.info("Cleaned stalled uphold access parameters for #{publisher.brave_publisher_id}.")
+      Rails.logger.info("Cleaned stalled uphold access parameters for #{publisher.id}.")
+      Raven.capture_message("Cleaned stalled uphold access parameters for #{publisher.id}.")
     end
     Rails.logger.info("CleanStaleUpholdDataJob cleared #{n} stalled uphold access parameters.")
+    Raven.capture_message("CleanStaleUpholdDataJob cleared #{n} stalled uphold access parameters.")
   end
 end
