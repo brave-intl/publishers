23fe266ac4e56e250733a872ca4d62968a4c6d07
Nick von Pentz <nickvonpentz@gmail.com>
2017-12-20 11:42:32 -0500

    Add sentry log when sending verification email to blank address
    * Raise if "To" address is blank in PublisherMailer#verify_email, send to sentry
    * Add corresponding test to PublisherMailerTest

--- test/mailers/publisher_mailer_test.rb
+++ test/mailers/publisher_mailer_test.rb
@@ -65,4 +65,25 @@ class PublisherMailerTest < ActionMailer::TestCase
     assert_equal ['brave-publishers@localhost.local'], email.from
     assert_equal [publisher.pending_email], email.to
   end
+
+  test "verify_email raises error if there is no send address" do
+    publisher = publishers(:default)
+    publisher.pending_email = ""
+    publisher.email = ""
+    publisher.save
+
+    # verify error raised if no pending email
+    assert_raises do
+      PublisherMailer.verify_email(publisher).deliver_now
+    end
+
+    publisher.pending_email = "alice@default.org"
+    publisher.email = "alice@default.org"
+    publisher.save
+    
+    # verify nothing raised if pending email exists
+    assert_nothing_raised do
+      PublisherMailer.verify_email(publisher).deliver_now
+    end
+  end
 end
