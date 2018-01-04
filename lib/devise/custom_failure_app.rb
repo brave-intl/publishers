bf33f92aee4948bcb072f6cd343ad326dc8622fc
Nick von Pentz <nickvonpentz@gmail.com>
2018-01-04 11:23:20 -0500

    Improve the CustomFailureApp
    * Ensure only unauthenticated requests are overridden
    
    * Ensure we only send attempts to claim expired tokens to #expired_auth_token not other requests
    
    * Add support for session timeouts; now redirected to login page
    
    * Add translation to en.yml for session timeout
    
    Refs #381

--- lib/devise/custom_failure_app.rb
+++ lib/devise/custom_failure_app.rb
@@ -1,11 +1,27 @@
-# Overrides the Devise default for attempts to sign in with expired token
+# Overrides the Devise defaults for unauthenticated requests
 class CustomFailureApp < Devise::FailureApp
   def redirect_url
-    flash[:alert] = nil
-    expired_auth_token_publishers_path(publisher_id: params[:id])
+    warden_message = warden_options[:message]
+    attempted_path = warden_options[:attempted_path]
+    unauthenticated = warden_options[:action] == "unauthenticated"
+    has_token_claim_params = params[:token].present? && params[:id].present?
+
+    if has_token_claim_params && unauthenticated
+      if attempted_path == publisher_path(token: params[:token])
+        # send publishers attempting to claim expired token to expired token page
+        flash[:alert] = nil
+        expired_auth_token_publishers_path(publisher_id: params[:id])
+      end
+    elsif warden_message == :timeout && unauthenticated
+      # send publishers whose session timed out to login page
+      flash[:alert] = I18n.t("publishers.login_session_expired")
+      new_auth_token_publishers_path
+    else
+      super
+    end
   end
 
-  # You need to override respond to eliminate recall
+  # Override respond to eliminate recall
   def respond
     if http_auth?
       http_auth
