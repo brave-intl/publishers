5a955ed51b8142223bb77c8fc6e45b9b8c1db577
Larry Gebhardt <larry@cerebris.com>
2018-01-05 07:03:36 -0500

    Remove ledger secrets. Ensure tests have api_eyeshade_key set.

--- config/environment.rb
+++ config/environment.rb
@@ -9,9 +9,3 @@ if Rails.application.secrets[:api_eyeshade_base_uri].present?
 else
   ENV["API_EYESHADE_OFFLINE"] = "1"
 end
-
-if Rails.application.secrets[:api_ledger_base_uri].present?
-  puts "Ledger API: #{ENV['API_LEDGER_BASE_URI']}"
-else
-  ENV["API_LEDGER_OFFLINE"] = "1"
-end
--- docs/publishers-secrets.example.sh
+++ docs/publishers-secrets.example.sh
@@ -8,8 +8,7 @@
 #export API_IP_WHITELIST="1.2.3.4,5.6.7.8" # Enable local API whitelisting.
 #export API_EYESHADE_KEY="" # e.g. 00000000-0000-4000-0000-000000000000
 #export API_EYESHADE_BASE_URI="" # e.g. http://127.0.0.1:3002
-#export API_LEDGER_BASE_URI="" # e.g. https://ledger-server.example.com/ or run with API_LEDGER_OFFLINE=1
-export API_LEDGER_OFFLINE=1
+export API_EYESHADE_OFFLINE=1
 #export ATTR_ENCRYPTED_KEY="" # Encrypt sensitive things in the DB at rest.
 #export BASIC_AUTH_USER="" # Enable HTTP basic auth for the whole app.
 #export BASIC_AUTH_PASSWORD="" # see above
