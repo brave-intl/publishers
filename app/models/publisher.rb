81e47678e139db8f861ff7c8fc4a44c3bd51a9f7
Nick von Pentz <nickvonpentz@gmail.com>
2018-01-11 09:51:25 -0500

    Move lookup logic from Publisher model to PublisherUnverifiedCalculator service class
    * Remove Publisher#win_back_publishers, WIN_BACK_THRESHOLD constant
    
    * Remove tests from PublisherTest
    
    * Add PublisherUnverifiedCalculator service class, WIN_BACK_THRESHOLD constant
    
    * Add PublisherUnverifiedCalculatorTest
    
    * Fix 'publihser' spelling error
    
    * Additionally report total publishers in print_win_back_publishers rake task
    
    Refs #454

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -3,8 +3,7 @@ class Publisher < ApplicationRecord
 
   UPHOLD_CODE_TIMEOUT = 5.minutes
   UPHOLD_ACCESS_PARAMS_TIMEOUT = 2.hours
-  WIN_BACK_THRESHOLD = 5.days
-
+  
   has_many :statements, -> { order('created_at DESC') }, class_name: 'PublisherStatement'
   has_many :u2f_registrations, -> { order("created_at DESC") }
   has_one :totp_registration
@@ -238,36 +237,6 @@ class Publisher < ApplicationRecord
 
   def dont_destroy_verified_publishers
     throw :abort if verified?
-  end
-
-  # Returns publishers who have abandoned verification process and do not have another publisher account
-  def self.win_back_publishers
-    verified_publishers = Publisher.where(verified: true)
-
-    # Only include publishers who have filled contact information
-    unverified_publishers = Publisher.where(verified: false).where.not(brave_publisher_id: [nil]).where("created_at < ?", WIN_BACK_THRESHOLD.ago)
-
-    win_back_publihsers = []
-    unverified_publishers.find_each do |publisher|
-      include_publisher = true
-
-      # Check if a verified publisher exists with same brave_publisher_id, email, or phone number
-      unless publisher.brave_publisher_id.blank?
-        include_publisher = verified_publishers.exists?(brave_publisher_id: publisher.brave_publisher_id) ? false : include_publisher
-      end
-
-      include_publisher = verified_publishers.exists?(email: publisher.email) ? false : include_publisher
-
-      unless publisher.phone_normalized.blank?
-        include_publisher = verified_publishers.exists?(phone_normalized: publisher.phone_normalized) ? false : include_publisher
-      end
-
-      if include_publisher
-        win_back_publihsers.push(publisher)
-      end
-    end
-
-    win_back_publihsers
   end
 
   class << self
--- lib/tasks/publisher_inventory.rake
+++ lib/tasks/publisher_inventory.rake
@@ -1,13 +1,15 @@
 namespace :publisher_inventory do
   task :print_win_back_publishers => :environment do
+    total_publishers = Publisher.count
     total_verified_publishers = Publisher.where(verified: true).count
     total_unverified_publishers = Publisher.where(verified: false).count
-    total_win_back_publishers = Publisher.get_win_back_publishers.count
+    total_win_back_publishers = PublisherUnverifiedCalculator.new.perform.count
 
-    puts "---Publisher Inventory----"
+    puts "\n------Publisher Inventory------"
+    puts "Publishers:            #{total_publishers}"
     puts "Verified Publishers:   #{total_verified_publishers}"
     puts "Unverified Publishers: #{total_unverified_publishers}"
     puts "Win Back Publishers:   #{total_win_back_publishers}"
-    puts "--------------------------"
+    puts "-------------------------------\n"
   end
 end
