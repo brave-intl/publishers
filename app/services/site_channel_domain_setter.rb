9a86f7747d0ad9f2f1e3e3a26ef8f7eb97bb8397
Dan Gebhardt <dan@cerebris.com>
2018-01-26 16:43:36 -0500

    Change SiteChannelDomainSetter to accept channel_details
    This service only works with the SiteChannelDetails model, so there is no need for it to be aware of the general Channel model associated with it.

--- app/services/site_channel_domain_setter.rb
+++ app/services/site_channel_domain_setter.rb
@@ -1,13 +1,13 @@
 class SiteChannelDomainSetter < BaseService
-  attr_reader :channel
+  attr_reader :channel_details
 
-  def initialize(channel:)
-    @channel = channel
+  def initialize(channel_details:)
+    @channel_details = channel_details
   end
 
   def perform
-    normalize_domain if channel.details.brave_publisher_id_unnormalized
-    inspect_host unless channel.details.brave_publisher_id_error_code
+    normalize_domain if channel_details.brave_publisher_id_unnormalized
+    inspect_host unless channel_details.brave_publisher_id_error_code
   end
 
   private
@@ -15,36 +15,37 @@ class SiteChannelDomainSetter < BaseService
   def normalize_domain
     require "faraday"
 
-    brave_publisher_id = SiteChannelDomainNormalizer.new(domain: channel.details.brave_publisher_id_unnormalized).perform
+    brave_publisher_id = SiteChannelDomainNormalizer.new(domain: channel_details.brave_publisher_id_unnormalized).perform
 
     if SiteChannelDetails.joins(:channel).where(brave_publisher_id: brave_publisher_id, "channels.verified": true).any?
-      channel.details.brave_publisher_id_error_code = :taken
+      channel_details.brave_publisher_id = brave_publisher_id
+      channel_details.brave_publisher_id_error_code = :taken
     else
-      channel.details.brave_publisher_id = brave_publisher_id
-      channel.details.brave_publisher_id_error_code = nil
-      channel.details.brave_publisher_id_unnormalized = nil
+      channel_details.brave_publisher_id = brave_publisher_id
+      channel_details.brave_publisher_id_error_code = nil
+      channel_details.brave_publisher_id_unnormalized = nil
     end
 
   rescue SiteChannelDomainNormalizer::DomainExclusionError
-    channel.details.brave_publisher_id_error_code = :exclusion_list_error
+    channel_details.brave_publisher_id_error_code = :exclusion_list_error
   rescue Faraday::Error
-    channel.details.brave_publisher_id_error_code = :api_error_cant_normalize
+    channel_details.brave_publisher_id_error_code = :api_error_cant_normalize
   rescue URI::InvalidURIError
-    channel.details.brave_publisher_id_error_code = :invalid_uri
+    channel_details.brave_publisher_id_error_code = :invalid_uri
   end
 
   def inspect_host
-    return unless channel.details.brave_publisher_id
+    return unless channel_details.brave_publisher_id
 
-    result = SiteChannelHostInspector.new(brave_publisher_id: channel.details.brave_publisher_id).perform
+    result = SiteChannelHostInspector.new(brave_publisher_id: channel_details.brave_publisher_id).perform
     if result[:host_connection_verified]
-      channel.details.supports_https = result[:https]
-      channel.details.detected_web_host = result[:web_host]
-      channel.details.host_connection_verified = true
+      channel_details.supports_https = result[:https]
+      channel_details.detected_web_host = result[:web_host]
+      channel_details.host_connection_verified = true
     else
-      channel.details.supports_https = false
-      channel.details.detected_web_host = nil
-      channel.details.host_connection_verified = false
+      channel_details.supports_https = false
+      channel_details.detected_web_host = nil
+      channel_details.host_connection_verified = false
     end
   end
 end
