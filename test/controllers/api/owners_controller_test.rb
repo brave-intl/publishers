e6fdd5d8cf01ea82be7df249d1a282a3a4f7aa41
Larry Gebhardt <larry@cerebris.com>
2018-01-30 17:12:32 -0500

    move serialization test functionality

--- test/controllers/api/owners_controller_test.rb
+++ test/controllers/api/owners_controller_test.rb
@@ -9,15 +9,6 @@ class Api::OwnersControllerTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
 
     response_json = JSON.parse(response.body)
-    assert response_json[1].has_key?("owner_identifier")
-    assert response_json[1].has_key?("email")
-    assert response_json[1].has_key?("name")
-    assert response_json[1].has_key?("phone_normalized")
-    assert response_json[1].has_key?("channel_identifiers")
-    assert response_json[1].has_key?("show_verification_status")
-    assert response_json[1]["channel_identifiers"].is_a?(Array)
-
-    refute response_json[4].has_key?("channel_identifiers")
 
     assert_match /#{owner.owner_identifier}/, response.body
     assert_match /#{owner.channels.verified.first.details.channel_identifier}/, response.body
