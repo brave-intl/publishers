03e870c61b074c9dba3008a5a2a2c773ca97a1ba
Larry Gebhardt <larry@cerebris.com>
2018-01-30 17:11:04 -0500

    redirect back to dashboard if channel does not exist

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -31,10 +31,6 @@ class ChannelsController < ApplicationController
   def setup_current_channel
     @current_channel = current_publisher.channels.find(params[:id])
   rescue ActiveRecord::RecordNotFound => e
-    respond_to do |format|
-      format.json {
-        render status: 404
-      }
-    end
+    redirect_to home_publishers_path, notice: t("channel.channel_not_found")
   end
 end
