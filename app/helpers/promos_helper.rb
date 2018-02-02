d4ee13b34502759bb3eb051a0f096bcdf99e3276
Nick von Pentz <nickvonpentz@gmail.com>
2018-02-02 08:48:31 -0500

    Display total_number of possible referrals on the dashboard
    * Add :promo_end_date config var
    
    * Add :promo_bonus_mulitplier config var
    
    * Add promo_ends_in, total_possible_referrals, and referral_bonus methods to PromosHelper
    
    * Use these methods in home.html.slim to display promo statistics info
    
    * Create PromosHelperTest
    
    * Add tests to verify correctness of total_possible_referral method that parses promo_stats_2018q1 to PromosHelperTest

--- app/helpers/promos_helper.rb
+++ app/helpers/promos_helper.rb
@@ -23,4 +23,25 @@ module PromosHelper
   def generate_referral_link(referral_code)
     "#{I18n.t("promo.shared.base_referral_link")}/#{referral_code.downcase}"
   end
+
+  def promo_ends_in
+    Rails.application.secrets[:promo_end_date].present? ? (Rails.application.secrets[:promo_end_date].to_datetime - DateTime.now).to_i : "∞"
+  end
+
+  def total_possible_referrals(publisher)
+    stats = publisher.promo_stats_2018q1
+    if publisher.promo_stats_2018q1 == "{}"
+      return 0
+    else
+      downloads_for_time_periods = stats["series"]["values"]  
+      total_downloads = downloads_for_time_periods.inject(0) {|sum, downloads_for_period| sum + downloads_for_period}
+      return total_downloads
+    end
+  end
+
+  def referral_bonus(publisher)
+    total_possible_referrals = total_possible_referrals(publisher)
+    promo_bonus_multiplier = Rails.application.secrets[:promo_bonus_multiplier].to_f
+    bonus = total_possible_referrals * promo_bonus_multiplier
+  end
 end
