d498539c736b0f8b26c97d4728ea9907d42eab69
Nick von Pentz <nickvonpentz@gmail.com>
2018-02-13 13:53:03 -0500

    Add tests ensuring deleting a channel works if it has a promo registration

--- test/jobs/delete_publisher_channel_job_test.rb
+++ test/jobs/delete_publisher_channel_job_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 require "webmock/minitest"
 
-class DeletePublisherChannelJobTest < ActiveJob::TestCase
+class DeletePublisherChannelJobTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "deletes channel for publisher" do
     prev_api_eyeshade_offline = Rails.application.secrets[:api_eyeshade_offline]
     begin
@@ -18,6 +20,21 @@ class DeletePublisherChannelJobTest < ActiveJob::TestCase
       DeletePublisherChannelJob.perform_now(publisher_id: publisher.id, channel_identifier: channel_identifier, update_promo_server: false, referral_code: nil)
     ensure
       Rails.application.secrets[:api_eyeshade_offline] = prev_api_eyeshade_offline
+    end
+  end
+
+  test "does not throw error if channel has a promo_regsitration" do
+    publisher = publishers(:completed)
+    sign_in publisher
+
+    # enable the promo
+    post promo_registrations_path
+    channel_identifier = publisher.channels.first.details.channel_identifier
+
+    assert_not_nil publisher.channels.first.promo_registration.referral_code
+
+    assert_nothing_raised do
+      DeletePublisherChannelJob.perform_now(publisher_id: publisher.id, channel_identifier: channel_identifier, update_promo_server: true, referral_code: nil)
     end
   end
 end
