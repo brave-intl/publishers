5d6711cc93b73aa846b713e9b2d9446d749125a8
Larry Gebhardt <larry@cerebris.com>
2018-02-14 12:18:34 -0500

    Adds `default_currency` and `uphold_verified` to owners serializer

--- test/serializers/publisher_serializer_test.rb
+++ test/serializers/publisher_serializer_test.rb
@@ -2,7 +2,9 @@ require "test_helper"
 
 class PublisherSerializerTest < ActiveSupport::TestCase
   test "owners are serialized as JSON and include owner and channel details rolled up" do
-    owner = publishers(:verified)
+    owner = publishers(:small_media_group)
+    owner.uphold_verified = true
+    owner.save!
 
     result = PublisherSerializer.new(owner).to_json
     json_result = JSON.parse(result)
@@ -12,6 +14,8 @@ class PublisherSerializerTest < ActiveSupport::TestCase
     assert json_result.has_key?("name")
     assert json_result.has_key?("phone_normalized")
     assert json_result.has_key?("channel_identifiers")
+    assert json_result.has_key?("default_currency")
+    assert json_result.has_key?("uphold_verified")
     assert json_result.has_key?("show_verification_status")
     assert json_result["channel_identifiers"].is_a?(Array)
 
