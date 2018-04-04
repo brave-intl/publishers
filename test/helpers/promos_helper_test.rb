3e240110ae1ef785401b9155c93847c59ba59136
Nick von Pentz <nickvonpentz@gmail.com>
2018-04-04 16:26:10 -0700

    Update PromosHelper
    * Replace #total_possible_referrals with #total_referral_downloads
    
    * Add #qualified_referral_downloads
    
    * Update offline_promo_stats to match the new stats format
    
    * Update PromosHelperTest

--- test/helpers/promos_helper_test.rb
+++ test/helpers/promos_helper_test.rb
@@ -1,27 +1,14 @@
 require 'test_helper'
-
 class PromosHelperTest < ActionView::TestCase
-  test "total_possible_referrals returns 0 for promo disabled publisher" do
+  test "returns correct number of promo stats " do
     publisher = publishers(:completed)
-    total_possible_referrals = total_possible_referrals(publisher)
-    assert_equal total_possible_referrals, 0
-  end
+    publisher.promo_stats_2018q1 = offline_promo_stats
+    publisher.save!
 
-  test "total_possible_referrals returns correct number of downloads for only one time period" do
-    publisher = publishers(:completed)
-    publisher.promo_stats_2018q1 = {"times"=>[Time.now.to_s], "series"=>{"name"=>"downloads", "values"=>[100]}}
-    publisher.save
-
-    total_possible_referrals = total_possible_referrals(publisher)
-    assert_equal total_possible_referrals, 100
-  end
-
-  test "total_possible_referrals returns correct number of downloads for many time periods" do
-    publisher = publishers(:completed)
-    publisher.promo_stats_2018q1 = {"times"=>[Time.now.to_s], "series"=>{"name"=>"downloads", "values"=>[100, 10, 0, 20]}}
-    publisher.save
-
-    total_possible_referrals = total_possible_referrals(publisher)
-    assert_equal total_possible_referrals, 130
+    total_referral_downloads = total_referral_downloads(publisher)
+    assert_equal total_referral_downloads, 200
+    
+    qualified_referral_downloads = qualified_referral_downloads(publisher)
+    assert_equal qualified_referral_downloads, 30
   end
 end
