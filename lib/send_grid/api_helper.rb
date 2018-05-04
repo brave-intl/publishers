fa97636ed03989aa3523f456e2bae2c6f538f086
Larry Gebhardt <larry@cerebris.com>
2018-05-04 09:56:54 -0400

    Add sendgrid:init and sendgrid:info rake tasks

--- lib/send_grid/api_helper.rb
+++ lib/send_grid/api_helper.rb
@@ -13,7 +13,7 @@ module SendGrid
       def find_contact_by_email(email:)
         params = { "email": email }
         result = sg.client.contactdb.recipients.search.get(query_params: params)
-        check_result(result: result, sucess: '200')
+        check_result(result: result, success: '200')
 
         json_result = JSON.parse(result.body)
 
@@ -26,7 +26,7 @@ module SendGrid
 
       def get_contact_by_email(email:)
         result = sg.client.contactdb.recipients._(id_from_email(email)).get
-        check_result(result: result, sucess: '200')
+        check_result(result: result, success: '200')
         JSON.parse(result.body)
       end
 
@@ -38,7 +38,7 @@ module SendGrid
         end
 
         result = sg.client.contactdb.recipients.patch(request_body: body)
-        check_result(result: result, sucess: '201')
+        check_result(result: result, success: '201')
 
         result_json = JSON.parse(result.body)
         if result_json['error_count'] > 0
@@ -56,18 +56,18 @@ module SendGrid
 
       def add_contact_to_list(list_id:, contact_id:)
         result = sg.client.contactdb.lists._(list_id).recipients._(contact_id).post()
-        check_result(result: result, sucess: '201')
+        check_result(result: result, success: '201')
       end
 
       def add_contacts_to_list(list_id:, contact_ids:)
         result = sg.client.contactdb.lists._(list_id).recipients.post(request_body: contact_ids)
-        check_result(result: result, sucess: '201')
+        check_result(result: result, success: '201')
       end
 
       def remove_contact_from_list(list_id:, contact_id:)
         params = {"recipient_id": contact_id, "list_id": list_id}
         result = sg.client.contactdb.lists._(list_id).recipients._(contact_id).delete(query_params: params)
-        check_result(result: result, sucess: '204')
+        check_result(result: result, success: '204')
       end
 
       def add_contact_by_email_to_list(list_id:, email:)
@@ -85,15 +85,61 @@ module SendGrid
         params
       end
 
+      def get_lists
+        result = sg.client.contactdb.lists.get
+        check_result(result: result, success: '200')
+        JSON.parse(result.body)['lists']
+      end
+
+      def get_list(list_name:)
+        lists = get_lists
+        lists.each do |list|
+          if list['name'].downcase == list_name.downcase
+            return list
+          end
+        end
+        return nil
+      end
+
+      def create_list(name:)
+        data = { 'name': name }
+        result = sg.client.contactdb.lists.post(request_body: data)
+        check_result(result: result, success: '201')
+        JSON.parse(result.body)
+      end
+
+      def get_custom_fields
+        result = sg.client.contactdb.custom_fields.get()
+        check_result(result: result, success: '200')
+        JSON.parse(result.body)['custom_fields']
+      end
+
+      def get_custom_field(field_name:)
+        fields = get_custom_fields
+        fields.each do |field|
+          if field['name'].downcase == field_name.downcase
+            return field
+          end
+        end
+        return nil
+      end
+
+      def create_custom_field(field_name:, type:)
+        data = { 'name': field_name, 'type': type }
+        result = sg.client.contactdb.custom_fields.post(request_body: data)
+        check_result(result: result, success: '201')
+        JSON.parse(result.body)
+      end
+
       private
 
       def id_from_email(email)
         Base64.strict_encode64(email.downcase)
       end
 
-      def check_result(result:, sucess:)
+      def check_result(result:, success:)
         case result.status_code
-        when sucess
+        when success
           true
         when '400'
           raise Error.new(result.body)
@@ -109,7 +155,7 @@ module SendGrid
       end
 
       def sg
-        SendGrid::API.new(api_key: Rails.application.secrets[:sendgrid_api_key])
+        @sg ||= SendGrid::API.new(api_key: Rails.application.secrets[:sendgrid_api_key])
       end
     end
   end
