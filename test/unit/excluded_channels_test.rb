33c1c163ce9c36986f13aa6ce0bf74d2aa33db1c
Albert Wang <amwang217@gmail.com>
2018-05-22 12:14:35 -0700

    Update tests to include #excluded?

--- test/unit/excluded_channels_test.rb
+++ test/unit/excluded_channels_test.rb
@@ -9,4 +9,14 @@ class ExcludedChannelsTest < ActiveSupport::TestCase
   test "excluded_brave_publisher_id? for unexcluded" do
     refute Publishers::ExcludedChannels.excluded_brave_publisher_id?("tomatoland.org")
   end
+
+  test "excluded? for excluded site Channel" do
+    c = channels(:to_verify_restricted)
+    assert Publishers::ExcludedChannels.excluded?(c)
+  end
+
+  test "restricted? for unrestricted site Channel" do
+    c = channels(:to_verify_dns)
+    refute Publishers::ExcludedChannels.excluded?(c)
+  end
 end
