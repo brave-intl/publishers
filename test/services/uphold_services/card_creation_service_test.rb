78fe185002f742ff6a2b05353e0a1a51ccf48529
Nick von Pentz <nickvonpentz@gmail.com>
2018-06-05 17:15:15 -0400

    Add UpholdServices::CardCreationService and tests

---
+++ test/services/uphold_services/card_creation_service_test.rb
@@ -0,0 +1,49 @@
+require "test_helper"
+require "webmock/minitest"
+
+class CardCreationServiceTest < ActiveJob::TestCase
+  test "when offline returns true" do
+    prev_offline = Rails.application.secrets[:api_eyeshade_offline]
+    begin
+      Rails.application.secrets[:api_eyeshade_offline] = true
+
+      publisher = publishers(:verified)
+      result = UpholdServices::CardCreationService.new(publisher: publisher, currency_code: "BAT").perform
+
+      assert_equal true, result
+
+    ensure
+      Rails.application.secrets[:api_eyeshade_offline] = prev_offline
+    end
+  end
+
+  test "request has correct format" do
+    prev_offline = Rails.application.secrets[:api_eyeshade_offline]
+    begin
+      Rails.application.secrets[:api_eyeshade_offline] = true
+      publisher = publishers(:verified)
+
+      stub_request(:post, /v3\/owners\/#{URI.escape(publisher.owner_identifier)}\/wallet\/card/).
+          with(headers: {'Accept'=>'*/*',
+                         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                         'Authorization'=>"Bearer #{Rails.application.secrets[:api_eyeshade_key]}",
+                         'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.2'},
+               body:
+                 <<~BODY
+                 {
+                   "currency": "BAT",
+                   "label": "BAT"
+                 }
+                 BODY
+          ).
+          to_return(status: 200, headers: {})
+
+      result = UpholdServices::CardCreationService.new(publisher: publisher, currency_code: "BAT").perform
+
+      assert_equal true, result
+
+    ensure
+      Rails.application.secrets[:api_eyeshade_offline] = prev_offline
+    end
+  end
+end
