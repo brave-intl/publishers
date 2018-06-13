3029f5246ae2465c2646c553fc297be0df03c57a
Dan Gebhardt <dan@cerebris.com>
2018-06-13 08:23:01 -0400

    Introduce wait_until capybara helper
    This helper is not typically needed for feature tests, but is useful for waiting on a general Ruby conditional.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -71,6 +71,14 @@ module Capybara
       def js_logs
         page.driver.browser.manage.logs.get(:browser)
       end
+
+      def wait_until
+        require "timeout"
+        Timeout.timeout(Capybara.default_max_wait_time) do
+          sleep(0.1) until value = yield
+          value
+        end
+      end
     end
   end
 end
