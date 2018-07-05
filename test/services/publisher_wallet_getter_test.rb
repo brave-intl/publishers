7d9d3960026ca45cdb440abc359d9d89c7bc6397
Nick von Pentz <nickvonpentz@gmail.com>
2018-07-05 08:09:05 -0600

    Only request individual balances for verified channels

--- test/services/publisher_wallet_getter_test.rb
+++ test/services/publisher_wallet_getter_test.rb
@@ -95,4 +95,49 @@ class PublisherWalletGetterTest < ActiveJob::TestCase
     )
   end
 
+  test "when online only returns channel balances for verified channels" do
+    Rails.application.secrets[:api_eyeshade_offline] = false
+
+    # Has one verified, and one unverified channel
+    publisher = publishers(:partially_completed)
+
+    wallet = {
+      "wallet": {
+        "provider": "uphold",
+        "authorized": true,
+        "defaultCurrency": "USD",
+        "availableCurrencies": [ "EUR", "BTC", "ETH" ],
+        "possibleCurrencies": [ "USD", "EUR", "BTC", "ETH", "BAT" ],
+        "scope": ["cards:write"]
+      }
+    }.to_json
+
+    stub_request(:get, %r{v1/owners/#{URI.escape(publisher.owner_identifier)}/wallet}).
+      with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.9.2'}).
+      to_return(status: 200, body: wallet, headers: {})
+
+    publisher.channels.each do |channel|
+      balance_json = {
+        "amount" => "5.80",
+        "currency" => "USD",
+        "altcurrency" => "BAT",
+        "probi" => "25000000000000000000",
+        "rates" => {
+          "BTC" => 0.00005418424016883016,
+          "ETH" => 0.000795331082073117,
+          "USD" => 0.2363863335301452,
+          "EUR" => 0.20187818378874756,
+          "GBP" => 0.1799810085548496
+        }
+      }.to_json
+
+      stub_request(:get, %r{v2/publishers/#{URI.escape(channel.details.channel_identifier)}/balance}).
+          with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.9.2'}).
+          to_return(status: 200, body: balance_json, headers: {})
+    end
+    result = PublisherWalletGetter.new(publisher: publisher).perform
+
+    # Ensure the wallet getter only returns channel balance for the verified channel
+    assert result.channel_balances.count == 1
+  end
 end
