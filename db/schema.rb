ff96de74ed865c8011ce85ea57289ce34c04cde9
Albert Wang <amwang217@gmail.com>
2018-07-22 12:30:46 -0700

    Migration to hide publisher statements as part of the graph on the dashboard
    Part of #542

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628164707) do
+ActiveRecord::Schema.define(version: 20180722192428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -185,6 +185,7 @@ ActiveRecord::Schema.define(version: 20180628164707) do
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.boolean  "created_by_admin",      default: false
+    t.boolean  "hidden",                default: false
     t.index ["publisher_id"], name: "index_publisher_statements_on_publisher_id", using: :btree
   end
 
