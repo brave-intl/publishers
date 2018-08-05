f1527ed761e0cd9d4bc86f6e6d96700be06f1598
Albert Wang <amwang217@gmail.com>
2018-08-04 21:08:45 -0700

    Broken commit with 0.20.3

--- app/javascript/packs/brave_rewards_banner_display.jsx
+++ app/javascript/packs/brave_rewards_banner_display.jsx
@@ -5,7 +5,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import SiteBanner from 'brave-ui/rewards/siteBanner'
+import SiteBanner from 'brave-ui'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
@@ -13,6 +13,24 @@ document.addEventListener('DOMContentLoaded', () => {
       bgImage={"https://www.popsci.com/sites/popsci.com/files/styles/655_1x_/public/images/2017/07/mars-surface.jpg?itok=wZc9vU-e&fc=50,50"}
       logo={"https://pbs.twimg.com/profile_images/920841899992236032/mSDhBoC9_400x400.jpg"}
       title={"Alexis Ren"}
+      currentDonation={"5"}
+      donationAmounts={[
+        {
+          "tokens": 1,
+          "converted": 0.3,
+          "selected": false
+        },
+        {
+          "tokens": 5,
+          "converted": 1.5,
+          "selected": false
+        },
+        {
+          "tokens": 10,
+          "converted": 3,
+          "selected": false
+        }
+      ]}
     />,
     document.body.appendChild(document.createElement('div')),
   )
