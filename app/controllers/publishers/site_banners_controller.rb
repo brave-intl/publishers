ba4551d0b0d1e1f332d4e2e4946b0129b1bfc15f
Albert Wang <amwang217@gmail.com>
2018-08-15 13:39:01 -0700

    Support image upload

--- app/controllers/publishers/site_banners_controller.rb
+++ app/controllers/publishers/site_banners_controller.rb
@@ -1,5 +1,4 @@
 class Publishers::SiteBannersController < ApplicationController
-
   def new
     @site_banner = current_publisher.site_banner || SiteBanner.new
   end
@@ -11,17 +10,43 @@ class Publishers::SiteBannersController < ApplicationController
       title: params[:title],
       description: params[:description]
     )
-
-    site_banner.logo.attach(params[:logo]) if params[:logo].present?
-    site_banner.background_image.attach(params[:background_image]) if params[:background_image].present?
     redirect_to(new_publisher_site_banner_path(current_publisher))
   end
 
   def update_logo
+    site_banner = current_publisher.site_banner
+    update_image(site_banner.logo)
     head :ok
   end
 
   def update_background_image
+    site_banner = current_publisher.site_banner
+    update_image(site_banner.background_image)
     head :ok
+  end
+
+  private
+
+  def update_image(attachment)
+    data_url = params[:image].split(',')[0]
+    if data_url.starts_with?("data:image/jpeg")
+      content_type = "image/jpeg"
+      extension = ".jpg"
+    elsif data_url.starts_with?("data:image/png")
+      content_type = "image/png"
+      extension = ".png"
+    else
+      # TODO: Throw an exception here
+    end
+    filename = Time.now.to_s.gsub!(" ", "_").gsub!(":", "_") + current_publisher.id + "_logo"
+
+    file = Tempfile.new([filename, extension])
+    File.open(file.path, 'wb') do |f|
+      f.write(Base64.decode64(params[:image].split(',')[1]))
+    end
+    attachment.attach(io: open(file.path),
+                      filename: filename,
+                      content_type: content_type
+                     )
   end
 end
