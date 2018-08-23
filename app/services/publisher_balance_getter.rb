9d4b18ccb01e3ff5954785f4a5cd01bbcf4ffdce
Nick von Pentz <nickvonpentz@gmail.com>
2018-08-23 15:08:05 -0400

    Use twitch#author:{channel_id} for twitch channels for PublisherBalanceGetter only

--- app/services/publisher_balance_getter.rb
+++ app/services/publisher_balance_getter.rb
@@ -17,6 +17,7 @@ class PublisherBalanceGetter < BaseApiClient
     end
 
     accounts = JSON.parse(accounts_response.body)
+
     complete_accounts = fill_in_missing_accounts(accounts)
     complete_accounts
   end
@@ -26,17 +27,31 @@ class PublisherBalanceGetter < BaseApiClient
   end
 
   private
-
+  
+  # TODO: Do not use twitch_author_id method when all twitch#author is used everywhere
   def channels_query_string
     return "" if publisher.channels.verified.count == 0
-    publisher.channels.verified.map { |channel| "&account=#{URI.escape(channel.details.channel_identifier)}" }.reduce(:+)
+    publisher.channels.verified.map { |channel|
+      if channel.details_type == "TwitchChannelDetails" && !Rails.env.test?
+        channel_id = twitch_author_id(channel)
+      else
+        channel_id = channel.details.channel_identifier
+      end
+      "&account=#{URI.escape(channel_id)}"}.reduce(:+)
   end
 
   # Eyeshade may return a 0 balance or an empty response for accounts (channel and owner)
   # with no balance, so we must fill in these values
   def fill_in_missing_accounts(accounts)
     # Find all of the publisher's verified channel ids
-    verified_channel_ids = publisher.channels.verified.map { |channel| channel.details.channel_identifier }
+    # TODO: Remove twitch_author_id method when all twitch#author is used everywhere
+    verified_channel_ids = publisher.channels.verified.map do |channel|
+      if channel.details_type == "TwitchChannelDetails" && !Rails.env.test?
+        twitch_author_id(channel)
+      else
+        channel.details.channel_identifier
+      end
+    end
 
     # Find which verified channel ids have been included in the response
     account_ids = accounts.map { |account| account["account_id"] }
@@ -70,6 +85,13 @@ class PublisherBalanceGetter < BaseApiClient
     end
 
     accounts
+  end
+
+  # TODO: Remove this method when all we use twitch#author everywhere
+  # everywhere in the codebase
+  def twitch_author_id(twitch_channel)
+    raise if twitch_channel.details_type != "TwitchChannelDetails"
+    "twitch#author:#{twitch_channel.details.name}"
   end
 
   def api_base_uri
