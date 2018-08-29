8d3fb1908fd458ffebd9e27fc11ef791eab942b8
Nick von Pentz <nickvonpentz@gmail.com>
2018-08-29 17:47:12 -0400

    Add TransferChannelsJob  & tests
    This job will process all valid transfers (ie those that have
    waited the contest period) once a day at 8am.

---
+++ test/jobs/transfer_channels_job_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+require "webmock/minitest"
+
+class TransferChannelsJobTest < ActiveJob::TestCase
+  test "does not approve channels unless timeout period has passed" do
+    channel = channels(:fraudulently_verified_site)
+    contested_by_channel = channels(:locked_out_site)
+    # contest channel
+    Channels::ContestChannel.new(channel: channel, contested_by: contested_by_channel).perform
+
+    assert_enqueued_jobs(0) do 
+      TransferChannelsJob.perform_now
+    end
+
+    contested_by_channel.reload
+    refute contested_by_channel.verified
+  end
+
+  test "approves channels that have waited the timeout period" do
+    channel = channels(:fraudulently_verified_site)
+    contested_by_channel = channels(:locked_out_site)
+    # contest channel
+    Channels::ContestChannel.new(channel: channel, contested_by: contested_by_channel).perform
+
+    travel (Channel::CONTEST_TIMEOUT + 1.minute) do
+      assert_enqueued_jobs(4) do
+        TransferChannelsJob.perform_now\
+      end
+      
+      contested_by_channel.reload
+      assert contested_by_channel.verified
+    end
+  end
+end
