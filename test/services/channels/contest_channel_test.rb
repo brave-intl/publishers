6b4c2abe38520d5c8c4b70c476f947f21c385efc
Nick von Pentz <nickvonpentz@gmail.com>
2018-08-30 13:09:55 -0400

    Add Channels::ContestChannel test for multiple challenges of the same channel

--- test/services/channels/contest_channel_test.rb
+++ test/services/channels/contest_channel_test.rb
@@ -53,4 +53,33 @@ class ContestChannelTest < ActiveJob::TestCase
       Channels::ContestChannel.new(channel: channel, contested_by: contested_by_channel).perform
     end
   end
+
+  test "if a channel is contested, then contested again, the second replaces the first" do
+    channel = channels(:fraudulently_verified_site)
+    contested_by_channel_one = channels(:locked_out_site)
+    contested_by_channel_two = Channel.new(publisher: Publisher.new(email: "contest@verified.org"),
+                                           details: SiteChannelDetails.new(brave_publisher_id: channel.details.brave_publisher_id))
+    contested_by_channel_two.save!
+
+    # contest channel
+    Channels::ContestChannel.new(channel: channel, contested_by: contested_by_channel_one).perform
+    channel.reload; contested_by_channel_one.reload
+
+    assert_equal channel.contested_by_channel, contested_by_channel_one
+    assert_equal contested_by_channel_one.contesting_channel, channel
+    assert contested_by_channel_one.verification_pending
+    
+    first_contest_token = channel.contest_token
+
+    # contest channel from a different account
+    Channels::ContestChannel.new(channel: channel, contested_by: contested_by_channel_two).perform
+
+    assert_equal channel.contested_by_channel, contested_by_channel_two
+
+    assert_equal contested_by_channel_two.contesting_channel, channel
+
+    assert_not_equal first_contest_token, channel.contest_token
+    # ensure original contested_by_channel was deleted when the second contest occurred
+    assert Channel.where(id: contested_by_channel_one.id).empty?
+  end
 end
