88847d9e453f7ea9647ecd8f72ab3d2aa3f761ea
Albert Wang <amwang217@gmail.com>
2018-09-13 10:22:13 -0700

    Revert "Revert "Feature/piwik api""
    This reverts commit ad0793537fa0f6c71a715395474badad7dfe9e13.

---
+++ app/javascript/admin/dashboard/index.js
@@ -0,0 +1,64 @@
+import Chart from 'chart.js';
+
+function createChart(id, title, dataSource) {
+  var wrapper = document.getElementById('wrapper');
+  var canvas = document.createElement('canvas');
+  canvas.setAttribute("width", "400");
+  canvas.setAttribute("height", "100");
+  wrapper.appendChild(canvas);
+
+  var devicesDetection = JSON.parse(document.getElementById(dataSource).value);
+  var labels = [];
+  var visits = [];
+  for (let device of devicesDetection) {
+    labels.push(device.label);
+    visits.push(device.nb_visits);
+  }
+
+  var myChart = new Chart(canvas, {
+      type: 'bar',
+      data: {
+          labels: labels,
+          datasets: [{
+              label: title,
+              data: visits,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+  });
+}
+
+function loadPiwikData() {
+  createChart("piwikDevicesDetectionTypeChart", "Device Visits (Past Week) (nb_visits)", "piwikDevicesDetectionType");
+  createChart("piwikDevicesDetectionBrowserVersionsChart", "Browser Versions (Past Week)", "piwikDevicesDetectionBrowserVersions");
+  createChart("piwikEventsCategoryChart", "Events (Past Week) (nb_visits)", "piwikEventsCategory");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  loadPiwikData();
+});
