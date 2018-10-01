599047b3c94f7ea33b7ef36363bae53b7c51a88b
Nick von Pentz <nickvonpentz@gmail.com>
2018-10-01 15:46:45 -0400

    Use heroku-buildpack-apt to install wkhtmltopdf-binary instead of gem
    * Remove wkhtmltopdf-binary gem
    
    * Specify /usr/bin/wkhtmltopdf as the wkhtmltopdf binary location
    
    * Add Aptfile to downoad wkhtmltopdf and deps
    
    * Also install binary within .travis.yml
    
    * Update README.md

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -9,13 +9,7 @@
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
 WickedPdf.config = {
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  # exe_path: '/usr/local/bin/wkhtmltopdf',
-  #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-
-  # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # layout: 'pdf.html',
+  # We install the binary with heroku-buildpack-apt
+  # https://elements.heroku.com/buildpacks/heroku/heroku-buildpack-apt (see Aptfile)
+  exe_path: Rails.root.join('bin/wkhtmltopdf-with-xvfb.sh').to_s
 }
