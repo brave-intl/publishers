3447891db21a15c13a81d2a4921e48eadef6a270
Nick von Pentz <nickvonpentz@gmail.com>
2018-10-11 14:10:24 -0400

    Only email upon successful channel deletion

--- app/services/channels/approve_channel_transfer.rb
+++ app/services/channels/approve_channel_transfer.rb
@@ -29,21 +29,23 @@ module Channels
         channel_name = @channel.publication_title
 
         # Delete the channel from eyeshade and clean up the promo registration
-        DeletePublisherChannelJob.perform_now(channel_id: @channel.id) if @should_delete
+        success = DeletePublisherChannelJob.perform_now(channel_id: @channel.id) if @should_delete
 
-        # Email the original owner
-        PublisherMailer.channel_transfer_approved_primary(channel_name, original_owner_name, original_owner_email).deliver_later
-        PublisherMailer.channel_transfer_approved_primary_internal(channel_name, original_owner_name, original_owner_email).deliver_later
+        if success 
+          # Email the original owner
+          PublisherMailer.channel_transfer_approved_primary(channel_name, original_owner_name, original_owner_email).deliver_later
+          PublisherMailer.channel_transfer_approved_primary_internal(channel_name, original_owner_name, original_owner_email).deliver_later
 
-        new_owner_email = contested_by.publisher.email
-        channel_name = contested_by.publication_title
+          new_owner_email = contested_by.publisher.email
+          channel_name = contested_by.publication_title
 
-        # Email the new owner
-        PublisherMailer.channel_transfer_approved_secondary(contested_by).deliver_later
-        PublisherMailer.channel_transfer_approved_secondary_internal(contested_by).deliver_later
+          # Email the new owner
+          PublisherMailer.channel_transfer_approved_secondary(contested_by).deliver_later
+          PublisherMailer.channel_transfer_approved_secondary_internal(contested_by).deliver_later
 
-        # Notify Slack
-        SlackMessenger.new(message: "#{@channel.details.channel_identifier} has been successfully contested by #{@channel.publisher.owner_identifier}.").perform
+          # Notify Slack
+          SlackMessenger.new(message: "#{@channel.details.channel_identifier} has been successfully contested by #{@channel.publisher.owner_identifier}.").perform
+        end
       end
     end
   end
