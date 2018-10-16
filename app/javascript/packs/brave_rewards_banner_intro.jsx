02db2ccfc43c96b6b57b3908ab12751635e37fb4
Albert Wang <amwang217@gmail.com>
2018-10-16 09:59:10 -0700

    Revert "Dlipeles feature/rewards banner"

---
+++ app/javascript/packs/brave_rewards_banner_intro.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class BraveRewardsBannerIntro extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="brave-rewards-banner-intro">
+      <p> Hi! Intro! </p>
+      </div>
+    );
+  }
+}
