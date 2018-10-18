4e90d6ed10d04df98d01aa50796e062cf561cc63
Albert Wang <amwang217@gmail.com>
2018-10-18 14:42:15 -0700

    Remove unused test for whitelist check

--- test/controllers/publishers/site_banners_controller_test.rb
+++ test/controllers/publishers/site_banners_controller_test.rb
@@ -4,24 +4,6 @@ class PublishersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   include PublishersHelper
 
-  test "publisher not part of the whitelist can't upload" do
-    publisher = publishers(:completed)
-    sign_in publisher
-
-    get home_publishers_path
-    assert_response :success
-
-    post(publisher_site_banners_path(publisher),
-      params: {
-        title: "Hello World",
-        description: "Lorem Ipsum",
-        donation_amounts: "[5, 10, 15]",
-      }
-    )
-
-    assert_response :unauthorized
-  end
-
   test "publisher saves a site banner and the data is consistent" do
     publisher = publishers(:completed)
     sign_in publisher
