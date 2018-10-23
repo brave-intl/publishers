618188f9d141fd081b17910b91b0d1ab094e71a8
Nick von Pentz <nickvonpentz@gmail.com>
2018-10-23 08:28:05 -0400

    Modify referral promo data modal to support multiple channels
    * Introduce "kind" column on the promo_registrations table
      that indicates what the promo registration belongs to (
      one of channel, publisher, or unattached).
    
    * Relax constraints on the PromoRegistration model and table
      such that a channel_id is not required
    
    * Add a rake task to set kind = "channel" for all
      PromoRegistrations up until this point.
    
    * Add stats to PromoRegistration.  For unattached codes,
      stats must be associated with the PromoRegistration,
      since there is no associated Publisher which would
      own the stats.

--- app/models/promo_registration.rb
+++ app/models/promo_registration.rb
@@ -1,11 +1,30 @@
 class PromoRegistration < ApplicationRecord
   has_paper_trail
+
+  # A promo registration can belong to a channel,
+  # a publisher, or be unattached.  Unattached codes
+  # are created by admins.
+
+  KINDS = ["channel", "owner", "unattached"].freeze
   
   belongs_to :channel, validate: true, autosave: true
+  belongs_to :promo_campaign
 
-  validates :channel_id, presence: true
+  validates :channel_id, presence: true, if: -> { kind == "channel"}
 
   validates :promo_id, presence: true
-
+  validates :kind, presence: true
+  validates :kind, inclusion: { in: KINDS, message: "%{value} is not a valid kind of promo registration." }
   validates :referral_code, presence: true, uniqueness: { scope: :promo_id }
+
+  def aggregate_stats
+    JSON.parse(stats).reduce({"retrievals" => 0,
+                              "first_runs" => 0,
+                              "finalized" => 0}) { |aggregate_stats, event|
+      aggregate_stats["retrievals"] += event["retrievals"]
+      aggregate_stats["first_runs"] += event["first_runs"]
+      aggregate_stats["finalized"] += event["finalized"]
+      aggregate_stats.slice("retrievals", "first_runs", "finalized")
+    }
+  end
 end
--- test/models/promo_registration_test.rb
+++ test/models/promo_registration_test.rb
@@ -4,21 +4,39 @@ class PromoRegistrationTest < ActiveSupport::TestCase
   PROMO_ID = "2018q1"
   REFERRAL_CODE = "BATS-123"
 
-  test "promo registration must have an associated channel" do
+  test "promo registration doesn't need an associated channel_id if the kind is unattached" do
     channel = channels(:verified)
-    promo_registration = PromoRegistration.new(channel_id: nil, promo_id: PROMO_ID, referral_code: REFERRAL_CODE)
+    promo_registration = PromoRegistration.new(channel_id: nil, promo_id: PROMO_ID, kind: "unattached", referral_code: REFERRAL_CODE)
+    assert promo_registration.valid?
+  end
 
-    # verify validation fails if no associated channel
-    assert !promo_registration.valid?
+  test "promo registration must have be kind channel if channel_id is present" do
+    channel = channels(:verified)
+    promo_registration = PromoRegistration.new(channel_id: channel.id, promo_id: PROMO_ID, referral_code: REFERRAL_CODE)
 
-    # verify validation passes with associated channel
+    # verify validation fails if no kind is present
+    refute promo_registration.valid?
+
+    # verify validation passes is kind is set to channel
+    promo_registration.kind = "channel"
+    assert promo_registration.valid?
+  end
+
+  test "promo registration must have a channel_id if kind is channel" do
+    channel = channels(:verified)
+    promo_registration = PromoRegistration.new(kind: "channel", promo_id: PROMO_ID, referral_code: REFERRAL_CODE)
+
+    # verify validation fails if no channel_id
+    refute promo_registration.valid?
+
+    # verify validation passes is kind is set to channel
     promo_registration.channel_id = channel.id
     assert promo_registration.valid?
   end
 
   test "promo registration must have a promo id" do
     channel = channels(:verified)
-    promo_registration = PromoRegistration.new(channel_id: channel.id, promo_id: "", referral_code: REFERRAL_CODE)
+    promo_registration = PromoRegistration.new(channel_id: channel.id, promo_id: "", kind: "channel", referral_code: REFERRAL_CODE)
 
     # verify validation fails if no associated promo_id
     assert !promo_registration.valid?
@@ -30,7 +48,7 @@ class PromoRegistrationTest < ActiveSupport::TestCase
 
   test "promo registration must have a referral code" do
     channel = channels(:verified)
-    promo_registration = PromoRegistration.new(channel_id: channel.id, promo_id: PROMO_ID, referral_code: nil)
+    promo_registration = PromoRegistration.new(channel_id: channel.id, kind: "channel", promo_id: PROMO_ID, referral_code: nil)
 
     # verify validation fails is no associated referral code
     assert !promo_registration.valid?
@@ -43,11 +61,11 @@ class PromoRegistrationTest < ActiveSupport::TestCase
   test "promo registration must have a unique referral code " do
     channel_verified = channels(:verified)
     channel_completed = channels(:completed)
-    promo_registration = PromoRegistration.new(channel_id: channel_verified.id, promo_id: PROMO_ID, referral_code: REFERRAL_CODE)
+    promo_registration = PromoRegistration.new(channel_id: channel_verified.id, promo_id: PROMO_ID, kind: "channel", referral_code: REFERRAL_CODE)
     promo_registration.save!
 
     # verify validation fails with non unique referall code
-    promo_registration_invalid = PromoRegistration.new(channel_id: channel_completed.id, promo_id: PROMO_ID, referral_code: REFERRAL_CODE)
+    promo_registration_invalid = PromoRegistration.new(channel_id: channel_completed.id, promo_id: PROMO_ID, kind: "channel", referral_code: REFERRAL_CODE)
     assert !promo_registration_invalid.valid?
 
     # verify validation passes with unique referral code
@@ -58,9 +76,9 @@ class PromoRegistrationTest < ActiveSupport::TestCase
   # This might be better suited for ChannelTest
   test "channel can only have one promo registration" do
     channel = channels(:verified)
-    promo_registration = PromoRegistration.new(channel_id: channel.id, promo_id: PROMO_ID, referral_code: REFERRAL_CODE)
+    promo_registration = PromoRegistration.new(channel_id: channel.id, promo_id: PROMO_ID, kind: "channel", referral_code: REFERRAL_CODE)
     promo_registration.save!
-    promo_registration_invalid = PromoRegistration.new(channel_id: channel.id, promo_id: PROMO_ID, referral_code: "BATS-321")
+    promo_registration_invalid = PromoRegistration.new(channel_id: channel.id, promo_id: PROMO_ID, kind: "channel", referral_code: "BATS-321")
     promo_registration.save!
 
     # verify channel has only the first promo detail
@@ -70,7 +88,7 @@ class PromoRegistrationTest < ActiveSupport::TestCase
 
   test "if promo registration deleted, associated channel shouldn't be deleted" do
     channel = channels(:verified)
-    promo_registration = PromoRegistration.new(channel_id: channel.id, promo_id: PROMO_ID, referral_code: REFERRAL_CODE)
+    promo_registration = PromoRegistration.new(channel_id: channel.id, promo_id: PROMO_ID, kind: "channel", referral_code: REFERRAL_CODE)
     promo_registration.save!
 
     assert_equal channel.promo_registration, promo_registration
@@ -79,7 +97,34 @@ class PromoRegistrationTest < ActiveSupport::TestCase
     assert Channel.exists?(channel.id)
   end
 
-  test "if promo registration deleted, associated publisher shouldn't be deleted" do
-    # TO DO
+  test "aggregate_stats aggregates stats" do
+    stats = [{"referral_code"=>"GHI789",
+              "ymd"=>"2018-02-24",
+              "retrievals"=>0,
+              "first_runs"=>3,
+              "finalized"=>0},
+             {"referral_code"=>"GHI789",
+              "ymd"=>"2018-02-25",
+              "retrievals"=>0,
+              "first_runs"=>3,
+              "finalized"=>0},
+             {"referral_code"=>"GHI789",
+              "ymd"=>"2018-02-25",
+              "retrievals"=>0,
+              "first_runs"=>1,
+              "finalized"=>1},
+             {"referral_code"=>"GHI789",
+              "ymd"=>"2018-02-25",
+              "retrievals"=>0,
+              "first_runs"=>1,
+              "finalized"=>1}].to_json
+
+    PromoRegistration.create!(referral_code: "GHI789", kind: "unattached", promo_id: PROMO_ID, stats: stats)
+
+    promo_registration = PromoRegistration.find_by_referral_code("GHI789")
+
+    assert_equal promo_registration.aggregate_stats["retrievals"], 0
+    assert_equal promo_registration.aggregate_stats["first_runs"], 8
+    assert_equal promo_registration.aggregate_stats["finalized"], 2   
   end
 end
