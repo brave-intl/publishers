ddc5539fa5ac301b910437a7198c2a6800582310
Nick von Pentz <nickvonpentz@gmail.com>
2018-10-31 15:54:42 -0400

    Update promo registration flow for channels that have previously registered (#1294)
    * Update promo registration flow for channels that have previously registered
    
    * Update the url used by the PromoRegistrationGetter - this ensures
      the referral code owned by the channel_id supplied is returned
    
    * No longer update the referral server when a channel has changed
      owners via the PromoRegistrationsGetter
    
    * Allow publishers to remove verified channels
    
    * Remove dead tests
    
    * Improve PromoRegistrar code style
    
    * PromoRegistrar#register_channel now returns a hash containing
      the referral_code, and whether or not the referral server
      should be updated with the new owner
    
    * Add test to cover new/existing PromoRegistrar logic
    
    * More style changes

--- app/services/promo_registration_getter.rb
+++ app/services/promo_registration_getter.rb
@@ -1,4 +1,4 @@
-# Gets the referral code associated with a channel + publisher combo
+# Gets the referral code associated with a channel_id
 class PromoRegistrationGetter < BaseApiClient
   include PromosHelper
 
@@ -18,42 +18,17 @@ class PromoRegistrationGetter < BaseApiClient
     response = connection.get do |request|
       request.headers["Authorization"] = api_authorization_header
       request.headers["Content-Type"] = "application/json"
-      request.url("/api/1/promo/publishers?channel=#{@channel.channel_id}")
+      request.url("/api/2/promo/referral_code/channel/#{@channel.channel_id}")
     end
-    registrations =  JSON.parse(response.body)
-    referral_code = referral_code_for_promo_id(registrations)
-    
-    if should_update_channel_owner_on_promo_server(registrations)
-      PromoChannelOwnerUpdater.new(publisher_id: @publisher.id, referral_code: referral_code).perform
-    end
-    
-    referral_code
+    JSON.parse(response.body)
   end
 
   def perform_offline
     Rails.logger.info("PromoRegistrationGetter offline.")
-
-    registrations =
-    [
-      {
-      "referral_code" => offline_referral_code,
-      "promo" => "#{@promo_id}",
-      "publisher" => "#{@channel.channel_id}",
-      "name" => "#{@channel.publication_title}",
-      },
-      {
-      "referral_code" => offline_referral_code,
-      "promo" => "free-bats-2018q2",
-      "publisher" => "#{@channel.channel_id}",
-      "name" => "#{@channel.publication_title}",
-      }
-    ]
-    referral_code = referral_code_for_promo_id(registrations)
+    {}.to_json
   end
 
   class PublisherChannelMismatchError < RuntimeError; end
-
-  class NoReferralCodeError < RuntimeError; end
 
   private
 
@@ -67,23 +42,5 @@ class PromoRegistrationGetter < BaseApiClient
 
   def publisher_owners_channel
     @publisher.channels.include?(@channel)
-  end
-
-  def should_update_channel_owner_on_promo_server(registrations)
-    owner_on_promo_server = registrations[0]["owner_id"]
-    if owner_on_promo_server != @publisher.id
-      return true
-    else
-      return false
-    end
-  end
-
-  def referral_code_for_promo_id(registrations)
-    registrations.each do |registration|
-      if registration["promo"] == active_promo_id
-        return registration["referral_code"]
-      end
-      raise NoReferralCodeError.new("No referral_code has been registered for channel #{@channel.id} for promo #{@promo_id}")
-    end
   end
 end
