f55fa51a3b7c61d64b00b6baeba5abad9ea31c39
Nick von Pentz <nickvonpentz@gmail.com>
2018-11-12 13:54:22 -0500

    Remove active_promo_id from SyncChannelPromoRegistrationStatsJob

--- app/jobs/sync_channel_promo_registrations_stats_job.rb
+++ app/jobs/sync_channel_promo_registrations_stats_job.rb
@@ -3,8 +3,8 @@ class SyncChannelPromoRegistrationsStatsJob < ApplicationJob
   include PromosHelper
   queue_as :scheduler
 
-  def perform(promo_id: active_promo_id)
+  def perform
     promo_registrations = PromoRegistration.channel
-    success = PromoRegistrationsStatsFetcher.new(promo_registrations: promo_registrations).perform
+    PromoRegistrationsStatsFetcher.new(promo_registrations: promo_registrations).perform
   end
 end
