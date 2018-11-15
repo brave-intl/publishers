cc84aeefd4661eced8eb89faa5eebe048eb71bae
Cory McDonald <cory@corywmcdonald.com>
2018-11-15 13:38:57 -0600

    Add pg_stat_statements back in

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2018_10_16_134245) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
