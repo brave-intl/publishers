9d809d69c860afcca55318e47d28a556d4f1d036
Nick von Pentz <nickvonpentz@gmail.com>
2018-11-19 10:14:21 -0500

    Remove publisher promo stats infrastructure
    * Remove publisher#promo_stats_201q1
    
    * Remove publsher#promo_stats_updated_at_2018q1
    
    * Remove Sync::PublisherPromoStatsJob
    
    * Remove PublisherPromoStatsFetcher
    
    * Remove Publisher#promo_stats_status

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -67,8 +67,6 @@ class Publisher < ApplicationRecord
   after_update :set_onboarding_status, if: -> { email.present? && email_before_last_save.nil? }
   after_update :set_active_status, if: -> { saved_change_to_two_factor_prompted_at? && two_factor_prompted_at_before_last_save.nil? }
 
-  after_save :set_promo_stats_updated_at_2018q1, if: -> { saved_change_to_promo_stats_2018q1? }
-
   scope :created_recently, -> { where("created_at > :start_date", start_date: 1.week.ago) }
 
   scope :email_verified, -> { where.not(email: nil) }
@@ -223,22 +221,6 @@ class Publisher < ApplicationRecord
     else
       :inactive
     end
-  end
-
-  def promo_stats_status
-    promo_disabled = !self.promo_enabled_2018q1
-    has_no_promo_enabled_channels = !self.channels.joins(:promo_registration).where.not(promo_registrations: {referral_code: nil}).any?
-    if promo_disabled || has_no_promo_enabled_channels
-      :disabled
-    elsif self.promo_stats_updated_at_2018q1.nil? || self.promo_stats_updated_at_2018q1 < PROMO_STATS_UPDATE_DELAY.ago
-      :update
-    else
-      :updated
-    end
-  end
-
-  def set_promo_stats_updated_at_2018q1
-    update_column(:promo_stats_updated_at_2018q1, Time.now)
   end
 
   def has_verified_channel?
