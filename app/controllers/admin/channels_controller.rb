7ceca13872b0f91faaa1e6c0406ea65449ade4a3
Cory McDonald <cory@corywmcdonald.com>
2018-11-20 16:49:30 -0600

    Fix a lil more lint and restructure search

--- app/controllers/admin/channels_controller.rb
+++ app/controllers/admin/channels_controller.rb
@@ -4,23 +4,8 @@ module Admin
       @channels = Channel
 
       if params[:q].present?
-        # Returns an ActiveRecord::Relation of publishers for pagination
-        search_query = "%#{remove_prefix_if_necessary(params[:q])}%"
-        @channels = @channels
-                      .joins(:publisher)
-                      .left_outer_joins(:site_channel_details)
-                      .left_outer_joins(:youtube_channel_details)
-                      .left_outer_joins(:twitch_channel_details)
-
-        @channels =
-          @channels.where("publishers.email LIKE ?", search_query)
-          .or(@channels.where("publishers.name LIKE ?", search_query))
-          .or(@channels.where("site_channel_details.brave_publisher_id LIKE ?", search_query))
-          .or(@channels.where("twitch_channel_details.twitch_channel_id LIKE ?", search_query))
-          .or(@channels.where("twitch_channel_details.display_name LIKE ?", search_query))
-          .or(@channels.where("twitch_channel_details.email LIKE ?", search_query))
-          .or(@channels.where("youtube_channel_details.youtube_channel_id LIKE ?", search_query))
-          .or(@channels.where("youtube_channel_details.auth_email LIKE ?", search_query))
+        query = "%#{remove_prefix_if_necessary(params[:q])}%"
+        @channels = @channels.search(query)
       end
 
       @channels = @channels.verified if params[:verified].present?
@@ -30,13 +15,14 @@ module Admin
 
     private
 
-    # TODO Remove when https://github.com/brave-intl/publishers/pull/1354 is merged
+    # TODO: Remove when https://github.com/brave-intl/publishers/pull/1354 is merged
     def remove_prefix_if_necessary(query)
       query = query.sub("publishers#uuid:", "")
       query = query.sub("youtube#channel:", "")
       query = query.sub("twitch#channel:", "")
       query = query.sub("twitch#author:", "")
       query = query.sub("twitter#channel:", "")
+      query.strip
     end
   end
 end
--- test/controllers/admin/channels_controller_test.rb
+++ test/controllers/admin/channels_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 require "webmock/minitest"
 
 class Admin::ChannelsControllerTest < ActionDispatch::IntegrationTest
@@ -8,9 +8,9 @@ class Admin::ChannelsControllerTest < ActionDispatch::IntegrationTest
     publisher = publishers(:completed)
     sign_in publisher
 
-    assert_raises(CanCan::AccessDenied) {
+    assert_raises(CanCan::AccessDenied) do
       get admin_publishers_path
-    }
+    end
   end
 
   test "filters correctly" do
@@ -22,23 +22,23 @@ class Admin::ChannelsControllerTest < ActionDispatch::IntegrationTest
 
     get admin_channels_path
     assert_response :success
-    assert_select 'tbody' do
-      assert_select 'tr' do
-        assert_select 'td', channel.id
+    assert_select "tbody" do
+      assert_select "tr" do
+        assert_select "td", channel.id
       end
     end
 
-    get admin_channels_path, params: {q: "#{query}"}
+    get admin_channels_path, params: { q: query }
     assert_response :success
-    assert_select 'tbody' do
-      assert_select 'tr', 1
-      assert_select 'td', channel.id
+    assert_select "tbody" do
+      assert_select "tr", 1
+      assert_select "td", channel.id
     end
 
-    get admin_channels_path, params: {q: "failure"}
+    get admin_channels_path, params: { q: "failure" }
     assert_response :success
-    assert_select 'tbody' do
-      assert_select 'tr', false
+    assert_select "tbody" do
+      assert_select "tr", false
     end
   end
 end
