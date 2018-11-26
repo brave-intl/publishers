683e58608285cb4d255b9211217e340e542c51be
Nick von Pentz <nickvonpentz@gmail.com>
2018-11-26 17:00:36 -0500

    Merge changes only made on master into staging (#1373)
    * Report first runs as downloads instead of retrievals
    
    * Request referral code stats in batches of 1000
    
    * Request referral code stats in batches of 100, not 1000
    
    * Also add BATCH_SIZE constant

--- app/services/promo_registrations_stats_fetcher.rb
+++ app/services/promo_registrations_stats_fetcher.rb
@@ -2,6 +2,8 @@
 class PromoRegistrationsStatsFetcher < BaseApiClient
   include PromosHelper
 
+  BATCH_SIZE = 100
+
   def initialize(promo_registrations:)
     @referral_codes = promo_registrations.map { |promo_registration|
       promo_registration.referral_code
@@ -10,20 +12,24 @@ class PromoRegistrationsStatsFetcher < BaseApiClient
 
   def perform
     return perform_offline if Rails.application.secrets[:api_promo_base_uri].blank?
-    response = connection.get do |request|
-      request.options.params_encoder = Faraday::FlatParamsEncoder
-      request.headers["Authorization"] = api_authorization_header
-      request.headers["Content-Type"] = "application/json"
-      request.url("/api/2/promo/statsByReferralCode#{query_string}")
-    end
-    referral_code_events_date_list = JSON.parse(response.body)
+    @referral_codes.each_slice(BATCH_SIZE).to_a.each do |referral_code_batch|
+      query_string = query_string(referral_code_batch)
+      response = connection.get do |request|
+        request.options.params_encoder = Faraday::FlatParamsEncoder
+        request.headers["Authorization"] = api_authorization_header
+        request.headers["Content-Type"] = "application/json"
+        request.url("/api/2/promo/statsByReferralCode#{query_string}")
+      end
 
-    @referral_codes.each do |referral_code|
-      promo_registration = PromoRegistration.find_by_referral_code(referral_code)
-      promo_registration.stats = referral_code_events_date_list.select {|referral_code_event_date|
-        referral_code_event_date["referral_code"] == referral_code
-      }.to_json
-      promo_registration.save!
+      referral_code_events_by_date = JSON.parse(response.body)
+
+      referral_code_batch.each do |referral_code|
+        promo_registration = PromoRegistration.find_by_referral_code(referral_code)
+        promo_registration.stats = referral_code_events_by_date.select {|referral_code_event_date|
+          referral_code_event_date["referral_code"] == referral_code
+        }.to_json
+        promo_registration.save!
+      end
     end
   end
 
@@ -41,9 +47,9 @@ class PromoRegistrationsStatsFetcher < BaseApiClient
     "Bearer #{Rails.application.secrets[:api_promo_key]}"
   end
 
-  def query_string
+  def query_string(referral_codes)
     query_string = "?"
-    @referral_codes.each do |referral_code|
+    referral_codes.each do |referral_code|
       query_string = "#{query_string}referral_code=#{referral_code}&"
     end
     query_string.chomp("&") # Remove the final ampersand
