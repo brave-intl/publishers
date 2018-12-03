be12d939c597353cd6ffceacad3829c6dca61702
Cory McDonald <cory@corywmcdonald.com>
2018-12-03 09:47:58 -0600

    Update ENV variables for s3

--- lib/publishers/service/public_s3_service.rb
+++ lib/publishers/service/public_s3_service.rb
@@ -3,15 +3,13 @@ module Publishers
     attr_reader :client, :bucket, :upload_options
 
     def initialize
-
       @client = Aws::S3::Resource.new(**
         {
-          :access_key_id=> ENV['S3_REWARDS_ACCESS_KEY_ID='],
-          :secret_access_key=> ENV['S3_REWARDS_SECRET_ACCESS_KEY='],
-          :region=>"us-east-2"
+          access_key_id: ENV['S3_REWARDS_ACCESS_KEY_ID'],
+          secret_access_key: ENV['S3_REWARDS_SECRET_ACCESS_KEY'],
+          region: ENV['S3_REWARDS_BUCKET_REGION']
         }
-      )
-      @bucket = @client.bucket('rewards-stg')
+      @bucket = @client.bucket(ENV['S3_REWARDS_BUCKET_NAME'])
 
       @upload_options = {}
     end
