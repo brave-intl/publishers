089e55def3907cf46e40808c6b63697c8992a44e
Dan A. Lipeles <danlipeles@icloud.com>
2018-12-05 13:11:21 -0500

    Add default_banner to publisher, restore rake task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_173603) do
+ActiveRecord::Schema.define(version: 2018_12_05_180923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -300,6 +300,7 @@ ActiveRecord::Schema.define(version: 2018_12_05_173603) do
     t.datetime "javascript_last_detected_at"
     t.boolean "excluded_from_payout", default: false, null: false
     t.uuid "created_by_id"
+    t.uuid "default_banner"
     t.index "lower((email)::text)", name: "index_publishers_on_lower_email", unique: true
     t.index ["created_at"], name: "index_publishers_on_created_at"
     t.index ["created_by_id"], name: "index_publishers_on_created_by_id"
