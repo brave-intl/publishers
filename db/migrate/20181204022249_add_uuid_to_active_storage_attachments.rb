909b929dbee8e75ecd48932ea01637b4031218a1
Dan A. Lipeles <danlipeles@icloud.com>
2018-12-05 01:28:48 -0500

    Do not recreate index until Stage 2

--- db/migrate/20181204022249_add_uuid_to_active_storage_attachments.rb
+++ db/migrate/20181204022249_add_uuid_to_active_storage_attachments.rb
@@ -7,6 +7,5 @@ class AddUuidToActiveStorageAttachments < ActiveRecord::Migration[5.2]
      end
      execute "ALTER TABLE active_storage_attachments ALTER COLUMN legacy_record_id DROP NOT NULL;"
      remove_index :active_storage_attachments, name: :index_active_storage_attachments_uniqueness
-     add_index :active_storage_attachments, [:record_type, :record_id, :name, :blob_id], name: "index_active_storage_attachments_uniqueness", unique: true
    end
  end
