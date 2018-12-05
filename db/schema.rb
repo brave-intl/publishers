625173b1d997aeae861a89de066d5f893c1ba7f4
Dan A. Lipeles <danlipeles@icloud.com>
2018-12-05 01:46:23 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2018_12_04_022249) do
     t.datetime "created_at", null: false
     t.uuid "record_id"
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
