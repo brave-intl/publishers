db57ceba6dd3d2c3eabb1f58eaf046dfe26ce502
Cory McDonald <cory@corywmcdonald.com>
2018-12-12 09:24:03 -0600

    Search by referral code (#1435)
    I rewrote the query that was originally there because it was very unoptimized. 
    
    Before: `cost=24226.35..26190.97 rows=20150 width=749`
    After: `cost=17396.52..17446.89 rows=20150 width=749`
    
    It's now 40% faster
    
    Closes #1270

--- app/controllers/concerns/search.rb
+++ app/controllers/concerns/search.rb
@@ -27,31 +27,25 @@ module Search
     %{
       publishers.id IN
       (
-        SELECT publishers.id
-        FROM   publishers
-              INNER JOIN(SELECT channels.*
-                          FROM   channels
-                                INNER JOIN site_channel_details
-                                        ON site_channel_details.id = channels.details_id
-                                            AND channels.details_type = 'SiteChannelDetails'
-                                            AND site_channel_details.brave_publisher_id ILIKE :search_query
-                          UNION ALL
-                          SELECT channels.*
-                          FROM   channels
-                                INNER JOIN youtube_channel_details
-                                        ON youtube_channel_details.id =
-                                            channels.details_id
-                                            AND youtube_channel_details.title ILIKE :search_query
-                                            OR youtube_channel_details.youtube_channel_id ILIKE :search_query
-                          UNION ALL
-                          SELECT channels.*
-                          FROM   channels
-                                INNER JOIN twitch_channel_details
-                                        ON twitch_channel_details.id = channels.details_id
-                                            AND twitch_channel_details.NAME ILIKE :search_query)
-                                          c
-                      ON c.publisher_id = publishers.id
+        SELECT publishers.id FROM publishers
+        INNER JOIN (
+          SELECT channels.publisher_id
+          FROM channels
+            LEFT JOIN site_channel_details ON site_channel_details.id = channels.details_id
+            LEFT JOIN youtube_channel_details ON youtube_channel_details.id = channels.details_id
+            LEFT JOIN twitch_channel_details ON twitch_channel_details.id = channels.details_id
+            LEFT JOIN promo_registrations ON promo_registrations.channel_id = channels.id
+          WHERE
+            site_channel_details.brave_publisher_id ILIKE :search_query
+            OR promo_registrations.referral_code ILIKE :search_query
+            OR youtube_channel_details.title ILIKE :search_query
+            OR youtube_channel_details.youtube_channel_id ILIKE :search_query
+            OR twitch_channel_details.NAME ILIKE :search_query
+          ) channel_search
+        ON channel_search.publisher_id = publishers.id
+
         UNION ALL
+
         SELECT publishers.id
         FROM publishers
         WHERE publishers.email ILIKE :search_query
