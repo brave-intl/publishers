8a60a0a82f0488f145fac2d84eb4b3acf1269659
Dan A. Lipeles <danlipeles@icloud.com>
2018-12-14 16:39:16 -0500

    Move no-channels edge case logic from UI to server

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -423,7 +423,7 @@ class PublishersController < ApplicationController
   end
 
   def get_site_banner_data
-    create_site_banners()
+    prepare_site_banner_data()
     default_site_banner_mode = current_publisher.default_site_banner_mode
     default_site_banner = {:id => current_publisher.default_site_banner_id, :name => "Default", :type => "Default"}
     channel_banners = current_publisher.channels.map { |channel| {id: channel.site_banner.id, name: channel.publication_title, type: channel.details_type} }
@@ -441,7 +441,7 @@ class PublishersController < ApplicationController
     end
   end
 
-  def create_site_banners
+  def prepare_site_banner_data
     if current_publisher.default_site_banner_id.nil?
       default_site_banner = SiteBanner.new_helper(current_publisher.id, nil)
       current_publisher.update(default_site_banner_id: default_site_banner.id)
@@ -450,6 +450,9 @@ class PublishersController < ApplicationController
       if channel.site_banner.nil?
         channel.site_banner = SiteBanner.new_helper(current_publisher.id, channel.id)
       end
+    end
+    if current_publisher.channels.length.zero?
+      current_publisher.update(default_site_banner_mode: true)
     end
   end
 
