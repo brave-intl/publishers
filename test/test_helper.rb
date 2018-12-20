2c9757a6e5ecebff1b109bfd45f66a7d4d59a6df
Nick von Pentz <nickvonpentz@gmail.com>
2018-12-20 10:46:09 -0500

    Create EyeshadeHelper module for stubbing eyeshade requests for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,8 @@ require "selenium/webdriver"
 require "minitest/rails/capybara"
 require "webmock/minitest"
 require "chromedriver/helper"
-require 'sidekiq/testing'
+require "sidekiq/testing"
+require "test_helpers/eyeshade_helper"
 
 # https://github.com/rails/rails/issues/31324
 if ActionPack::VERSION::STRING >= "5.2.0"
@@ -109,6 +110,7 @@ module ActionDispatch
     end
   end
 end
+
 
 # Load rake tasks here so it only happens one time. If tasks are loaded again they will run once for each time loaded.
 require 'rake'
