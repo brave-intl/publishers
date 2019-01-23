ea194acffc6e4bf323f7e096b47b2440e14dcc72
Dan A. Lipeles <danlipeles@icloud.com>
2019-01-22 19:44:58 -0500

    PromoCampaigns API for stats (#1547)
    * v1 stats promo-campaigns API

--- config/routes.rb
+++ config/routes.rb
@@ -102,6 +102,7 @@ Rails.application.routes.draw do
           get :youtube_channels_by_view_count
         end
         resources :channels, defaults: { format: :json }, only: %i(show)
+        resources :promo_campaigns, defaults: { format: :json }, only: %i(index show)
         namespace :publishers, defaults: { format: :json } do
           get :signups_per_day
           get :email_verified_signups_per_day
