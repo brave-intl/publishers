2cea7d3f8ecfdaee8db404b2c547fdec6ac95e56
Nick von Pentz <nickvonpentz@gmail.com>
2019-02-22 15:54:17 -0500

    Use camel case for payout report json

--- test/models/json_builders/payout_report_json_builder_test.rb
+++ test/models/json_builders/payout_report_json_builder_test.rb
@@ -9,7 +9,7 @@ class PayoutReportJsonBuilderTest < ActiveSupport::TestCase
     payout_json.each do |potential_payment|
       assert_equal "90000000000", potential_payment["probi"]
       assert_equal "bc29c05c-e6aa-450c-8ea2-7d598666fac0", potential_payment["address"]
-      assert_equal "6c6bb015-f307-4440-9935-552a9fa184cb", potential_payment["uphold_id"]
+      assert_equal "6c6bb015-f307-4440-9935-552a9fa184cb", potential_payment["upholdId"]
     end
   end
 end
