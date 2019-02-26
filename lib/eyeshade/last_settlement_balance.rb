4766e618a130119c0ceca7a4fd037ec0eb565270
Albert Wang <amwang217@gmail.com>
2019-02-26 13:38:52 -0800

    Check for settlement transactions

--- lib/eyeshade/last_settlement_balance.rb
+++ lib/eyeshade/last_settlement_balance.rb
@@ -6,10 +6,12 @@ module Eyeshade
       super(rates, default_currency)
 
       last_settlement = calculate_last_settlement(transactions)
-      @amount_bat = last_settlement["amount_bat"]
-      @timestamp = last_settlement["timestamp"]
-      @settlement_currency = last_settlement["currency"]
-      @amount_settlement_currency = convert(@amount_bat, @settlement_currency)
+      if last_settlement.present?
+        @amount_bat = last_settlement["amount_bat"]
+        @timestamp = last_settlement["timestamp"]
+        @settlement_currency = last_settlement["currency"]
+        @amount_settlement_currency = convert(@amount_bat, @settlement_currency)
+      end
     end
 
     private
@@ -17,10 +19,13 @@ module Eyeshade
     def calculate_last_settlement(transactions)
       return {} if transactions == []
        # Find most recent settlement transaction
-      last_settlement_time = transactions.select { |transaction|
+      last_transaction = transactions.select { |transaction|
         transaction["settlement_amount"].present?
-      }.last["created_at"].to_time # Eyeshade returns transactions ordered_by created_at
+      }.last # Eyeshade returns transactions ordered_by created_at
 
+      return {} if last_transaction.nil?
+
+      last_settlement_time = last_transaction["created_at"].to_time # Eyeshade returns transactions ordered_by created_at
 
        # Find all settlement transactions that occur within the same month as the last settlement timestamp
       last_settlement_transactions = transactions.select { |transaction|
@@ -33,7 +38,7 @@ module Eyeshade
       last_settlement_bat = last_settlement_transactions.map { |transaction|
         transaction["settlement_amount"].to_d
       }.reduce(:+)
-      
+
       {"amount_bat" => last_settlement_bat, "timestamp" => last_settlement_time.to_i, "currency" => last_settlement_currency}
     end
   end
