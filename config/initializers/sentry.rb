cec8a2440929c56e80e63d47d7c222ad15da1642
Cory McDonald <cory@corywmcdonald.com>
2019-02-28 10:00:05 -0600

    Track release versions in Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,6 +3,7 @@ if %w(production staging).include?(Rails.env)
   Raven.configure do |config|
     config.dsn = ENV["SENTRY_DSN"] || raise
     config.environments = %w(production staging)
+    config.release = ENV["HEROKU_RELEASE_VERSION"]
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end
