070b27066124182fc3a576e7571a073a090cfa90
Cory McDonald <cory@corywmcdonald.com>
2019-02-28 18:08:53 -0600

    When cache is nil don't parse json wrong

--- app/controllers/admin/stats/top_balances_controller.rb
+++ app/controllers/admin/stats/top_balances_controller.rb
@@ -3,11 +3,11 @@ class Admin::Stats::TopBalancesController < AdminController
     @limit = params[:limit].present? ? params[:limit].to_i : 25
     @result = JSON.parse(case params[:type]
     when Eyeshade::TopBalances::CHANNEL
-      Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_CHANNEL_BALANCES) || []
+      Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_CHANNEL_BALANCES) || "{}"
     when Eyeshade::TopBalances::OWNER
-      Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_PUBLISHER_BALANCES) || []
+      Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_PUBLISHER_BALANCES) || "{}"
     when Eyeshade::TopBalances::UPHOLD
-      Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_UPHOLD_BALANCES) || []
+      Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_UPHOLD_BALANCES) || "{}"
     end)
     if @result.present?
       @result = @result.take(@limit)
