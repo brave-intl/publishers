18500756e79bc95564a1b2b70a6da11c2471485e
Albert Wang <amwang217@gmail.com>
2019-03-06 12:20:18 -0800

    Revert "Remove IP Address from Publishers Table (#1639)"
    This reverts commit b363fac9b0d17aacaed2c3ac75ec2891b9cd1a74.

--- db/migrate/20190222204716_remove_ip_tracking_from_publishers.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIpTrackingFromPublishers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :publishers, :current_sign_in_ip, :inet
-    remove_column :publishers, :last_sign_in_ip, :inet
-  end
-end
