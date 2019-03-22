34ff5b4c90345678c52fb626d6805badd6767af6
Cory McDonald <cory@corywmcdonald.com>
2019-03-22 17:18:37 -0500

    Fix broken tests

--- test/models/partner_test.rb
+++ test/models/partner_test.rb
@@ -8,13 +8,40 @@ class PartnerTest < ActiveSupport::TestCase
 
   describe 'when balance is called' do
     let(:partner) { partners(:default_partner) }
-    before do
-      Invoice.destroy_all
-      Invoice.create(partner: partner, date: "2019-01-01", amount: 20)
+
+    describe 'when there are 2 invoices in progress' do
+      before do
+        Invoice.destroy_all
+        Invoice.create(partner: partner, date: "2019-01-01", status: "in progress", amount: 20)
+        Invoice.create(partner: partner, date: "2019-02-01", status: "in progress", amount: 20)
+      end
+
+      it 'adds the total amount of invoices' do
+        expect(partner.balance).must_equal 40
+      end
     end
 
-    it 'adds the total amount of invoices' do
-      expect(partner.balance).must_equal 20
+    describe 'when there is 1 invoice in progress' do
+      before do
+        Invoice.destroy_all
+        Invoice.create(partner: partner, date: "2019-01-01", status: "in progress", amount: 20)
+      end
+
+      it 'shows only the amount of the invoice' do
+        expect(partner.balance).must_equal 20
+      end
+    end
+
+    describe 'when there is 1 invoice in progress and 1 paid' do
+      before do
+        Invoice.destroy_all
+        Invoice.create(partner: partner, date: "2019-01-01", status: "in progress", amount: 20)
+        Invoice.create(partner: partner, date: "2019-01-01", status: "paid", amount: 20)
+      end
+
+      it 'only shows the in progress balance' do
+        expect(partner.balance).must_equal 20
+      end
     end
   end
 
@@ -22,7 +49,7 @@ class PartnerTest < ActiveSupport::TestCase
     let(:partner) { publishers(:completed_partner).becomes(Partner) }
     before do
       Invoice.destroy_all
-      Invoice.create(partner: partner, date: "2019-01-01", amount: 200)
+      Invoice.create(partner: partner, status: "in progress",  date: "2019-01-01", amount: 200)
     end
 
     it 'adds invoices and displays in default currency' do
