b73ed4d472541ab29511e8cc67dcd7a2df04c76a
Dan A. Lipeles <danlipeles@icloud.com>
2019-03-22 10:59:58 -0400

    Initial Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_022345) do
+ActiveRecord::Schema.define(version: 2019_03_22_143005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -323,7 +323,7 @@ ActiveRecord::Schema.define(version: 2019_03_07_022345) do
   end
 
   create_table "publishers", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "", null: false
     t.string "email"
     t.string "pending_email"
     t.string "phone"
