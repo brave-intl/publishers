bc7c9b0a12c883f8714785035677e92a33935dcc
Cory McDonald <cory@corywmcdonald.com>
2019-03-22 14:34:46 -0500

    Minor test fix
    Missed a test

--- test/features/log_in_test.rb
+++ test/features/log_in_test.rb
@@ -68,7 +68,7 @@ class LogInTest < Capybara::Rails::TestCase
     fill_in 'publisher_email', with: publisher.email
     click_button 'Log In'
     visit publisher_path(publisher, token: publisher.reload.authentication_token)
-    assert_content page, "DASHBOARD"
+    assert_content page, "PENDING PAYOUTS"
   end
 
   test "a user with TOTP enabled will be asked for an auth code after log in" do
@@ -86,7 +86,7 @@ class LogInTest < Capybara::Rails::TestCase
 
     fill_in 'totp_password', with: '123456'
     click_button 'Verify'
-    assert_content page, "DASHBOARD"
+    assert_content page, "PENDING PAYOUTS"
   end
 
   test "a user with TOTP enabled can retry entry of their auth code" do
@@ -108,7 +108,7 @@ class LogInTest < Capybara::Rails::TestCase
     ROTP::TOTP.any_instance.stubs(:verify_with_drift_and_prior).returns(Time.now.to_i)
     fill_in 'totp_password', with: '123456'
     click_button 'Verify'
-    assert_content page, "DASHBOARD"
+    assert_content page, "PENDING PAYOUTS"
   end
 
   test "a user with U2F enabled will be asked to insert their U2F device after log in" do
@@ -130,7 +130,7 @@ class LogInTest < Capybara::Rails::TestCase
     # Simulate U2F device usage, which submits the form on success
     page.execute_script("document.querySelector('input[name=\"u2f_response\"]').value = '#{u2f_response}';")
     page.execute_script("document.querySelector('form.js-authenticate-u2f').submit();")
-    assert_content page, "DASHBOARD"
+    assert_content page, "PENDING PAYOUTS"
   end
 
   test "a user with U2F enabled can choose to use TOTP if they don't have their device" do
@@ -155,6 +155,6 @@ class LogInTest < Capybara::Rails::TestCase
 
     fill_in 'totp_password', with: '123456'
     click_button 'Verify'
-    assert_content page, "DASHBOARD"
+    assert_content page, "PENDING PAYOUTS"
   end
 end
