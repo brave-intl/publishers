62971033757f05b95f24b7b8e859ac4b2dd52cf3
Cory McDonald <cory@corywmcdonald.com>
2019-03-22 16:43:10 -0500

    Post settlement report (#1660)
    * Post settlement report
    
    * Tests woo
    
    * whitespace :fish:
    
    * Albert was right
    
    * Revert Gemfile
    
    * Track the invoice id
    
    * Address code review
    
    * Remove useless column
    
    * Missing end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_022345) do
+ActiveRecord::Schema.define(version: 2019_03_19_174004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,6 @@ ActiveRecord::Schema.define(version: 2019_03_07_022345) do
     t.date "date"
     t.string "amount", default: "0"
     t.string "finalized_amount"
-    t.boolean "paid", default: false
     t.uuid "paid_by_id"
     t.date "payment_date"
     t.uuid "finalized_by_id"
