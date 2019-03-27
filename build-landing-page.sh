754b8968f0929c3aff6f2d4668d9b01284367c91
Cory McDonald <cory@corywmcdonald.com>
2019-03-27 09:41:47 -0500

    Continue testing

--- build-landing-page.sh
+++ build-landing-page.sh
@@ -1,7 +1,11 @@
+echo "Running script"
 if git diff --cached --name-status | grep public/creators-landing
 then
+  echo "Running the build"
   cd public/creators-landing && yarn install && yarn build
   cd -
+  exit 0
 else
-   exit 0
+  echo "Exiting!"
+  exit 0
 fi
