55c4ed2d566a15da3ea0634897652981c4e6cbe5
Dan A. Lipeles <danlipeles@icloud.com>
2019-04-09 17:42:20 -0400

    Commit

---
+++ app/javascript/packs/views/admin/referrals/Referrals.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Referrals from "../../../../views/admin/referrals/Referrals";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <Referrals />,
+    document
+      .getElementById("main-content")
+      .appendChild(document.createElement("div"))
+  );
+});
---
+++ app/javascript/views/admin/referrals/Referrals.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default class Referrals extends React.Component<{}, {}> {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  public render() {
+    return <div>Hello</div>;
+  }
+}
