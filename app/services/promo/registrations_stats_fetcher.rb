eda903edde898415982f78e04bee48ca26c731df
Dan A. Lipeles <danlipeles@icloud.com>
2019-04-12 15:35:00 -0400

    Commit

--- app/services/promo/registrations_stats_fetcher.rb
+++ app/services/promo/registrations_stats_fetcher.rb
@@ -21,12 +21,12 @@ class Promo::RegistrationsStatsFetcher < BaseApiClient
       referral_code_events_by_date = JSON.parse(response.body)
       referral_code_batch.each do |referral_code|
         promo_registration = PromoRegistration.find_by_referral_code(referral_code)
-        promo_registration.stats = referral_code_events_by_date.select {|referral_code_event_date|
+        promo_registration.stats = referral_code_events_by_date.select { |referral_code_event_date|
           referral_code_event_date["referral_code"] == referral_code
         }.to_json
         promo_registration.save!
       end
-      stats = stats + referral_code_events_by_date
+      stats += referral_code_events_by_date
     end
 
     stats
@@ -35,17 +35,17 @@ class Promo::RegistrationsStatsFetcher < BaseApiClient
   def perform_offline
     stats = []
     @referral_codes.each do |referral_code|
-      ((1.month.ago.utc.to_date)..(Time.now.utc.to_date)).each do |day|
+      (1..6).reverse_each do |i|
         event = {
           "referral_code" => "#{referral_code}",
-          PromoRegistration::RETRIEVALS => 1,
-          PromoRegistration::FIRST_RUNS => 1,
-          PromoRegistration::FINALIZED => 1,
-          "ymd" => "#{day}",
+          PromoRegistration::RETRIEVALS => rand(50..75),
+          PromoRegistration::FIRST_RUNS => rand(30..50),
+          PromoRegistration::FINALIZED => rand(1..30),
+          "ymd" => "#{i.month.ago.utc.to_date}",
         }
-        PromoRegistration.find_by_referral_code(referral_code).update(stats: [event].to_json)
         stats.push(event)
       end
+      PromoRegistration.find_by_referral_code(referral_code).update(stats: stats.to_json)
     end
 
     stats
