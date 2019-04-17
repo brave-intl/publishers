932118783bf67bdce5130aeb697e53edfce458ad
Albert Wang <amwang217@gmail.com>
2019-04-16 19:40:53 -0700

    Omniauth tests

--- app/controllers/publishers/omniauth_callbacks_controller.rb
+++ app/controllers/publishers/omniauth_callbacks_controller.rb
@@ -159,8 +159,8 @@ module Publishers
 
     def register_vimeo_channel
       vimeo_auth_hash = request.env['omniauth.auth']
-      channel = current_publisher.channels.new(verified: true)
-      channel.details = VimeoChannelDetails.new(
+      @channel = current_publisher.channels.new(verified: true)
+      @channel.details = VimeoChannelDetails.new(
         name: vimeo_auth_hash.info.name,
         vimeo_channel_id: vimeo_auth_hash.info.id,
         auth_provider: vimeo_auth_hash.info.auth_provider,
@@ -169,16 +169,16 @@ module Publishers
         nickname: vimeo_auth_hash.info.nickname
       )
 
-      existing_channel = Channel.verified.joins(:vimeo_channel_details).
-          where("vimeo_channel_details.vimeo_channel_id": vimeo_auth_hash['id']).first
+      existing_channel = Channel.joins(:vimeo_channel_details).
+        where("vimeo_channel_details.vimeo_channel_id": vimeo_auth_hash.info.id).first
 
       if existing_channel&.publisher == current_publisher
-        redirect_to home_publishers_path, notice: t(".channel_already_registered", { channel_title: existing_channel.details.nickname })
+        redirect_to home_publishers_path, notice: t(".channel_already_registered")
         return
       end
 
       contest_channel(existing_channel) and return if existing_channel
-      channel.save!
+      @channel.save!
 
       redirect_to home_publishers_path, notice: t("shared.channel_created")
     end
