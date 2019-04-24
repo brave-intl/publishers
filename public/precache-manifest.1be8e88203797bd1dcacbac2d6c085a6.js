1a961c76f5f9babc23044f300e1692ad90f39812
Cory McDonald <cmcdonald@brave.com>
2019-04-24 09:41:05 -0500

    Upgrade create-react-app 3

---
+++ public/precache-manifest.1be8e88203797bd1dcacbac2d6c085a6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b408f6de4b89deb6e53e7e7171e499f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf28a6dbd40dd5d48dd",
+    "url": "/static/css/main.e43cfce9.chunk.css"
+  },
+  {
+    "revision": "0714d1baebf2047752cb",
+    "url": "/static/js/2.afab0b54.chunk.js"
+  },
+  {
+    "revision": "bcf28a6dbd40dd5d48dd",
+    "url": "/static/js/main.01b9c38f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74b296338283810bc8aa24521154e176",
+    "url": "/static/media/brave-rewards-creators-logo.74b29633.svg"
+  },
+  {
+    "revision": "58394134d4f10d125217be29381bcbbd",
+    "url": "/static/media/brave-rewards-creators-mobile-logo.58394134.svg"
+  },
+  {
+    "revision": "f4c2d5ec3e17b33d5700a4fa021e64e6",
+    "url": "/static/media/built-with-bat-pill.f4c2d5ec.svg"
+  },
+  {
+    "revision": "fe7c3954fbc4553f3038ee1dcce68986",
+    "url": "/static/media/built-with-bat.fe7c3954.svg"
+  },
+  {
+    "revision": "8cc226d99931cf25dfec78e6bbf3a38e",
+    "url": "/static/media/card-baker.8cc226d9.webp"
+  },
+  {
+    "revision": "fe7cb3bcf66643a0457d0417fe71899a",
+    "url": "/static/media/card-bobby.fe7cb3bc.webp"
+  },
+  {
+    "revision": "68815fcecfe20da93ed101ead1d5a190",
+    "url": "/static/media/card-decicco.68815fce.webp"
+  },
+  {
+    "revision": "1bad4ceba7166ae060b6b8c94d759ddf",
+    "url": "/static/media/card-defranco.1bad4ceb.webp"
+  },
+  {
+    "revision": "9e6e4e5b14edf16d188d6d18c8ae5e4f",
+    "url": "/static/media/creator-logos-mobile.9e6e4e5b.webp"
+  },
+  {
+    "revision": "8f3dba9ec23c270e1401e9bc3d9a01c1",
+    "url": "/static/media/creator-logos-wide.8f3dba9e.webp"
+  }
+]);
