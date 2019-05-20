487694b6310b86142680b4a17726def3c87a7ab0
Dan A. Lipeles <danlipeles@icloud.com>
2019-05-20 18:49:27 -0400

    Commit

--- test/models/github_channel_details_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe GithubChannelDetails do
-  let(:github_channel_details) { GithubChannelDetails.new }
-
-  it "must be valid" do
-    value(github_channel_details).must_be :valid?
-  end
-end
--- test/models/reddit_channel_details_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe RedditChannelDetails do
-  let(:reddit_channel_details) { RedditChannelDetails.new }
-
-  it "must be valid" do
-    value(reddit_channel_details).must_be :valid?
-  end
-end
