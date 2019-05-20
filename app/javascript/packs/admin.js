da99eb9870ca769d97a65fe24f9a400d76e1c127
Cory McDonald <cmcdonald@brave.com>
2019-05-20 16:32:13 -0500

    Perhaps it's tribute

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,50 +1,8 @@
-import "utils/request";
-import "admin/dashboard/index";
-import "admin/stats/index";
-import "admin/dashboard/unattached_promo_registration";
-import Rails from "rails-ujs";
-import Tribute from "tributejs";
-import "tributejs/dist/tribute.css";
-
-import Avatar from "../views/admin/components/userNavbar/components/TopNav/Avatar.svg";
-
-document.addEventListener("DOMContentLoaded", function() {
-  if (window.location.href.indexOf("admin/publishers/") !== -1) {
-    fetch("/admin/publishers?role=admin")
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(publishers) {
-        const list = publishers.map(p => {
-          console.log(p);
-          return {
-            key: p.name,
-            value: p.email.split("@")[0],
-            avatarColor: p.avatar_color
-          };
-        });
-
-        var tribute = new Tribute({
-          values: list,
-          menuItemTemplate: function(item) {
-            console.log(item);
-            return (
-              `<div class="d-flex align-items-center">` +
-              `<div class="user-avatar-dropdown" style="background: #${
-                item.original.avatarColor
-              };">` +
-              `<img src=${Avatar} />` +
-              `</div>` +
-              `${item.string}` +
-              `</div>`
-            );
-          }
-        });
-
-        tribute.attach(document.querySelectorAll(".note-form"));
-      });
-  }
-});
+import 'utils/request';
+import 'admin/dashboard/index';
+import 'admin/stats/index';
+import 'admin/dashboard/unattached_promo_registration'
+import Rails from 'rails-ujs';
 
 /*
  * Override the default way Rails picks an href for a data-method link. This
@@ -73,12 +31,10 @@ Rails.href = function Rails_href_override(element) {
 };
 
 Rails.start();
-document.addEventListener(
-  "DOMContentLoaded",
-  function() {
-    let sidebarToggles = document.getElementsByClassName("sidebar-toggle");
+document.addEventListener('DOMContentLoaded', function(){ 
+  let sidebarToggles = document.getElementsByClassName("sidebar-toggle"); 
     for (var i = 0; i < sidebarToggles.length; i++) {
-      sidebarToggles[i].addEventListener("click", function(event) {
+      sidebarToggles[i].addEventListener('click', function (event) {
         var item = event.target || event.srcElement;
         // If the clicked element doesn't have the right selector, bail
         document.activeElement.blur();
@@ -88,18 +44,16 @@ document.addEventListener(
         let icon = item.children[0];
 
         // Toggle the menu
-        let element = item.parentElement.querySelector(".sub-menu");
-        if (element.style.display === "none") {
+        let element = item.parentElement.querySelector('.sub-menu');
+        if(element.style.display === 'none') {
           icon.classList.remove("fa-chevron-down");
           icon.classList.add("fa-chevron-up");
-          element.style.display = "";
+          element.style.display = '';
         } else {
           icon.classList.remove("fa-chevron-up");
           icon.classList.add("fa-chevron-down");
-          element.style.display = "none";
+          element.style.display = 'none';
         }
       });
     }
-  },
-  false
-);
+  }, false);
