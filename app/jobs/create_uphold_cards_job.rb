939d5998b67153715cbd02ce4ed010ae76cf3f06
Cory McDonald <cmcdonald@brave.com>
2019-06-05 09:07:50 -0500

    Remove silent error

--- app/jobs/create_uphold_cards_job.rb
+++ app/jobs/create_uphold_cards_job.rb
@@ -7,6 +7,7 @@ class CreateUpholdCardsJob < ApplicationJob
 
     unless publisher.uphold_connection.can_create_uphold_cards?
       Rails.logger.info("Could not create uphold card for publisher #{publisher.id}.")
+      SlackMessenger.new(message: "Could not create uphold card for publisher #{publisher.id}.").perform
       return
     end
 
