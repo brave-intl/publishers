554cc39bc6ea957d1e315143f6228cea91471dbb
Dan A. Lipeles <danlipeles@icloud.com>
2019-06-11 12:08:19 -0400

    Lint

--- app/models/json_builders/channels_json_builder.rb
+++ app/models/json_builders/channels_json_builder.rb
@@ -36,50 +36,52 @@ class JsonBuilders::ChannelsJsonBuilder
     append_excluded
     @channels.to_json
   end
-end
 
-def join_verified_channels
-  [
-    Channel.verified.site_channels.includes(:site_banner).includes(publisher: :site_banners),
-    Channel.verified.youtube_channels.includes(:site_banner).includes(publisher: :site_banners),
-    Channel.verified.twitch_channels.includes(:site_banner).includes(publisher: :site_banners),
-    Channel.verified.twitter_channels.includes(:site_banner).includes(publisher: :site_banners),
-    Channel.verified.vimeo_channels.includes(:site_banner).includes(publisher: :site_banners),
-    Channel.verified.reddit_channels.includes(:site_banner).includes(publisher: :site_banners),
-    Channel.verified.github_channels.includes(:site_banner).includes(publisher: :site_banners),
-  ]
-end
+  ##################
+  # Helper Functions
+  ##################
 
-def include_verified_channel(verified_channel)
+  def join_verified_channels
+    [
+      Channel.verified.site_channels.includes(:site_banner).includes(publisher: :site_banners),
+      Channel.verified.youtube_channels.includes(:site_banner).includes(publisher: :site_banners),
+      Channel.verified.twitch_channels.includes(:site_banner).includes(publisher: :site_banners),
+      Channel.verified.twitter_channels.includes(:site_banner).includes(publisher: :site_banners),
+      Channel.verified.vimeo_channels.includes(:site_banner).includes(publisher: :site_banners),
+      Channel.verified.reddit_channels.includes(:site_banner).includes(publisher: :site_banners),
+      Channel.verified.github_channels.includes(:site_banner).includes(publisher: :site_banners),
+    ]
+  end
 
-  ############################################################################################
-  # Step one, if channel is on exclusion list, then skip it.
-  ############################################################################################
+  def include_verified_channel(verified_channel)
+    ############################################################################################
+    # Step one, if channel is on exclusion list, then skip it.
+    ############################################################################################
 
-  return if @excluded_channel_ids.include?(verified_channel.details.channel_identifier)
+    return if @excluded_channel_ids.include?(verified_channel.details.channel_identifier)
 
-  ############################################################################################
-  # Step two, if channel not on exclusion list, check for KYC'd Uphold.
-  ############################################################################################
+    ############################################################################################
+    # Step two, if channel not on exclusion list, check for KYC'd Uphold.
+    ############################################################################################
 
-  return if verified_channel.publisher&.uphold_connection&.is_member != true
+    return if verified_channel.publisher&.uphold_connection&.is_member != true
 
-  ############################################################################################
-  # Step three, if channel not on exclusion list, and publisher has KYC'd, add the channel.
-  ############################################################################################
+    ############################################################################################
+    # Step three, if channel not on exclusion list, and publisher has KYC'd, add the channel.
+    ############################################################################################
 
-  @channels.push([
-    verified_channel.details.channel_identifier, 
-    true, 
-    false, 
-    verified_channel.publisher.uphold_connection.address,
-    site_banner_details(verified_channel)
-  ])
+    @channels.push([
+      verified_channel.details.channel_identifier,
+      true,
+      false,
+      verified_channel.publisher.uphold_connection.address,
+      site_banner_details(verified_channel),
+    ])
+  end
 
-end
-
-def append_excluded
-  @excluded_channel_ids.each do |excluded_channel_id|
-    @channels.push([excluded_channel_id, false, true, {}, nil])
+  def append_excluded
+    @excluded_channel_ids.each do |excluded_channel_id|
+      @channels.push([excluded_channel_id, false, true, {}, nil])
+    end
   end
 end
