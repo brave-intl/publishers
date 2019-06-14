9787afa938a22cfe14f68bbce263b03a33e44005
Albert Wang <amwang217@gmail.com>
2019-06-13 22:59:31 -0700

    Logic for policy agreements and UI

--- app/controllers/publishers/policy_agreements_controller.rb
+++ app/controllers/publishers/policy_agreements_controller.rb
@@ -3,8 +3,20 @@ class Publishers::PolicyAgreementsController < ApplicationController
   end
 
   def create
+    flash[:alert] = "You must accept the Terms of Service to proceed" unless create_params[:accepted_publisher_tos] == 'true'
+    flash[:alert] = "You must accept the Privacy Policy to proceed" unless create_params[:accepted_publisher_privacy_policy] == 'true'
+    redirect_to home_publishers_path and return if flash[:alert].present?
+    result = PolicyAgreement.create(
+      user_id: current_publisher.id,
+      accepted_publisher_tos: create_params[:accepted_publisher_tos] == 'true',
+      accepted_publisher_privacy_policy: create_params[:accepted_publisher_privacy_policy] == 'true'
+    )
+
+    redirect_to home_publishers_path
   end
 
-  def update
+  def create_params
+    params.require(:policy_agreement)
+      .permit(:accepted_publisher_tos, :accepted_publisher_privacy_policy)
   end
 end
