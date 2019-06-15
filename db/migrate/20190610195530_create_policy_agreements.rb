1eb328f6f303b1be6c953b1d9a3cd32a67fa99a7
Albert Wang <amwang217@gmail.com>
2019-06-15 11:35:22 -0700

    Add test for policy_agreements

--- db/migrate/20190610195530_create_policy_agreements.rb
+++ db/migrate/20190610195530_create_policy_agreements.rb
@@ -1,7 +1,7 @@
 class CreatePolicyAgreements < ActiveRecord::Migration[5.2]
   def change
     create_table :policy_agreements, id: :uuid, default: -> { "uuid_generate_v4()"}, force: :cascade  do |t|
-      t.references  :user, type: :uuid, null: false, index: true
+      t.references  :user, type: :uuid, null: false, index: true, unique: true
       t.boolean     :accepted_publisher_tos, default: false, null: false
       t.boolean     :accepted_publisher_privacy_policy, default: false, null: false
       t.timestamps
