4c6987fae12cb86566336cc91d32c0a8ff6d8db8
Cory McDonald <cmcdonald@brave.com>
2019-06-18 12:13:42 -0500

    Create responsive add channels modal

--- app/controllers/publishers/omniauth_callbacks_controller.rb
+++ app/controllers/publishers/omniauth_callbacks_controller.rb
@@ -231,6 +231,56 @@ module Publishers
       redirect_to home_publishers_path, notice: t("shared.channel_created")
     end
 
+    def register_reddit_channel
+      reddit_auth_hash = request.env['omniauth.auth']
+      @channel = current_publisher.channels.new(verified: true)
+      @channel.details = RedditChannelDetails.new(
+        name: reddit_auth_hash.info.name,
+        reddit_channel_id: reddit_auth_hash.uid,
+        auth_provider: reddit_auth_hash.provider,
+        thumbnail_url: reddit_auth_hash.extra.raw_info.icon_img,
+        channel_url: "https://www.reddit.com/user/#{reddit_auth_hash.info.name}",
+        nickname: reddit_auth_hash.info.name,
+      )
+
+      existing_channel = Channel.joins(:reddit_channel_details).where("reddit_channel_details.reddit_channel_id": reddit_auth_hash.uid).first
+
+      if existing_channel&.publisher == current_publisher
+        redirect_to home_publishers_path, notice: t(".channel_already_registered")
+        return
+      end
+
+      contest_channel(existing_channel) and return if existing_channel
+      @channel.save!
+
+      redirect_to home_publishers_path, notice: t("shared.channel_created")
+    end
+
+    def register_github_channel
+      github_auth_hash = request.env['omniauth.auth']
+      @channel = current_publisher.channels.new(verified: true)
+      @channel.details = GithubChannelDetails.new(
+        name: github_auth_hash.info.name,
+        github_channel_id: github_auth_hash.uid,
+        auth_provider: github_auth_hash.provider,
+        thumbnail_url: github_auth_hash.info.image,
+        channel_url: github_auth_hash.info.urls.GitHub,
+        nickname: github_auth_hash.info.nickname,
+      )
+
+      existing_channel = Channel.joins(:github_channel_details).where("github_channel_details.github_channel_id": github_auth_hash.uid).first
+
+      if existing_channel&.publisher == current_publisher
+        redirect_to home_publishers_path, notice: t(".channel_already_registered")
+        return
+      end
+
+      contest_channel(existing_channel) and return if existing_channel
+      @channel.save!
+
+      redirect_to home_publishers_path, notice: t("shared.channel_created")
+    end
+
     private
 
     def contest_channel(existing_channel)
