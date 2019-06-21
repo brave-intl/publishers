7b0aa3ca9a9d26a75b4affc3c1b169d3ab6e4e17
Cory McDonald <cmcdonald@brave.com>
2019-06-20 19:02:19 -0500

    Test

--- app/mailers/batch_mailer.rb
+++ app/mailers/batch_mailer.rb
@@ -1,11 +1,13 @@
 # Used for designating batch jobs, usually to all publishers - thus we should queue as low
 class BatchMailer < ApplicationMailer
+  ActionMailer::Base.deliver_later_queue_name = :low
+
   def update_to_tos(publisher)
-    ActionMailer::Base.deliver_later_queue_name = :lowkk
     @publisher = publisher
     mail(
       to: @publisher.email || @publisher.pending_email,
       subject: default_i18n_subject
     )
+    raise
   end
 end
