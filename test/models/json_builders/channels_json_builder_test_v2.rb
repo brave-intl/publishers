985db70241ecc4489afc2173f1c791619791aa21
Albert Wang <amwang217@gmail.com>
2019-07-08 19:38:12 -0700

    Gemini v2 api for a single channel

--- test/models/json_builders/channels_json_builder_test_v2.rb
+++ test/models/json_builders/channels_json_builder_test_v2.rb
@@ -1,12 +1,6 @@
 require 'test_helper'
 
 class ChannelsJsonBuilderTestV2 < ActiveSupport::TestCase
-    
-  test "returns V2 Json" do
-    channels = JSON.parse(JsonBuilders::ChannelsJsonBuilderV2.new.build)
-    assert channels
-  end
-
   test "each verified channel in V2 Json has an associated KYC'd Uphold Connection" do
     channels = JSON.parse(JsonBuilders::ChannelsJsonBuilderV2.new.build)
     verified_channels = []
@@ -15,6 +9,22 @@ class ChannelsJsonBuilderTestV2 < ActiveSupport::TestCase
     end
     verified_channels.each do |verified_channel|
       assert Channel.find_by_channel_identifier(verified_channel[0]).publisher.uphold_connection.is_member
+    end
+  end
+
+  test "Make sure a publisher's uphold address only appears once" do
+    channels = JSON.parse(JsonBuilders::ChannelsJsonBuilderV2.new.build)
+    verified_channels = []
+    channels.each do |channel|
+      verified_channels.push(channel) if channel[1]
+    end
+    published_channels = {}
+    valid_addresses = UpholdConnection.pluck(:address)
+    verified_channels.each do |verified_channel|
+      wallet_address_id = verified_channel[3]
+      refute published_channels.key? wallet_address_id
+      assert wallet_address_id.in?(valid_addresses)
+      published_channels[wallet_address_id] = true
     end
   end
 
