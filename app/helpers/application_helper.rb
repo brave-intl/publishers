6c11363d9b0816d45f82b12597a341a57a0754af
Albert Wang <amwang217@gmail.com>
2019-08-22 10:08:21 -0700

    Fix analytics url (#2181)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,8 @@ module ApplicationHelper
     begin
       host = URI.parse(request.original_url).host
       domain = "#{PublicSuffix.parse(host).domain}"
-      piwik_domain = "#{Rails.application.secrets[:piwik_host_prefix]}.#{domain}" if domain.in?(["basicattentiontoken.org", "brave.com"])
+      piwik_domain = "#{Rails.application.secrets[:piwik_host_prefix]}.#{domain}/" if domain.in?(["basicattentiontoken.org", "brave.com"])
     rescue
     end
-    piwik_domain
   end
 end
