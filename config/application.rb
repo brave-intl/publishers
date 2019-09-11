c17cde2156f94262eae0d0a60da01268bb9b486f
Albert Wang <amwang217@gmail.com>
2019-09-11 13:51:11 -0700

    Use 6.0 default setting

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Publishers
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.load_defaults "6.0"
     config.middleware.use HttpHeaderMiddleware
     config.middleware.use Rack::Deflater
 
@@ -31,6 +32,7 @@ module Publishers
     end
 
     config.lograge.enabled = true
+    config.active_record.belongs_to_required_by_default = false
 
     config.time_zone = "Pacific Time (US & Canada)"
     config.active_record.default_timezone = :local
