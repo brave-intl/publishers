5966ef90ce7ea91c84fc0fa1d34125dea3a0e1df
Albert Wang <amwang217@gmail.com>
2019-09-16 10:14:56 -0700

    Warn if queue size is greater than 1000. Closes #1969 (#2080)
    * Warn if queue size is greater than 1000. Closes #1969
    
    Co-Authored-By: Cory McDonald <cmcdonald@brave.com>

--- config/routes.rb
+++ config/routes.rb
@@ -275,4 +275,9 @@ Rails.application.routes.draw do
     end
   end
   mount Sidekiq::Web, at: "/magic"
+  require 'sidekiq/api'
+  match "mailer-queue-status" => proc { [200, {"Content-Type" => "text/plain"}, [Sidekiq::Queue.new('mailer').size < 100 ? "OK" : "UHOH" ]] }, via: :get
+  match "default-queue-status" => proc { [200, {"Content-Type" => "text/plain"}, [Sidekiq::Queue.new('default').size < 5000 ? "OK" : "UHOH" ]] }, via: :get
+  match "scheduler-queue-status" => proc { [200, {"Content-Type" => "text/plain"}, [Sidekiq::Queue.new('scheduler').size < 5000 ? "OK" : "UHOH" ]] }, via: :get
+  match "transactional-queue-status" => proc { [200, {"Content-Type" => "text/plain"}, [Sidekiq::Queue.new('transactional').size < 5000 ? "OK" : "UHOH" ]] }, via: :get
 end
