abba7f715cfaeddd6dade47cdb2f69ef4c9c0ab3
Albert Wang <amwang217@gmail.com>
2019-09-16 14:26:27 -0700

    Bugfix/performance dyno caching (#2218)
    * Allow dyno caching.

--- app/controllers/api/v2/public/channels_controller.rb
+++ app/controllers/api/v2/public/channels_controller.rb
@@ -1,14 +1,15 @@
 class Api::V2::Public::ChannelsController < Api::V2::Public::BaseController
-  def channels
-    channels_json = Rails.cache.fetch('browser_channels_json_v2', race_condition_ttl: 30) do
-      require 'sentry-raven'
-      Raven.capture_message("Failed to use redis cache for /api/public/channels V2, using DB instead.")
-      channels_json = JsonBuilders::ChannelsJsonBuilderV2.new.build
-    end
-    render(json: channels_json, status: 200)
-  end
+  include BrowserChannelsDynoCaching
+  @@cached_payload = nil
+  REDIS_KEY = 'browser_channels_json_v2'.freeze
 
   def totals
     render(json: Channel.statistical_totals, status: 200)
+  end
+
+  private
+
+  def dyno_expiration_key
+    "browser_v3_channels_expiration:#{ENV['DYNO']}"
   end
 end
---
+++ app/controllers/concerns/browser_channels_dyno_caching.rb
@@ -0,0 +1,41 @@
+module BrowserChannelsDynoCaching
+  extend ActiveSupport::Concern
+  require 'sentry-raven'
+
+  def channels
+    if dyno_cache_expired? || invalid_dyno_cache?
+      update_dyno_cache
+    end
+    render(json: self.class.class_variable_get(klass_dyno_cache), status: 200)
+  end
+
+  private
+
+  def dyno_cache_expired?
+    expiration_time = Rails.cache.fetch(dyno_expiration_key)
+    return expiration_time.nil? || Time.parse(expiration_time) <= Time.now
+  end
+
+  def invalid_dyno_cache?
+    cached_dyno_value = self.class.class_variable_get(klass_dyno_cache)
+    cached_dyno_value.nil? || !cached_dyno_value.is_a?(String)
+  end
+
+  def update_dyno_cache
+    redis_value = Rails.cache.fetch(self.class::REDIS_KEY, race_condition_ttl: 30) do
+      Raven.capture_message("Failed to use redis cache for Dyno cache: #{klass_dyno_cache}, continuing to read from cache instead")
+    end
+    if redis_value.present?
+      self.class.class_variable_set(klass_dyno_cache, redis_value)
+      Rails.cache.write(dyno_expiration_key, 1.hour.from_now.to_s, expires_in: 1.hour.from_now )
+    end
+  end
+
+  def dyno_expiration_key
+    raise "Define me for dyno_expiration_name!"
+  end
+
+  def klass_dyno_cache
+    :@@cached_payload
+  end
+end
