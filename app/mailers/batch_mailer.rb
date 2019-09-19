0d3ee40d29aca5d60d41184fc098316b8659f531
Cory McDonald <cmcdonald@brave.com>
2019-09-19 16:27:35 -0500

    Upcoming email structure

--- app/mailers/batch_mailer.rb
+++ app/mailers/batch_mailer.rb
@@ -1,18 +1,7 @@
 # Used for designating batch jobs, usually to all publishers - thus we should queue as low
 class BatchMailer < ApplicationMailer
-  def update_to_tos(publisher)
+  def notification_for_kyc(publisher)
     @publisher = publisher
-    mail(
-      to: @publisher.email || @publisher.pending_email,
-      subject: default_i18n_subject
-    )
-  end
-
-  def update_to_rates(publisher)
-    @publisher = publisher
-    mail(
-      to: @publisher.email || @publisher.pending_email,
-      subject: default_i18n_subject
-    )
+    mail(to: @publisher.email || @publisher.pending_email, subject: default_i18n_subject)
   end
 end
