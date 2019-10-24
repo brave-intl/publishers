b18b9bca1c8397ea56a06e2dd427339798e25094
Albert Wang <amwang217@gmail.com>
2019-10-23 17:17:56 -0700

    Updated controller

--- app/controllers/publishers/paypal_accounts_controller.rb
+++ app/controllers/publishers/paypal_accounts_controller.rb
@@ -9,13 +9,34 @@ module Publishers
 
     def connect_callback
       authorization_code = params[:code]
-      current_publisher.update(name: authorization_code)
-      p "albert #{authorization_code}"
       result = Faraday.post("https://api.sandbox.paypal.com/v1/oauth2/token") do |req|
-        req.headers['Authorization'] = "Basic #{Rails.application.secrets[:paypal_client_id]}:#{Rails.application.secrets[:paypal_client_secret]}="
+        req.headers['Authorization'] = "Basic " + Base64.encode64("#{Rails.application.secrets[:paypal_client_id]}:#{Rails.application.secrets[:paypal_client_secret]}").rstrip.tr("\n", "")
+        req.headers['Accept'] = 'application/json'
+        req.headers['Accept-Language'] = "en_US"
         req.params['grant_type'] = "authorization_code"
         req.params['code'] = authorization_code
       end
+
+      access_token = JSON.parse(result.body)["access_token"]
+      refresh_token = JSON.parse(result.body)["refresh_token"]
+
+      user_info_response = Faraday.get("https://api.sandbox.paypal.com/v1/identity/oauth2/userinfo") do |req|
+        req.headers['Authorization'] = "Bearer #{access_token}"
+        req.headers['Accept'] = 'application/json'
+        req.params['schema'] = "paypalv1.1"
+      end
+
+      user_info = JSON.parse(user_info_response.body)
+      paypal_connection = PaypalConnection.find_or_initialize_by(
+        user_id: current_publisher.id
+      )
+      paypal_connection.update(
+        refresh_token: refresh_token,
+        email: user_info['email'],
+        country: user_info['country'],
+        verification_status: user_info['verification_status'],
+        paypal_account_id: user_info['paypal_account_id']
+      )
     end
   end
 end
