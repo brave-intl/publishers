1fd884838385de330ddd0f16a92459519eb7921f
Albert Wang <amwang217@gmail.com>
2019-11-12 16:42:17 -0800

    Disable UC

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -155,7 +155,7 @@ class PublishersController < ApplicationController
       uphold_connection.create_uphold_cards if uphold_connection.missing_card?
     end
 
-    flash[:notice] = "Payouts will be coming soon. Keep an eye on this space to connect your wallet." if current_publisher.japanese_locale?(params[:locale])
+    flash[:notice] = I18n.t("publishers.home.disabled_payouts") if current_publisher.japanese_locale?(params[:locale])
   end
 
   def choose_new_channel_type
