361363a14e445b841b18fb530ab374dcd7de2ea0
Albert Wang <amwang217@gmail.com>
2019-11-19 15:52:13 -0800

    Additional translation from token on landing page (#2391)
    ✨ Additional translation from token
    ✨ Updated translations on landing page
    ✨ Remove uphold reference for connected Japanese accounts
    ✨  Disable site banner for Japan until we get translations

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8afd802382e3670b174623cdb321c73a.js"
+  "/precache-manifest.db933928cdef075ba41a3c17fa2eaf7b.js"
 );
 
 self.addEventListener('message', (event) => {
