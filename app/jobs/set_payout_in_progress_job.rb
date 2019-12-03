be9e63b07e9f1f54c67108e73f20677ed9ae44e3
Albert Wang <amwang217@gmail.com>
2019-12-03 15:05:48 -0800

    Fix labels for payout in progress. Closes #2415 (#2421)
    * Fix UI. Closes #2415

--- app/jobs/set_payout_in_progress_job.rb
+++ app/jobs/set_payout_in_progress_job.rb
@@ -4,5 +4,6 @@ class SetPayoutInProgressJob < ApplicationJob
   def perform(set_to: true)
     Rails.cache.write('payout_in_progress', set_to)
     Rails.logger.info("Set payout in progress to #{set_to}")
+    Rails.cache.write('payout_report_generating', set_to)
   end
 end
