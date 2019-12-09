e74a89daa29a156197187fda61e1723f09883d66
Albert Wang <amwang217@gmail.com>
2019-12-09 12:14:28 -0800

    Remove similar validation to 'verified_duplicate_channels_must_be_contested'. Closes #2424

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -50,8 +50,6 @@ class Channel < ApplicationRecord
     in: %w(domain_not_found connection_failed too_many_redirects timeout no_txt_records token_incorrect_dns token_not_found_dns token_not_found_public_file no_https),
   }, allow_nil: true
 
-  validate :site_channel_details_brave_publisher_id_unique_for_publisher, if: -> { details_type == 'SiteChannelDetails' }
-
   validate :verified_duplicate_channels_must_be_contested, if: -> { verified? }
 
   after_commit :register_channel_for_promo, if: :should_register_channel_for_promo
@@ -308,16 +306,6 @@ class Channel < ApplicationRecord
     SlackMessenger.new(
       message: "#{emoji} *#{details.publication_title}* verified by owner #{publisher.owner_identifier}; id=#{details.channel_identifier}; url=#{details.url}"
     ).perform
-  end
-
-  def site_channel_details_brave_publisher_id_unique_for_publisher
-    duplicate_unverified_channels = publisher.channels.visible_site_channels.
-      where(site_channel_details: { brave_publisher_id: details.brave_publisher_id }).
-      where.not(id: id)
-
-    if duplicate_unverified_channels.any?
-      errors.add(:brave_publisher_id, "must be unique")
-    end
   end
 
   def preserve_contested_by_channels
