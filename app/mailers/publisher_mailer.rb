c469938833cb950a842b1c4c30c2435a6c32afad
Albert Wang <amwang217@gmail.com>
2020-01-13 16:51:38 -0800

    Include payout emails

--- app/mailers/publisher_mailer.rb
+++ app/mailers/publisher_mailer.rb
@@ -121,6 +121,14 @@ class PublisherMailer < ApplicationMailer
     )
   end
 
+  def paypal_missing_bank_account(publisher)
+    @publisher = publisher
+    mail(
+      to: @publisher.email,
+      subject: default_i18n_subject
+    )
+  end
+
   def uphold_kyc_incomplete(publisher, total_amount)
     @publisher = publisher
     @total_amount = total_amount
