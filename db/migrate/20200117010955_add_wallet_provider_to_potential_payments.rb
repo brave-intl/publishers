0376f6581578f6207853443a635ca7610038b2b5
Albert Wang <amwang217@gmail.com>
2020-01-17 09:53:07 -0800

    Fix schema

--- db/migrate/20200117010955_add_wallet_provider_to_potential_payments.rb
+++ db/migrate/20200117010955_add_wallet_provider_to_potential_payments.rb
@@ -1,17 +1,17 @@
 class AddWalletProviderToPotentialPayments < ActiveRecord::Migration[6.0]
   def up
     add_column :potential_payments, :wallet_provider_id, :string
-    # execute <<-SQL
-      # CREATE TYPE wallet_provider AS ENUM ('uphold', 'paypal');
-    # SQL
+    execute <<-SQL
+      CREATE TYPE wallet_provider AS ENUM ('uphold', 'paypal');
+    SQL
     add_column :potential_payments, :wallet_provider, :wallet_provider, default: "uphold"
   end
 
   def down
     execute <<-SQL
-      REMOVE TYPE wallet_provider;
+     DROP TYPE wallet_provider;
     SQL
-    # remove_column :potential_payments, :wallet_provider_id
-    # remove_column :potential_payments, :wallet_provider
+    remove_column :potential_payments, :wallet_provider
+    remove_column :potential_payments, :wallet_provider_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_001627) do
-ActiveRecord::Schema.define(version: 2020_01_08_065840) do
+ActiveRecord::Schema.define(version: 2020_01_17_010955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
