0d54791ef2104a65fabefe6a357eb12c2bb5de58
Cory McDonald <cmcdonald@brave.com>
2020-01-29 10:59:33 -0600

    Fix Sidekiq UI

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,3 +7,8 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: Rails.application.secrets[:redis_url], size: 40, network_timeout: 5 }
 end
+
+# Prevent user's sessions from being overwritten so you can be logged in and also have the Sidekiq UI open.
+# https://github.com/mperham/sidekiq/wiki/Monitoring#sessions-being-lost
+require 'sidekiq/web'
+Sidekiq::Web.set :sessions, false
