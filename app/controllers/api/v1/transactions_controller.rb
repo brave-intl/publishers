ef56e211f5e36fb08e766bb14cb9d9d47edcda89
Cory McDonald <cmcdonald@brave.com>
2020-02-05 15:19:40 -0600

    Use channel identifier instead

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::TransactionsController < Api::BaseController
   class GetTransactionError < StandardError; end
 
   def show
-    channel = get_merchant!(params[:merchant])
+    channel = get_merchant!(params[:channel])
     uphold_client = Uphold::Client.new(uphold_connection: channel.publisher.uphold_connection)
 
     render json: uphold_client.transaction.find(id: params[:id])
@@ -12,11 +12,13 @@ class Api::V1::TransactionsController < Api::BaseController
     render json: { errors: "Could not find the specified transaction under the publishers account" }, status: 400
   end
 
-  def get_merchant!(merchant)
-    channel = Channel.site_channels.verified.find_by(site_channel_details: { brave_publisher_id: merchant })
+  def get_merchant!(channel)
+    raise GetTransactionError.new("Missing channel query parameter.") if channel.blank?
 
-    raise GetTransactionError.new("Could not find merchant") if channel.blank?
-    raise GetTransactionError.new("Merchant does not have connected Uphold account") if channel.publisher&.uphold_connection.blank?
+    channel = Channel.find_by_channel_identifier(channel)
+
+    raise GetTransactionError.new("Could not find channel. Did you encode the identifier?") if channel.blank?
+    raise GetTransactionError.new("Channel does not have connected Uphold account") if channel.publisher&.uphold_connection.blank?
     channel
   end
 end
