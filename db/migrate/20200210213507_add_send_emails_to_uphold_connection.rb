06c67064f71662c3644df540cb66ddcaf433594f
Cory McDonald <cmcdonald@brave.com>
2020-02-21 11:38:37 -0600

    Remove unused code

--- db/migrate/20200210213507_add_send_emails_to_uphold_connection.rb
+++ db/migrate/20200210213507_add_send_emails_to_uphold_connection.rb
@@ -2,7 +2,4 @@ class AddSendEmailsToUpholdConnection < ActiveRecord::Migration[6.0]
   def change
     add_column :uphold_connections, :send_emails, :datetime, default: -> { 'CURRENT_TIMESTAMP' }
   end
-
-  # If send_emails is blank then the user has opted out
-  UpholdConnection.update(send_emails: DateTime.now)
 end
