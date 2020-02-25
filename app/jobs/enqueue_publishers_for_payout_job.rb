6e1ee652e17ec4c7fbf8d9ef1a4eccc54430a1e2
Albert Wang <amwang217@gmail.com>
2020-02-24 21:45:55 -0800

    Test fixes

--- app/jobs/enqueue_publishers_for_payout_job.rb
+++ app/jobs/enqueue_publishers_for_payout_job.rb
@@ -7,34 +7,61 @@ class EnqueuePublishersForPayoutJob < ApplicationJob
   def perform(should_send_notifications: false, final: true, manual: false, payout_report_id: "", publisher_ids: [], args: [])
     Rails.logger.info("Enqueuing publishers for payment.")
 
+    should_send_notifications = SEND_NOTIFICATIONS.in? args
     if payout_report_id.present?
       payout_report = PayoutReport.find(payout_report_id)
-    else
+    elsif !should_send_notifications
       payout_report = PayoutReport.create(final: final,
                                           manual: manual,
                                           fee_rate: fee_rate,
                                           expected_num_payments: 0)
     end
 
-    should_send_notifications = SEND_NOTIFICATIONS.in? args
+    if should_send_notifications
+      enqueue_emails_only(
+        manual: manual,
+        publisher_ids: publisher_ids
+      )
+    else
+      enqueue_payout(
+        manual: manual,
+        payout_report: payout_report,
+        publisher_ids: publisher_ids
+      )
+    end
+    payout_report if payout_report.present?
+  end
+
+  private
+
+  def enqueue_emails_only(manual:, publisher_ids:)
     Payout::UpholdJob.perform_later(
       manual: manual,
-      should_send_notifications: should_send_notifications,
+      should_send_notifications: true,
+      publisher_ids: publisher_ids
+    )
+    Payout::PaypalJob.perform_later(
+      should_send_notifications: true,
+    )
+    Payout::WireJob.perform_later(
+      should_send_notifications: true,
+      payout_report_id: payout_report.id
+    )
+  end
+
+  def enqueue_payout(payout_report:, manual:, publisher_ids:)
+    Payout::UpholdJob.perform_later(
+      manual: manual,
       payout_report_id: payout_report.id,
       publisher_ids: publisher_ids
     )
     Payout::PaypalJob.perform_later(
-      should_send_notifications: should_send_notifications,
       payout_report_id: payout_report.id
     )
     Payout::WireJob.perform_later(
       payout_report_id: payout_report.id
     )
-
-    payout_report
   end
-
-  private
 
   def fee_rate
     raise if Rails.application.secrets[:fee_rate].blank?
