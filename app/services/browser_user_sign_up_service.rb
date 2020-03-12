e89be6a1c9c844b8a9f8ab47a83d23ebb81381fc
Albert Wang <amwang217@gmail.com>
2020-03-12 16:55:19 -0700

    Bug fixes

--- app/services/browser_user_sign_up_service.rb
+++ app/services/browser_user_sign_up_service.rb
@@ -4,10 +4,10 @@ class BrowserUserSignUpService < BaseService
 
   def perform
     uphold_user = BrowserUser.create(role: Publisher::BROWSER_USER)
-    Promo::Models::OwnerRegistration.create(
-      publisher: uphold_user, 
+    PromoClient.owner_registration.create(
+      publisher: uphold_user,
       promo_campaign: PromoCampaign.find_by(name: PromoCampaign::PEER_TO_PEER).id,
-      peer_to_peer: true
+      is_peer_to_peer: true
     )
     uphold_user
   end
