3c54074f326e2390ed13e558df073d2c6bcac56d
Albert Wang <amwang217@gmail.com>
2020-03-17 14:24:37 -0700

    Partial migration change

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_02_10_213507) do
+ActiveRecord::Schema.define(version: 2020_03_17_211000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -455,8 +456,8 @@ ActiveRecord::Schema.define(version: 2020_02_10_213507) do
     t.uuid "publisher_id", null: false
     t.text "title", null: false
     t.text "description", null: false
-    t.integer "donation_amounts", null: false, array: true
-    t.integer "default_donation", null: false
+    t.integer "donation_amounts", array: true
+    t.integer "default_donation"
     t.json "social_links"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
