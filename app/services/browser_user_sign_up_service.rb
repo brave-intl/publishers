72e42515153ec1465f32d2e5b62309d329a875b2
Albert Wang <amwang217@gmail.com>
2020-04-07 19:08:43 -0700

    Use a separate route for peer-to-peer (#2611)
    * Use a separate route for peer-to-peer

--- app/services/browser_user_sign_up_service.rb
+++ app/services/browser_user_sign_up_service.rb
@@ -4,10 +4,9 @@ class BrowserUserSignUpService < BaseService
 
   def perform
     uphold_user = BrowserUser.create(role: Publisher::BROWSER_USER)
-    PromoClient.owner_registration.create(
+    PromoClient.peer_to_peer_registration.create(
       publisher: uphold_user,
-      promo_campaign: PromoCampaign.find_by(name: PromoCampaign::PEER_TO_PEER).id,
-      is_peer_to_peer: true
+      promo_campaign: PromoCampaign.find_by(name: PromoCampaign::PEER_TO_PEER).id
     )
     uphold_user
   end
