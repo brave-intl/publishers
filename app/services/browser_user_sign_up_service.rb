3c1517c22bdfdec029e0e69095fd33ec4c9a17a0
Albert Wang <amwang217@gmail.com>
2020-04-07 20:30:42 -0700

    Fix id for promo_campaign lookup

--- app/services/browser_user_sign_up_service.rb
+++ app/services/browser_user_sign_up_service.rb
@@ -6,7 +6,7 @@ class BrowserUserSignUpService < BaseService
     uphold_user = BrowserUser.create(role: Publisher::BROWSER_USER)
     PromoClient.peer_to_peer_registration.create(
       publisher: uphold_user,
-      promo_campaign: PromoCampaign.find_by(name: PromoCampaign::PEER_TO_PEER).id
+      promo_campaign: PromoCampaign.find_by(name: PromoCampaign::PEER_TO_PEER)
     )
     uphold_user
   end
