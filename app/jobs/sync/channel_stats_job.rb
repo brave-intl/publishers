a27a2f518ec564fe68d191a8115f711076b66bd0
Albert Wang <amwang217@gmail.com>
2020-04-16 14:31:14 -0700

    We're hitting the API limit on youtube. Closes #2636.

--- app/jobs/sync/channel_stats_job.rb
+++ app/jobs/sync/channel_stats_job.rb
@@ -3,10 +3,6 @@ class Sync::ChannelStatsJob < ApplicationJob
   queue_as :low
 
   def perform
-    YoutubeChannelDetails.find_each do |youtube_channel_details|
-      ChannelStatsServices::YoutubeChannelStatsService.new(youtube_channel_details: youtube_channel_details).perform
-    end
-
     TwitchChannelDetails.find_in_batches(batch_size: 30).with_index do |group, index|
       Sync::TwitchStatsJob.set(wait: index.minutes).perform_later(ids: group.map(&:id))
     end
