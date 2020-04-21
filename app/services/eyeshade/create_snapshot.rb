553aa78fad470e2b40ac96e3d8568c058b4a63f2
Albert Wang <amwang217@gmail.com>
2020-04-21 13:42:45 -0700

    Include refactor comments

--- app/services/eyeshade/create_snapshot.rb
+++ app/services/eyeshade/create_snapshot.rb
@@ -1,22 +1,19 @@
 class Eyeshade::CreateSnapshot < Eyeshade::BaseApiClient
   def perform(payout_report_id:)
-    if Rails.application.secrets[:api_eyeshade_offline]
-      result = perform_offline
-    else
-      begin
-        response = connection.post do |request|
-          request.headers["Authorization"] = api_authorization_header
-          request.url("/v1/snapshots/#{payout_report_id}")
-        end
-        result = JSON.parse(response.body)
+    return perform_offline if Rails.application.secrets[:api_eyeshade_offline]
+    begin
+      response = connection.post do |request|
+        request.headers["Authorization"] = api_authorization_header
+        request.url("/v1/snapshots/#{payout_report_id}")
       end
+      result = JSON.parse(response.body)
     end
     result
   end
 
   def perform_offline
     {
-      "id":"9e49faee-eeb5-48f4-ace8-97ad01f92773",
+      "id":"9e49faee-eeb5-48f4-ace8-97ad01f92773"
     }
   end
 end
