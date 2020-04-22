e0ee9e9489f48af570b0c5f3c670b11fce906ace
Albert Wang <amwang217@gmail.com>
2020-04-22 09:39:05 -0700

    use extension

--- app/jobs/cache/browser_channels/prefix_list.rb
+++ app/jobs/cache/browser_channels/prefix_list.rb
@@ -4,6 +4,7 @@ class Cache::BrowserChannels::PrefixList
   # Might need to adjust the value based on 
   PREFIX_LENGTH = 9
   ALL_CHANNELS_KEY = "all_channels"
+  EXTENSION = ".br"
 
   def perform(details_type: nil)
     result = ActiveRecord::Base.connection.execute("SELECT SUBSTRING(sha2_base16, 1, #{PREFIX_LENGTH}) FROM site_banner_lookups").map { |r| r['substring'] }.to_json
@@ -16,7 +17,7 @@ class Cache::BrowserChannels::PrefixList
     require 'aws-sdk-s3'
     Aws.config[:credentials] = Aws::Credentials.new(Rails.application.secrets[:s3_rewards_access_key_id], Rails.application.secrets[:s3_rewards_secret_access_key])
     s3 = Aws::S3::Resource.new(region: Rails.application.secrets[:s3_rewards_bucket_region])
-    obj = s3.bucket(Rails.application.secrets[:s3_rewards_bucket_name]).object(ALL_CHANNELS_KEY)
+    obj = s3.bucket(Rails.application.secrets[:s3_rewards_bucket_name]).object(ALL_CHANNELS_KEY + EXTENSION)
     obj.upload_file(temp_file.path)
   end
 end
