286d4d6f4cf2a1a7bf8e7b94a57b73049293e91b
Albert Wang <amwang217@gmail.com>
2020-04-23 16:02:38 -0700

    Include payout_report_id as param for post (#2653)

--- app/services/eyeshade/create_snapshot.rb
+++ app/services/eyeshade/create_snapshot.rb
@@ -4,7 +4,8 @@ class Eyeshade::CreateSnapshot < Eyeshade::BaseApiClient
     begin
       response = connection.post do |request|
         request.headers["Authorization"] = api_authorization_header
-        request.url("/v1/snapshots/#{payout_report_id}")
+        request.url("/v1/snapshots")
+        request.body = { "snapshotId" => payout_report_id }.to_json
       end
       result = JSON.parse(response.body)
     rescue
