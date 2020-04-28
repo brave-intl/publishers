a67e7032c3fe407b68900219e1b0e206cff5577e
Albert Wang <amwang217@gmail.com>
2020-04-28 11:21:28 -0700

    Remove deadcode for serializers

--- app/serializers/site_channel_details_serializer.rb
+++
@@ -1,3 +0,0 @@
-class SiteChannelDetailsSerializer < BaseChannelDetailsSerializer
-
-end
--- app/serializers/youtube_channel_details_serializer.rb
+++
@@ -1,9 +0,0 @@
-class YoutubeChannelDetailsSerializer < BaseChannelDetailsSerializer
-
-  attributes :youtube_channel_id, :auth_provider, :auth_user_id, :auth_email, :auth_name, :title, :description,
-             :thumbnail_url, :subscriber_count
-
-  def method
-    object.auth_provider
-  end
-end
