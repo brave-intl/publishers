1a9359738716fc50e3049cfba850a9be3385aa22
Albert Wang <amwang217@gmail.com>
2020-05-13 10:47:27 -0700

    Rails gem update (#2699)
    * Rack attack update
    * Updated assets

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -40,6 +40,19 @@ class Rack::Attack
     req.ip if !req.path.start_with?("/assets")
   end
 
+  blocklist('fail2ban pentesters') do |req|
+    # `filter` returns truthy value if request fails, or if it's from a previously banned IP
+    # so the request is blocked
+    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 1.hour, bantime: 7.days) do
+      # The count for the IP is incremented if the return value is truthy
+      CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
+      req.path.include?('/etc/passwd') ||
+      req.path.include?('wp-admin') ||
+      req.path.include?('wp-login')
+
+    end
+  end
+
   ### Prevent Brute-Force Login Attacks ###
 
   # The most common brute-force login attack is a brute-force password
