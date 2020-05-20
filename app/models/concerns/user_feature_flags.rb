8e928ffe03f933871db6c1f461f2b9b8893d049e
Cory McDonald <cmcdonald@brave.com>
2020-05-20 11:26:02 -0500

    WIP On flags

--- app/models/concerns/user_feature_flags.rb
+++ app/models/concerns/user_feature_flags.rb
@@ -2,15 +2,30 @@ module UserFeatureFlags
   extend ActiveSupport::Concern
   WIRE_ONLY = :wire_only
   INVOICE = :invoice
-  REFERRAL_KYC_REQUIRED = :REFERRAL_KYC_REQUIRED
   MERCHANT = :merchant
 
-  VALID_FEATURE_FLAGS = [WIRE_ONLY, INVOICE, MERCHANT].freeze
+  # This flag will be set to "true" for all new publishers.
+  # It enforces KYC to be present in order to create a new promo code
+  REFERRAL_KYC_REQUIRED = :REFERRAL_KYC_REQUIRED
+
+  VALID_FEATURE_FLAGS = [
+    WIRE_ONLY,
+    INVOICE,
+    MERCHANT,
+    REFERRAL_KYC_REQUIRED
+  ].freeze
 
   included do
-    scope :wire_only, -> { where(feature_flags: { WIRE_ONLY => true }) }
-    scope :invoice,   -> { where(feature_flags: { INVOICE => true }) }
-    scope :merchant,  -> { where(feature_flags: { MERCHANT => true }) }
+    # Define scopes for each of the feature flags.
+    # The result is that consumers can filter down publishers to a specific feature flag
+    #
+    # Example:
+    #   Publisher.wire_only
+    #
+    # This scope would return all the publishers who have that flag enabled.
+    VALID_FEATURE_FLAGS.each do |flag|
+      scope flag, -> { where(feature_flags: { flag => true}) }
+    end
   end
 
   def update_feature_flags_from_form(update_flag_params)
@@ -27,15 +42,17 @@ module UserFeatureFlags
     save!
   end
 
-  def wire_only?
-    feature_flags.symbolize_keys[WIRE_ONLY].present?
-  end
-
-  def invoice?
-    feature_flags.symbolize_keys[INVOICE].present?
-  end
-
-  def merchant?
-    feature_flags.symbolize_keys[MERCHANT].present?
+  # This helper methods for each of the feature flags
+  # For example this generates a following method
+  #
+  #   def wire_only?
+  #     feature_flags.symbolize_keys[WIRE_ONLY].present?
+  #   end
+  #
+  # This is useful for methods that are checking if a feature is enbaled for a specific user.
+  VALID_FEATURE_FLAGS.each do |flag|
+    define_method :"#{flag}?" do
+      feature_flags.symbolize_keys[flag].present?
+    end
   end
 end
