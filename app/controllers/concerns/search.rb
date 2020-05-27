94be0e9b9619cfb1439b5894447f3cfe36c2f568
Albert Wang <amwang217@gmail.com>
2020-05-27 11:02:20 -0700

    Don't use full table scan against different tables. This might not be the conclusive fix though since search test still needs to be done. (#2725)

--- app/controllers/concerns/search.rb
+++ app/controllers/concerns/search.rb
@@ -27,11 +27,11 @@ module Search
 
     # Simple optimization to only search for the things that the admins search for the most
     if is_email?(search_query)
-      results = publishers.where('email ILIKE ?', "%#{search_query}%")
+      results = publishers.where('email ILIKE ?', "#{search_query}%")
     elsif is_promo_code?(search_query)
       results = publishers.left_joins(:channels).joins(channels: :promo_registration).where(promo_registrations: { referral_code: search_query.upcase })
     else
-      search_query = "%#{search_query}%" unless is_a_uuid?(search_query)
+      search_query = "#{search_query}%" unless is_a_uuid?(search_query)
       results = publishers.where(search_sql, search_query: search_query)
     end
 
