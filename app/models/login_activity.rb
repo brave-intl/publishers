f0ad21e693c8e86284dfbdd31ad504b3ba5a443a
Cory McDonald <cmcdonald@brave.com>
2020-06-08 17:30:51 -0500

    Lock down Gem versions

--- app/models/login_activity.rb
+++ app/models/login_activity.rb
@@ -2,8 +2,4 @@ class LoginActivity < ApplicationRecord
   belongs_to :publisher
 
   default_scope { order(created_at: :asc) }
-
-  def browser
-    Browser.new(user_agent, accept_language: accept_language)
-  end
 end
