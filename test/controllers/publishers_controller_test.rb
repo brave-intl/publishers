80fa9b61b89b8165425eb76390e329e0a1e1f172
Cory McDonald <cmcdonald@brave.com>
2020-06-22 15:04:49 -0500

    Add some test helpers :)

--- test/controllers/publishers_controller_test.rb
+++ test/controllers/publishers_controller_test.rb
@@ -8,12 +8,11 @@ class PublishersControllerTest < ActionDispatch::IntegrationTest
   include MailerTestHelper
   include PublishersHelper
   include EyeshadeHelper
+  include MockUpholdResponses
 
   before do
     @prev_eyeshade_offline = Rails.application.secrets[:api_eyeshade_offline]
-    stub_request(:get, /cards\?q/).to_return(body: [].to_json)
-    stub_request(:get, /v0\/me\/cards/).to_return(body: '{}')
-    stub_request(:post, Rails.application.secrets[:uphold_api_uri] + "/v0/me/cards").to_return(body: {id: '123e4567-e89b-12d3-a456-426655440000'}.to_json)
+    stub_uphold_cards!
   end
 
   after do
