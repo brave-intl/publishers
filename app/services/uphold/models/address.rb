e2aea861962becc258f214329fdbbca2830bf7a9
Cory McDonald <cmcdonald@brave.com>
2020-06-29 22:31:52 -0500

    Fix api_base_uri

--- app/services/uphold/models/address.rb
+++ app/services/uphold/models/address.rb
@@ -11,7 +11,7 @@ module Uphold
       # https://www.rfc-editor.org/rfc/rfc6570.txt
       PATH = Addressable::Template.new("/v0/me/cards/{id}/addresses")
 
-      attr_accessor :formats, :type
+      attr_accessor :formats, :type, :api_base_uri
 
       def initialize(params = {})
         super
