c09502c32e1800bee40ebdf4678a11c7ae64a016
Cory McDonald <cmcdonald@brave.com>
2020-07-13 19:47:55 -0500

    Fix UTC test breakage (#2812)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.time_zone = "UTC"
+  config.active_record.default_timezone = :utc
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
