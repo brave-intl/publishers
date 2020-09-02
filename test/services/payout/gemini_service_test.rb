13a2fa068fbf94b47cef821d52db32c72e91cd2a
Cory McDonald <cmcdonald@brave.com>
2020-09-02 14:19:54 -0400

    Resolve not null issue

---
+++ test/services/payout/gemini_service_test.rb
@@ -0,0 +1,87 @@
+require "test_helper"
+require "webmock/minitest"
+
+class GeminiServiceTest < ActiveJob::TestCase
+  include MockGeminiResponses
+  let(:payout_report) { PayoutReport.create(expected_num_payments: PayoutReport.expected_num_payments(Publisher.all)) }
+
+  let(:subject) do
+    perform_enqueued_jobs do
+      Payout::GeminiService.new(
+        payout_report: payout_report,
+        publisher: publisher,
+        should_send_notifications: true
+      ).perform
+    end
+  end
+
+  before do
+    ActionMailer::Base.deliveries.clear
+    PotentialPayment.destroy_all
+  end
+
+  describe "when publisher does not have a verified channel" do
+    let(:publisher) { publishers(:fake1) }
+
+    before { subject }
+
+    it "does not generate a report" do
+      assert_equal 0, PotentialPayment.count
+    end
+  end
+
+  describe "when publisher is suspended" do
+    let(:publisher) { publishers(:gemini_suspended) }
+
+    before { subject }
+
+    it 'marks the potential payment as suspended' do
+      PotentialPayment.all.each { |pp| assert_equal "suspended", pp.status }
+    end
+  end
+
+  describe 'when a gemini connection is not verified' do
+    let(:publisher) { publishers(:gemini_not_completed) }
+
+    before do
+      mock_gemini_unverified_account_request!
+      subject
+    end
+
+    it 'creates potential payments for the right publisher' do
+      refute_equal PotentialPayment.count, 0
+      PotentialPayment.all.each { |pp| assert_equal publisher.id, pp.publisher_id }
+    end
+
+    it 'has all payments marked as not verified' do
+      refute PotentialPayment.all.any? { |pp| pp.gemini_is_verified }
+    end
+
+    it 'the address is empty' do
+      refute PotentialPayment.all.any? { |pp| pp.address.present? }
+    end
+  end
+
+  describe 'when a gemini connection is not verified' do
+    let(:publisher) { publishers(:gemini_not_completed) }
+
+    before do
+      mock_gemini_account_request!
+      mock_gemini_recipient_id!
+      subject
+    end
+
+    it 'creates potential payments for the right publisher' do
+      refute_equal PotentialPayment.count, 0
+      PotentialPayment.all.each { |pp| assert_equal publisher.id, pp.publisher_id }
+    end
+
+    it 'has all payments marked as verified' do
+      PotentialPayment.all.each { |pp| assert pp.gemini_is_verified }
+    end
+
+    it 'the address is the recipient id' do
+      PotentialPayment.all.each { |pp| assert_equal pp.wallet_provider_id, '5f0cdc2f-622b-4c30-ad9f-3a5e6dc85079' }
+    end
+  end
+end
--- test/test_helpers/mock_gemini_responses.rb
+++ test/test_helpers/mock_gemini_responses.rb
@@ -40,6 +40,31 @@ module MockGeminiResponses
     stub_request(:post, regex).to_return(body: response.to_json)
   end
 
+  def mock_gemini_unverified_account_request!
+    path = Gemini::Account::PATH.expand(segments: nil).to_s
+    regex = Regexp.new(path)
+
+    response = {
+      "account": {
+        "accountName": "Primary",
+        "shortName": "primary",
+        "type": "exchange",
+        "created": "1594238561617",
+      },
+      "users": [
+        {
+          "name": "Alice Publisher",
+          "lastSignIn": "2020-07-20T21:16:39.563Z",
+          "status": "Active",
+          "countryCode": "US",
+          "isVerified": false,
+        },
+      ],
+      "memo_reference_code": "GEMMQDMPJ",
+    }
+    stub_request(:post, regex).to_return(body: response.to_json)
+  end
+
   def mock_gemini_recipient_id!
     path = Gemini::RecipientId::PATH
     regex = Regexp.new(path)
