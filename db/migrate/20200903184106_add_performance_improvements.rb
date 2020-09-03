abcbedab7621a6195fce352fbd2658280b9517d3
Cory McDonald <cmcdonald@brave.com>
2020-09-03 17:15:27 -0400

    Minor performance improvements (#2873)

---
+++ db/migrate/20200903184106_add_performance_improvements.rb
@@ -0,0 +1,9 @@
+class AddPerformanceImprovements < ActiveRecord::Migration[6.0]
+  def change
+    # Adding a gin index based off it's performance for text searches.
+    # https://www.cybertec-postgresql.com/en/postgresql-more-performance-for-like-and-ilike-statements/
+    enable_extension "btree_gin"
+    add_index :site_banner_lookups, :sha2_base16, using: :gin, name: 'index_gin_site_banner_lookups_on_sha2_base16'
+
+  end
+end
