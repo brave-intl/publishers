cffb26d495107c8c02a51f4370b73670e12d2596
Cory McDonald <cmcdonald@brave.com>
2020-10-02 10:52:37 -0500

    SiteBannerLookup reads from other database

--- app/models/site_banner_lookup.rb
+++ app/models/site_banner_lookup.rb
@@ -1,6 +1,9 @@
 class SiteBannerLookup < ActiveRecord::Base
   belongs_to :channel
   belongs_to :publisher
+
+  connects_to database: { writing: :primary, reading: :secondary }
+
   def set_sha2_base16
     self.sha2_base16 = Digest::SHA2.hexdigest(channel_identifier)
   end
