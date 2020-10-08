b686d797c87078f66e49cb004384c8a51008389c
Cory McDonald <cmcdonald@brave.com>
2020-10-08 13:04:00 -0500

    Add test environment to if checks

--- app/controllers/publishers/promo_registrations_controller.rb
+++ app/controllers/publishers/promo_registrations_controller.rb
@@ -22,7 +22,7 @@ class Publishers::PromoRegistrationsController < ApplicationController
   def index
     @publisher = current_publisher
     @promo_enabled_channels = @publisher.channels.joins(:promo_registration)
-    if Rails.env.development?
+    if Rails.env.development? || Rails.env.test?
       @publisher_promo_status = @publisher.promo_status(promo_running?)
     else
       @publisher_promo_status = :over
@@ -30,7 +30,7 @@ class Publishers::PromoRegistrationsController < ApplicationController
   end
 
   def create
-    return unless Rails.env.development?
+    return unless Rails.env.development? || Rails.env.test?
     @publisher = current_publisher
     @publisher.promo_enabled_2018q1 = true
     @publisher.save!
