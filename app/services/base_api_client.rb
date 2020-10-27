08ff27ec133de7cfbb785a158a218fa545a423a1
Michael McLaughlin <mmclaughlin@brave.com>
2020-10-27 14:56:23 -0500

    fixed check

--- app/services/base_api_client.rb
+++ app/services/base_api_client.rb
@@ -76,9 +76,8 @@ class BaseApiClient < BaseService
     @connection ||= begin
       require "faraday"
       Faraday.new(url: api_base_uri) do |faraday|
-        proxy = proxy_url if proxy_url.present?
-        if !proxy.nil? && !proxy.include?("eyeshade")
-          faraday.proxy = proxy
+        if !api_base_uri.nil? && !api_base_uri.include?("eyeshade")
+          faraday.proxy = proxy_url if proxy_url.present?
         end
         faraday.request :retry, max: retry_count, interval: 0.05, interval_randomness: 0.5, backoff_factor: 2
 
