7a2226c4f38166404a887d5351cdafcf62630015
Albert Wang <amwang217@gmail.com>
2020-11-25 12:26:47 -0800

    Feature flag that will enable top publishers to create referrals

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -298,7 +298,10 @@ class Channel < ApplicationRecord
   private
 
   def should_register_channel_for_promo
-    false
+    return false unless publisher.may_create_referrals? && publisher.may_register_promo?
+    promo_running = Rails.application.secrets[:active_promo_id].present? # Could use PromosHelper#active_promo_id
+    publisher_enabled_promo = publisher.promo_enabled_2018q1?
+    promo_running && publisher_enabled_promo && saved_change_to_verified? && verified && !publisher.only_user_funds?
   end
 
   def clear_verified_at_if_necessary
