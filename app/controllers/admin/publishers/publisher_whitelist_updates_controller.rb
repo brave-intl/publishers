c288cf62d80ac243fe0ceffb550949f7e603e288
Nick von Pentz <nick@brave.com>
2020-12-15 15:50:21 -0700

    Update app/controllers/admin/publishers/publisher_whitelist_updates_controller.rb
    Co-authored-by: 1nezer0 <67481302+1nezer0@users.noreply.github.com>

--- app/controllers/admin/publishers/publisher_whitelist_updates_controller.rb
+++ app/controllers/admin/publishers/publisher_whitelist_updates_controller.rb
@@ -1,7 +1,7 @@
 class Admin::Publishers::PublisherWhitelistUpdatesController < Admin::PublishersController
   def create
     note = @publisher.notes.create!(note: params[:note], created_by_id: current_publisher.id)
-    PublisherWhitelistUpdate.create!(publisher: @publisher, enabled: params[:enable], publisher_note: note)
+    PublisherWhitelistUpdate.create!(publisher: @publisher, enabled: params[:enable].present?, publisher_note: note)
     @publisher.reload
 
     flash[:notice] = "Added publisher to whitelist."
