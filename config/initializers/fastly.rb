5017ae81078cbdb74631ccbb214e8bd327db42f7
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2021-01-12 15:18:57 -0600

    Converted to cloudfront (#3000)
    * Access base url from anywhere (#2470) (#2472)
    
    🐛 Use base url for paypal redirect
    
    * Remove requests to prevent excess data use (#2689) (#2691)
    
    🐎  Remove requests to prevent excess data use
    
    * Release 2020-05-12 (2) (#2696)
    
    * Fix Statement Bugs (#2690)
    
    * Have warning for the quota (#2692)
    
    Co-authored-by: Albert Wang <amwang217@gmail.com>
    
    * Handle cases where there isn't an uphold_connection for a channel (#2789) (#2790)
    
    * Show CSV in a more clean way (#2982)
    
    Co-authored-by: Albert Wang <amwang217@gmail.com>
    
    * Fix the columns being selected
    
    * updated ip check to cloudfront
    
    * fixed config key
    
    * fixed logs
    
    Co-authored-by: Albert Wang <amwang217@gmail.com>
    Co-authored-by: Cory McDonald <cmcdonald@brave.com>
    Co-authored-by: Cory McDonald <cory@corywmcdonald.com>
    Co-authored-by: eV (㋎) <8796196+evq@users.noreply.github.com>
    Co-authored-by: Nick von Pentz <nick@brave.com>
    Co-authored-by: Albert Wang <=>

--- config/initializers/fastly.rb
+++ config/initializers/fastly.rb
@@ -1,13 +1,13 @@
-FASTLY_API = Rails.application.config.services.fastly
+CLOUDFRONT_API = Rails.application.config.services.cloudfront
 proxy_list = []
 
 begin
-  puts "⬇️  Pulling trusted_proxies list from Fastly"
-  response = Faraday.get(FASTLY_API)
+  puts "⬇️  Pulling trusted_proxies list from Cloudfront"
+  response = Faraday.get(CLOUDFRONT_API)
   list = JSON.parse(response.body)
-  proxy_list = list.dig("addresses")
+  proxy_list = list.dig("CLOUDFRONT_GLOBAL_IP_LIST")
 rescue StandardError => e
-  puts "Failed to load trusted_proxies from Fastly. Error: #{e.message}"
+  puts "Failed to load trusted_proxies from Cloudfront. Error: #{e.message}"
 end
 
 Rails.application.config.action_dispatch.trusted_proxies = proxy_list.map { |proxy| IPAddr.new(proxy) }
