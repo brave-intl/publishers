ed891ddc850ec249c004409e8650cb9476a4463a
Yachtcaptain23 <amwang217@gmail.com>
2021-01-21 09:56:23 -0800

    1. Add connection pooling option to Publishers App. One can use a (#3008)
    connection object like the following:
    
    REDIS.with do |conn|
      conn.set('a', 'b')
    end
    
    result = REDIS.with do |conn|
      conn.get('a')
    end
    
    2. Also change the health_checks_controller by getting rid of the
    migration checks, as it seems that if a migration is occurring, then
    Kubernetes pods will be thrown into a boot loop.
    
    Closes https://github.com/brave-intl/publishers/issues/3006
    
    Co-authored-by: Albert Wang <=>

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -15,9 +15,6 @@ class HealthChecksController < ActionController::Base
     [
       { name: "cache", healthy: cache_connected? },
       { name: "database", healthy: database_connected? },
-      { name: "migrations", healthy: database_migrations_updated? },
-      { name: "sidekiq", healthy: sidekiq_connected? },
-      { name: "sidekiq_workers", healthy: sidekiq_workers?, optional: true },
     ]
   end
 
@@ -26,7 +23,9 @@ class HealthChecksController < ActionController::Base
   end
 
   def cache_connected?
-    Rails.cache.redis.ping == "PONG"
+    REDIS.with do |conn|
+      conn.ping == "PONG"
+    end
   rescue StandardError
     false
   end
@@ -34,30 +33,6 @@ class HealthChecksController < ActionController::Base
   def database_connected?
     ApplicationRecord.connection
     ApplicationRecord.connected?
-  rescue
-    false
-  end
-
-  def database_migrations_updated?
-    return false unless database_connected?
-
-    !ApplicationRecord.connection.migration_context.needs_migration?
-  end
-
-  def sidekiq_connected?
-    Sidekiq.redis do |r|
-      res = r.ping
-      res == 'PONG'
-    end
-  rescue
-    false
-  end
-
-  def sidekiq_workers?
-    return false unless sidekiq_connected?
-
-    ps = Sidekiq::ProcessSet.new
-    ps.size.positive?
   rescue
     false
   end
