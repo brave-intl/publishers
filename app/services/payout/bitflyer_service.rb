d963feb1397d7245aa065e2c332b63450811d716
Dan A. Lipeles <danlipeles@icloud.com>
2021-02-17 22:22:17 -0500

    Bitflyer integration (#3044)
    * Initial Commit
    
    * Add Protobuf definitions for bitFlyer
    
    * Add feature flags, UI work, env variables, etc
    
    * Use creators host env var
    
    * Update routes, fix tests
    
    * Fix Tests
    
    * Add bitflyer scope
    
    * Fix rubocop error, add procfile back for local testing
    
    * Update routes
    
    * Add deposit_id per channel, various fixes and cleanup
    
    * Add procfile to gitignore
    
    * Delete Procfile.dev
    
    * Update channel response
    
    * Add enum for bitFlyer Wallet
    
    * Add bitflyer type to protobuf defs
    
    * Add bf wallet proto
    
    * Add remaining proto def
    
    * Safeguard for uphold connection
    
    * Add NR monitoring
    
    * Add JP translations & logic for bitFlyer
    
    * Update japanese translations
    
    * Update connect to bitFlyer button translation
    
    * Add default ja locale for bitFlyer
    
    * Remove gsub from bF controller
    
    * Add bitflyer to payouts

---
+++ app/services/payout/bitflyer_service.rb
@@ -0,0 +1,60 @@
+module Payout
+  class BitflyerService < Service
+    def perform
+      # Writes a message if we should skip a publisher or not.
+      # Implemented in Payout::Service
+      return if skip_publisher?
+
+      potential_payments = []
+
+      connection = @publisher.bitflyer_connection
+      # Sync the connection
+      connection.sync_connection!
+
+      potential_payments << PotentialPayment.new(
+        payout_report_id: @payout_report&.id,
+        name: @publisher.name,
+        amount: "0",
+        fees: "0",
+        publisher_id: @publisher.id,
+        kind: ::PotentialPayment::REFERRAL,
+        bitflyer_is_verified: true,
+        address: connection.recipient_id || '',
+        wallet_provider_id: connection.recipient_id,
+        wallet_provider: ::PotentialPayment.wallet_providers['bitflyer'],
+        suspended: @publisher.suspended?,
+        status: @publisher.last_status_update&.status
+      )
+
+      @publisher.channels.verified.each do |channel|
+        potential_payments << PotentialPayment.new(
+          payout_report_id: @payout_report&.id,
+          name: "#{channel.publication_title}",
+          amount: "0",
+          fees: "0",
+          publisher_id: @publisher.id,
+          channel_id: channel.id,
+          kind: ::PotentialPayment::CONTRIBUTION,
+          url: "#{channel.details.url}",
+          address: connection.recipient_id || '',
+          bitflyer_is_verified: true,
+          wallet_provider_id: connection.recipient_id,
+          wallet_provider: ::PotentialPayment.wallet_providers['bitflyer'],
+          suspended: @publisher.suspended?,
+          status: @publisher.last_status_update&.status,
+          channel_stats: channel.details.stats,
+          channel_type: channel.details_type
+        )
+      end
+
+      unless should_only_notify?
+        potential_payments.each do |payment|
+          unless payment.save
+            # If the payment couldn't save then we created a PayoutMessage
+            create_message("Could not save the potential_payment: #{payment.errors&.full_messages&.join(', ')}")
+          end
+        end
+      end
+    end
+  end
+  end
