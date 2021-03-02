99240f120df6bfe26ba2f00d87867538593eccfa
Dan A. Lipeles <danlipeles@icloud.com>
2021-03-02 16:17:34 -0500

    Update bF deposit id on channel transfer

--- app/jobs/channels/approve_channel_transfer_job.rb
+++ app/jobs/channels/approve_channel_transfer_job.rb
@@ -7,9 +7,27 @@ module Channels
 
       ActiveRecord::Base.transaction do
         contested_by = channel.contested_by_channel
+
+        ## There is a helper function in channel.rb that can help minimize this code
+        ## Need to generate new bF deposit ID on channel transfer:
+        channel.deposit_id = nil
+        if contested_by.publisher.selected_wallet_provider_type == BITFLYER_CONNECTION
+          # Request a deposit id from bitFlyer.
+          url = URI.parse(Rails.application.secrets[:bitflyer_host] + '/api/link/v1/account/create-deposit-id?request_id=' + SecureRandom.uuid)
+          request = Net::HTTP::Get.new(url.to_s)
+          request['Authorization'] = "Bearer " + contested_by.publisher.bitflyer_connection.access_token
+          response = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') do |http|
+            http.request(request)
+          end
+          deposit_id = JSON.parse(response.body)["deposit_id"]
+          contested_by.update(deposit_id: deposit_id)
+        end
+        ###################################################################
+
         channel.contested_by_channel_id = nil
         channel.contest_token = nil
         channel.contest_timesout_at = nil
+
         channel.save!
 
         original_owner_email = channel.publisher.email
