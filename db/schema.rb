02f42f84b67e42ed41901d46d8d5e9f3c05f1abd
Dan A. Lipeles <danlipeles@icloud.com>
2021-03-11 19:01:10 -0500

    Revert "Revert "Release 2021-03-10""

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_001133) do
+ActiveRecord::Schema.define(version: 2021_02_27_024418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
