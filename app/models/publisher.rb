ff06a3fe99317dfa9188e0a1f8b999253b8d2faa
Albert Wang <=>
2021-03-18 15:12:18 -0700

    Fix Rubocop

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -339,7 +339,7 @@ class Publisher < ApplicationRecord
 
   def last_supported_login_locale
     # If we update here, we should also update RegistrationsController.locale_from_header
-    locale = self.login_activities.order(created_at: :desc).limit(1).first.accept_language.first(2)
+    locale = login_activities.order(created_at: :desc).limit(1).first.accept_language.first(2)
     if locale == "ja"
       :ja
     else
