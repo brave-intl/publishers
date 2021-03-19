ba420f8df1d4a0f605b03930d776fd039a728b32
Albert Wang <=>
2021-03-18 19:27:04 -0700

    Immediately sync the verified channels with the server

--- app/models/site_banner_lookup.rb
+++ app/models/site_banner_lookup.rb
@@ -7,4 +7,9 @@ class SiteBannerLookup < ActiveRecord::Base
   def set_sha2_base16
     self.sha2_base16 = Digest::SHA2.hexdigest(channel_identifier)
   end
+
+  # Syncs the first X nibbles, and RESPONSES_PREFIX_LENGTH refers to bytes, so we have to multiply by 2
+  def sync!
+    Cache::BrowserChannels::ResponsesForPrefix.perform_async(sha2_base16.first(RESPONSES_PREFIX_LENGTH * 2))
+  end
 end
