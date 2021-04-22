a6765a2a78c6bfa479d727bdbc0b9dffcfe49471
Tyler Smart <tsmart@brave.com>
2021-04-22 16:00:22 -0700

    Spelling fix

--- app/controllers/publishers/registrations_controller.rb
+++ app/controllers/publishers/registrations_controller.rb
@@ -98,8 +98,8 @@ module Publishers
     private
 
     def locale_from_header
-      accept_langugage = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-      case accept_langugage
+      accept_language = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+      case accept_language
       when 'ja'
         :ja
       when 'jabap'
