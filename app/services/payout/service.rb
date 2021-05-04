cdab3cee537dcf4148e485e77d05f2a65da8f556
Tyler Smart <tsmart@brave.com>
2021-05-04 10:46:26 -0700

    Removing unneeded wallet checks

--- app/services/payout/service.rb
+++ app/services/payout/service.rb
@@ -51,31 +51,6 @@ module Payout
         return true
       end
 
-      # Checking these classes will be removed once the following issue is addressed
-      # https://github.com/brave-intl/publishers/issues/2858
-
-      if self.class == Payout::UpholdService && @publisher.gemini_connection.present? && @publisher.selected_wallet_provider_type != 'UpholdConnection'
-        # A publisher can have a gemini_connection and uphold_connection.
-        # We don't want to include users who have a GeminiConnection on the Uphold Payout
-        create_message("Publisher has a gemini connection and is not paid out through this job")
-        return true
-      end
-
-      if self.class != Payout::PaypalService
-        # A publisher can have a uphold_connection and a paypal_connection.
-        # Handle the case where the wallet_provider field is nil
-        connection = @publisher.selected_wallet_provider
-        if connection.japanese_account?
-          create_message("Publisher is located in Japan and is not paid out through this job")
-          return true
-        end
-
-        if @publisher.paypal_connection.present?
-          create_message("Publisher has a Paypal Connection  and is not paid out through this job")
-          return true
-        end
-      end
-
       false
     end
   end
