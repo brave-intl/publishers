2e9dbaae72a19bca59eb9ffc8606a12885bbf2cc
Tyler Smart <tsmart@brave.com>
2021-05-10 14:16:23 -0700

    Fix JS error on admin payments
    A recent yarn upgrade brought with it an updated styled-components
    which broke the admin payments page. I've rolled that back and
    added a simple capybara test to ensure it doesn't happen again.

---
+++ test/features/admin_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class AdminFeatureTest < Capybara::Rails::TestCase
+  include Devise::Test::IntegrationHelpers
+  include Rails.application.routes.url_helpers
+
+  let(:publisher) { publishers(:admin) }
+
+  before do
+    sign_in publisher
+  end
+
+  test "can view admin home" do
+    visit admin_root_path
+    assert_content page, "Contributions processed"
+  end
+
+  test "can view a creators payments" do
+    visit admin_publisher_payments_path(publisher_id: publisher.id)
+    assert_content page, "Earned To Date"
+  end
+end
