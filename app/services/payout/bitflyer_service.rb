412af870d489c1f0b4b94a65b56fca30e194108f
Tyler Smart <tsmart@brave.com>
2021-05-11 14:18:18 -0700

    Update Uphold Payout Test to cover BitFlyer
    Paypal is going away, and we can remove that code next month
    after the next payout. We also want to clean up the database
    to ensure that every creator has a selected_wallet so that we
    can simplify the service.rb skip_publisher? method, as well as
    any other code would benefit from the data cleanup.

--- app/services/payout/bitflyer_service.rb
+++ app/services/payout/bitflyer_service.rb
@@ -14,7 +14,7 @@ module Payout
       payout_utils = @payout_utils_class.new(payout_report: payout_report,
                                              publisher: publisher,)
 
-      return if payout_utils.skip_publisher?
+      return if payout_utils.skip_publisher?(is_bitflyer: true)
 
       potential_payments = []
 
