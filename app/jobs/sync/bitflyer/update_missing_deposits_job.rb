307830ae8191dc39f756e0a3135e61e65bb571fd
Albert Wang <=>
2021-05-12 00:47:47 -0700

    Fix readability and lint

--- app/jobs/sync/bitflyer/update_missing_deposits_job.rb
+++ app/jobs/sync/bitflyer/update_missing_deposits_job.rb
@@ -2,7 +2,13 @@ class Sync::Bitflyer::UpdateMissingDepositsJob < ApplicationJob
   queue_as :low
 
   def perform
-    Publisher.joins(:bitflyer_connection).joins(:channels).where(selected_wallet_provider: BitflyerConnection.class.name.to_s).where( channels: { deposit_id: nil}).select("channels.id").each do |channel_id|
+    Publisher.
+      joins(:bitflyer_connection).
+      joins(:channels).
+      where(selected_wallet_provider: BitflyerConnection.class.name.to_s).
+      where(channels: { deposit_id: nil }).
+      select("channels.id").
+      each do |channel_id|
       Sync::Bitflyer::UpdateMissingDepositsJob.perform_async(channel_id)
     end
   end
