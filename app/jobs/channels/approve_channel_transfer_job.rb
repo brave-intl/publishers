1548d4b7462aef700a58d79e8de3ca6b1bb57853
Yachtcaptain23 <amwang217@gmail.com>
2021-05-27 10:40:30 -0700

    Address missing deposit_id's in the channel model (#3210)
    Allow backfilling of BitFlyer deposit_id's via a background job.
    
    At the moment the after_save calls are all done in a after save as a blocking call.
    We could move these calls to a job if there doesn't seem to be any
    repercussions.
    
    Co-authored-by: Albert Wang <=>

--- app/jobs/channels/approve_channel_transfer_job.rb
+++ app/jobs/channels/approve_channel_transfer_job.rb
@@ -12,15 +12,7 @@ module Channels
         ## Need to generate new bF deposit ID on channel transfer:
         channel.deposit_id = nil
         if contested_by.publisher.selected_wallet_provider_type == Channel::BITFLYER_CONNECTION
-          # Request a deposit id from bitFlyer.
-          url = URI.parse(Rails.application.secrets[:bitflyer_host] + '/api/link/v1/account/create-deposit-id?request_id=' + SecureRandom.uuid)
-          request = Net::HTTP::Get.new(url.to_s)
-          request['Authorization'] = "Bearer " + contested_by.publisher.bitflyer_connection.access_token
-          response = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') do |http|
-            http.request(request)
-          end
-          deposit_id = JSON.parse(response.body)["deposit_id"]
-          contested_by.update(deposit_id: deposit_id)
+          Sync::Bitflyer::UpdateMissingDepositJob.new.perform(contested_by.id)
         end
         ###################################################################
 
