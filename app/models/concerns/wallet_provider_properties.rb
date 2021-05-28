057cd2a9dd37a40ecb3f1af07bb353fc4f88829b
Yachtcaptain23 <amwang217@gmail.com>
2021-05-27 18:09:32 -0700

    Fix after_destroy hooks for gemini and bitflyer (#3254)
    * Fix after_destroy hooks for gemini and bitflyer
    
    https://github.com/brave-intl/publishers/issues/3252
    https://github.com/brave-intl/publishers/issues/3253
    
    * Update test/fixtures/publishers.yml
    Update test/models/gemini_connection_test.rb
    
    Co-authored-by: Albert Wang <=>

---
+++ app/models/concerns/wallet_provider_properties.rb
@@ -0,0 +1,12 @@
+module WalletProviderProperties
+  extend ActiveSupport::Concern
+
+  included do
+    after_destroy :clear_selected_wallet_provider
+  end
+
+  def clear_selected_wallet_provider
+    return unless publisher.selected_wallet_provider == self
+    publisher.update(selected_wallet_provider: nil)
+  end
+end
