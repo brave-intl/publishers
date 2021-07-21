203ea0596dc6f427ea81f04b9f4a1ae7e5bab7d9
Tyler Smart <tsmart@brave.com>
2021-07-21 16:06:59 -0700

    Improve test

--- test/services/uphold/refresher_test.rb
+++ test/services/uphold/refresher_test.rb
@@ -22,11 +22,12 @@ class UpholdRefresherTest < ActiveSupport::TestCase
   test 'refresh with previous token' do
     connection = uphold_connections(:basic_connection)
     refute_nil connection.refresh_token
+    refute_equal(connection.reload.refresh_token, '82b0')
+
     mock_auth = MiniTest::Mock.new.expect(:refresh, received_from_uphold,
                                           [connection])
     refresher = Uphold::Refresher.new(impl_refresher: mock_auth)
     refresher.call(uphold_connection: connection)
-    refute_nil connection.refresh_token
     assert_equal(connection.reload.refresh_token, '82b0')
   end
 end
