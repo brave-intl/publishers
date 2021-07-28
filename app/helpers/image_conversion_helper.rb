c2e7c1ec07d153fd8238c7d8e0bb7cddfe70c15f
Tyler Smart <tsmart@brave.com>
2021-07-28 10:53:13 -0700

    Move resize method to gem

--- app/helpers/image_conversion_helper.rb
+++ app/helpers/image_conversion_helper.rb
@@ -18,87 +18,15 @@ module ImageConversionHelper
       return nil
     end
 
-    actual_jpeg_hex_to_save = resize_image_with_wasm(file_bytes: file_bytes, dimensions: dimensions)
+    actual_jpeg_to_save = Wasm::Thumbnail::Rb.resize_and_pad(file_bytes: file_bytes,
+                                                             width: dimensions[0],
+                                                             height: dimensions[1],
+                                                             size: 250000)
 
     new_filename = filename + "_resized"
     temp_file = Tempfile.new([new_filename, ".jpg"], binmode: true)
-    temp_file.write(actual_jpeg_hex_to_save)
+    temp_file.write(actual_jpeg_to_save)
     temp_file.path
-  end
-
-  def resize_image_with_wasm(file_bytes:, dimensions:, size: 250000)
-    # Let's compile the module to be able to execute it!
-    wasm_instance = Wasm::Thumbnail::Rb::GetWasmInstance.call
-
-    # This tells us how much space we'll need to put our image in the WASM env
-    image_length = file_bytes.length
-    input_pointer = wasm_instance.exports.allocate.call(image_length)
-    # Get a pointer on the allocated memory so we can write to it
-    memory = wasm_instance.exports.memory.uint8_view input_pointer
-
-    # Put the image to resize in the allocated space
-    for nth in 0..image_length - 1
-      memory[nth] = file_bytes[nth]
-    end
-
-    # Do the actual resize and pad
-    # Note that this writes to a portion of memory the new JPEG file, but right pads the rest of the space
-    # we gave it with 0.
-    begin
-      output_pointer = wasm_instance.exports.resize_and_pad.call(input_pointer, image_length, dimensions[0], dimensions[1], size)
-    rescue RuntimeError
-      raise "Error processing the image."
-    end
-    # Get a pointer to the result
-    memory = wasm_instance.exports.memory.uint8_view output_pointer
-
-    # Only take the buffer that we told the rust function we needed. The resize function
-    # makes a smaller image than the buffer we said, and then pads out the rest so we have to
-    # go hunting for the bytes which represent the JPEG image. In hex, JPEG images start with
-    # FFD8 and FFD9, so we can convert to hex and find the bounds of the image, then write to file
-    bytes = memory.to_a.take(size)
-
-    # Deallocate
-    wasm_instance.exports.deallocate.call(input_pointer, image_length)
-    wasm_instance.exports.deallocate.call(output_pointer, bytes.length)
-
-    # The bytes passed back to us are ASCII-encoded, i.e. 8bit bytes. Interpret them as so,
-    # and THEN convert to hex to search for the image bytes
-
-    # The first 4 bytes are a header until the image. The actual image probably ends well before
-    # the whole buffer, but we keep the junk data on the end to make all the images the same size
-    # for privacy concerns.
-    bytes[4..].pack('C*')
-  end
-
-  #   Adding an empty comment adds an arbitrary number of bytes
-  #   Add a character to a comment adds 5 bytes (4 for padding)
-  def add_padding_to_image(source_image_path:, attachment_type:, quality:)
-    # Add initial conversion to get file size
-    if attachment_type == SiteBanner::LOGO
-      target_file_size = SiteBanner::LOGO_UNIVERSAL_FILE_SIZE
-    elsif attachment_type == SiteBanner::BACKGROUND
-      target_file_size = SiteBanner::BACKGROUND_UNIVERSAL_FILE_SIZE
-    else
-      LogException.perform(StandardError.new("Unknown attachment_type:" + attachment_type), params: {})
-    end
-
-    file_size_after_one_byte = calculate_image_size_after_pad_one_byte(source_image_path, quality)
-
-    delta = target_file_size - file_size_after_one_byte
-
-    if delta < 0
-      raise OutsidePaddingRangeError, "Expect minimum to be #{file_size_after_one_byte}"
-    end
-
-    MiniMagick::Tool::Convert.new do |convert|
-      convert << source_image_path
-      convert.merge!(["-set", "comment", "a" * (delta + 1)])
-      convert.merge!(["-quality", quality])
-      convert << source_image_path
-    end
-
-    source_image_path
   end
 
   def generate_filename(source_image_path:)
@@ -106,27 +34,4 @@ module ImageConversionHelper
       Digest::SHA256.hexdigest f.read
     end
   end
-
-  private
-
-  def calculate_image_size_after_pad_one_byte(source_image_path, quality)
-    mini_magick_image = MiniMagick::Image.open(source_image_path)
-
-    new_filename = "_resized"
-
-    temp_file = Tempfile.new([new_filename, ".jpg"])
-
-    mini_magick_image.write(temp_file)
-
-    MiniMagick::Tool::Convert.new do |convert|
-      convert << temp_file.path
-      convert.merge!(["-set", "comment", "a"])
-      convert.merge!(["-quality", quality])
-      convert << temp_file.path
-    end
-
-    File.size(temp_file.path)
-  end
-
-  class OutsidePaddingRangeError < RuntimeError; end
 end
