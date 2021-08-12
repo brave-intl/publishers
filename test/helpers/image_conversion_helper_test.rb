be1ab5254729ae726c77bab9f5fd133eaf9383e8
tsmartt <81187695+tsmartt@users.noreply.github.com>
2021-08-11 18:19:31 -0700

    Completely remove imagemagick (#3360)
    * Completely remove imagemagick
    
    * Move fastimage to test and update Dockerfile

--- test/helpers/image_conversion_helper_test.rb
+++ test/helpers/image_conversion_helper_test.rb
@@ -1,49 +1,41 @@
 require 'test_helper'
-class ImageConversionHelperTest < ActionView::TestCase
-  test "resizes png to jpg" do
-    source_image_path = "./app/assets/images/bat-logo@3x.png"
-    temp_file_path = resize_to_dimensions_and_convert_to_jpg(
-      source_image_path: source_image_path.to_s,
-      attachment_type: SiteBanner::LOGO,
-      filename: "bat_logo"
-    )
+describe ImageConversionHelper do
+  include ImageConversionHelper
 
-    mini_magick_image = MiniMagick::Image.open(temp_file_path)
-    assert_operator SiteBanner::LOGO_DIMENSIONS.first, :>=, mini_magick_image.resolution.first
-    assert_operator SiteBanner::LOGO_DIMENSIONS.second, :>=, mini_magick_image.resolution.second
-    assert "jpg", File.extname(source_image_path)
-  end
-
-  test "resizes a jpg to jpg" do
-    source_image_path = "./app/assets/images/brave-lion@3x.jpg"
-    temp_file_path = resize_to_dimensions_and_convert_to_jpg(
-      source_image_path: source_image_path.to_s,
-      attachment_type: SiteBanner::LOGO,
-      filename: "brave_lion_logo"
-    )
-
-    mini_magick_image = MiniMagick::Image.open(temp_file_path)
-    assert_operator SiteBanner::LOGO_DIMENSIONS.first, :>=, mini_magick_image.resolution.first
-    assert_operator SiteBanner::LOGO_DIMENSIONS.second, :>=, mini_magick_image.resolution.second
-    assert "jpg", File.extname(source_image_path)
-  end
-
-  test "generates a consistent filename for an image" do
+  it "generates a consistent filename for an image" do
     assert_equal "8957c0ef46cdabe73f93ee92b9a43ccb7fa8c9c319212c1f01a67957cab3b6b9", generate_filename(source_image_path: "./app/assets/images/brave-lion@3x.jpg")
   end
 
-  test "adds padding to a logo for the correct file size" do
-    source_image_path = "./app/assets/images/brave-lion@3x.jpg"
-    temp_file = Tempfile.new(["brave_lion_copy", ".jpg"])
+  describe 'with image to test' do
+    before do
+      source_image_path = "./app/assets/images/brave-lion@3x.jpg"
+      temp_file = Tempfile.new(["brave_lion_copy", ".jpg"])
 
-    IO.copy_stream(source_image_path, temp_file.path)
+      IO.copy_stream(source_image_path, temp_file.path)
 
-    temp_file_path = resize_to_dimensions_and_convert_to_jpg(
-      source_image_path: temp_file.path,
-      attachment_type: SiteBanner::LOGO,
-      filename: "brave_lion_logo"
-    )
+      @temp_file_path = resize_to_dimensions_and_convert_to_jpg(
+        source_image_path: temp_file.path,
+        attachment_type: SiteBanner::LOGO,
+        filename: "brave_lion_logo"
+      )
+    end
 
-    assert_equal SiteBanner::LOGO_UNIVERSAL_FILE_SIZE, File.open(temp_file_path, 'r').size
+    it "resizes png to jpg" do
+      width, height = FastImage.size(@temp_file_path)
+      assert_operator SiteBanner::LOGO_DIMENSIONS.first, :>=, width
+      assert_operator SiteBanner::LOGO_DIMENSIONS.second, :>=, height
+      assert "jpg", File.extname(@temp_file_path)
+    end
+
+    it "resizes a jpg to jpg" do
+      width, height = FastImage.size(@temp_file_path)
+      assert_operator SiteBanner::LOGO_DIMENSIONS.first, :>=, width
+      assert_operator SiteBanner::LOGO_DIMENSIONS.second, :>=, height
+      assert "jpg", File.extname(@temp_file_path)
+    end
+
+    it "adds padding to a logo for the correct file size" do
+      assert_equal SiteBanner::LOGO_UNIVERSAL_FILE_SIZE, File.open(@temp_file_path, 'r').size
+    end
   end
 end
