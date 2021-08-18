3297450bd20bc04db7d28c4af9a2a39af71e45a1
Tyler Smart <tsmart@brave.com>
2021-08-18 12:39:08 -0700

    Remove unnecessary jobs, we'll just refresh on log in

--- app/jobs/sync/uphold/refresh_access_token_job.rb
+++
@@ -1,8 +0,0 @@
-class Sync::Uphold::RefreshAccessTokenJob
-  include Sidekiq::Worker
-
-  def perform(uphold_connection_id)
-    connection = UpholdConnection.find(uphold_connection_id)
-    Uphold::Refresher.build.call(uphold_connection: connection)
-  end
-end
