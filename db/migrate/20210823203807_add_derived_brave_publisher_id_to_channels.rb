ab8ebedefa4e2f0aacf6fc5b087c9a857a690ab6
Albert Wang <amwang217@gmail.com>
2021-08-31 09:13:09 -0700

    Set derived_brave_publisher_id in the channels table (#3371)
    Co-authored-by: Albert Wang <=>

---
+++ db/migrate/20210823203807_add_derived_brave_publisher_id_to_channels.rb
@@ -0,0 +1,9 @@
+class AddDerivedBravePublisherIdToChannels < ActiveRecord::Migration[6.1]
+  def up
+    add_column :channels, :derived_brave_publisher_id, :text, index: true
+  end
+
+  def down
+    remove_column :channels, :derived_brave_publisher_id
+  end
+end
