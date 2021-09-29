1bd7ed47b25e4ad58b230f6a143ea90c0c712f2e
Albert Wang <amwang217@gmail.com>
2021-09-29 08:57:57 -0700

    Fix EmailBreakdownsJob to properly count the number of confirmations in (#3399)
    our daily emails.
    
    Closes https://github.com/brave-intl/publishers/issues/3398
    
    Co-authored-by: Albert Wang <=>

--- app/jobs/promo/email_breakdowns_job.rb
+++ app/jobs/promo/email_breakdowns_job.rb
@@ -10,38 +10,38 @@ class Promo::EmailBreakdownsJob
     start_date = publisher.receives_mtd_promo_emails? ? Date.today.at_beginning_of_month.to_date : 1.days.ago.to_date
     start_date = start_date > end_date ? end_date : start_date
 
-    csv = CSV.parse(Promo::RegistrationStatsReportGenerator.new(
-      referral_codes: referral_codes,
-      start_date: start_date,
-      end_date: end_date,
-      reporting_interval: PromoRegistration::DAILY,
-      is_geo: true,
-      include_ratios: false
-    ).perform)
-    new_csv = []
-    header = true
-    csv.each do |row|
-      row = row.dup
-      row.delete_at(5) # delete the 30-day-confirmation column
-      if header
-        row << "Confirmations received"
-        header = false
-      else
-        row << total_for_date(date: row[2].to_date,
-                              referral_code: row[0],
-                              country: "\"" + (row[1] || "") + "\"",
-                              publisher_id: publisher_id)
+    csv = []
+    csv.append(["Date", "ReferralCode", "Platform", "CountryCode", "DownloadsTotal"].join(","))
+    (start_date..end_date).each do |date|
+      referral_codes.each do |referral_code|
+        result = ReferralDownload.where(referral_code: referral_code, owner_id: publisher_id, ymd: date.strftime("%Y-%m-%d")).pluck(:ymd, :referral_code, :platform, :country_code, :total)
+        result.map! { |entry| entry.join(",") }
+        csv += result
       end
-      new_csv << row.join(",")
     end
-    PublisherMailer.promo_breakdowns(publisher, new_csv.join("\n")).deliver_now
-  end
 
-  def total_for_date(date:, referral_code:, country:, publisher_id:)
-    ReferralDownload.where(finalized: true,
-                           owner_id: publisher_id,
-                           referral_code: referral_code,
-                           country: country,
-                           ymd: date - 30.days).count
+    csv.append("")
+
+    csv.append(["ConfirmationDate", "CountryCode", "ConfirmationsTotal"].join(","))
+    (start_date..end_date).each do |date|
+      referral_codes.each do |referral_code|
+        start_of_day = date.beginning_of_day
+        end_of_day = date.end_of_day
+        result = ReferralDownload.where(referral_code: referral_code, owner_id: publisher_id).
+          where("finalized_ts >= ?",start_of_day).
+          where("finalized_ts <= ?", end_of_day)
+        result.each do |referral_download|
+          csv.append(
+            [
+              referral_download["finalized_ts"].to_date, 
+              referral_download["country_code"], 
+              referral_download["total"]
+            ].join(",")
+          )
+        end
+      end
+    end
+
+    PublisherMailer.promo_breakdowns(publisher, csv.join("\n")).deliver_now
   end
 end
