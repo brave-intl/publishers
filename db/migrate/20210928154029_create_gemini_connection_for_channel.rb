297c76d76cbaab5f7ef2aa4b3ad0b3dd1ec0374d
Tyler Smart <tsmart@brave.com>
2021-09-29 16:14:56 -0500

    Remove extraneous index

--- db/migrate/20210928154029_create_gemini_connection_for_channel.rb
+++ db/migrate/20210928154029_create_gemini_connection_for_channel.rb
@@ -1,7 +1,7 @@
 class CreateGeminiConnectionForChannel < ActiveRecord::Migration[6.1]
   def change
     create_table :gemini_connection_for_channels, id: :uuid, default: -> { "uuid_generate_v4()"} do |t|
-      t.belongs_to :gemini_connection, type: :uuid, null: false
+      t.belongs_to :gemini_connection, type: :uuid, null: false, index: false
       t.belongs_to :channel, type: :uuid, index: true, null: false
 
       t.string :channel_identifier
