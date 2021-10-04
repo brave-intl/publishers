00be7638002462b442d778bc514f4b42625a2082
Albert Wang <=>
2021-10-04 12:55:31 -0700

    Not all users will have a an uphold connection for checking possible currencies.
    Closes https://github.com/brave-intl/publishers/issues/3422

--- app/controllers/publishers/wallets_controller.rb
+++ app/controllers/publishers/wallets_controller.rb
@@ -11,7 +11,7 @@ module Publishers
         uphold_connection: uphold_wallet,
         gemini_connection: gemini_wallet,
         bitflyer_connection: bitflyer_wallet,
-        possible_currencies: current_publisher.uphold_connection.uphold_details&.currencies || [],
+        possible_currencies: current_publisher.uphold_connection&.uphold_details&.currencies || [],
       }
 
       render(json: wallet_data)
