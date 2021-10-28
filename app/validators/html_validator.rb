07be72a19dc865f871a3e7a5bf149849075246f8
Andrea Brancaleoni <miwaxe@gmail.com>
2021-10-28 17:22:55 +0200

    Implement a new HtmlValidator (#3436)
    Create a new HtmlValidator. HtmlValidator can be included in any
    ActiveRecord model with the `valitades_with` directive.
    
    When `:attributes` is passed only the selected model attributes are
    validated against the blocklist.

---
+++ app/validators/html_validator.rb
@@ -0,0 +1,7 @@
+class HtmlValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if value.instance_of?(String) && ERB::Util::HTML_ESCAPE_ONCE_REGEXP.match?(value)
+      record.errors[attribute].add(options[:message] || "contains html sequences")
+    end
+  end
+end
