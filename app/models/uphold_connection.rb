e722b93e607abb05905dc5d0782b974703e56688
Tyler Smart <tsmart@brave.com>
2021-11-04 18:50:46 -0500

    Revert "Revert "Show uphold account needs reauthorization if we get a 401 (#3461)" (#3466)"
    This reverts commit e4b959220bed8f88a4cd55bf03e5ba9c862bc32e.

--- app/models/uphold_connection.rb
+++ app/models/uphold_connection.rb
@@ -26,6 +26,7 @@ class UpholdConnection < ApplicationRecord
     # aren't valid states: https://uphold.com/en/developer/api/documentation/#user-object
     RESTRICTED = :restricted
     BLOCKED = :blocked
+    OLD_ACCESS_CREDENTIALS = :old_access_credentials
   end
 
   OK = "ok"
@@ -94,7 +95,7 @@ class UpholdConnection < ApplicationRecord
     # TODO Let's make sure that if we can't access the user's information then we set uphold_verified? to false
     # Perhaps through a rescue on 401
     uphold_verified? &&
-      (uphold_access_parameters.blank? || scope.exclude?("cards:write") || uphold_details.nil?)
+      (uphold_access_parameters.blank? || scope.exclude?("cards:write") || uphold_details.nil? || status&.to_sym == UpholdAccountState::OLD_ACCESS_CREDENTIALS)
   end
 
   # Makes a remote HTTP call to Uphold to get more details
@@ -105,6 +106,7 @@ class UpholdConnection < ApplicationRecord
     @user ||= UpholdClient.user.find(self)
   rescue Faraday::ClientError => e
     if e.response&.dig(:status) == 401
+      update(status: UpholdAccountState::OLD_ACCESS_CREDENTIALS)
       Rails.logger.fatal("#{e.response[:body]} for uphold connection #{id}")
       nil
     else
