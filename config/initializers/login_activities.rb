6900e693a11ec881c9de99e77818ad2f33392180
tsmartt <81187695+tsmartt@users.noreply.github.com>
2021-11-09 16:01:29 -0600

    Add Wallet Syncer which Updates on Login (#3476)
    * Add Wallet Syncer which Updates on Login
    
    We don't want to update on home page view, and we want to cover
    all wallet providers. Currently we won't sync Bitflyer since I'm not
    entirely sure of the stability of that part of the codebase and this is
    mainly addressing Uphold, for the moment. Will need to come back and
    turn that on for Bitflyer, though it's not as big of a priority since
    at connection, the user is already KYCd.
    
    * Lint fix

--- config/initializers/login_activities.rb
+++ config/initializers/login_activities.rb
@@ -1,5 +1,5 @@
-# Record the publishers's successful login
-Warden::Manager.after_authentication except: :fetch do |publisher, auth, opts|
+# Record the publishers' successful login
+Warden::Manager.after_authentication except: :fetch do |publisher, auth, _opts|
   params = {
     publisher: publisher,
     user_agent: auth.request.headers["User-Agent"],
@@ -7,4 +7,8 @@ Warden::Manager.after_authentication except: :fetch do |publisher, auth, opts|
   }
 
   LoginActivity.create!(params)
+
+  if publisher && !Rails.env.test?
+    Util::Wallet::ConnectionSyncer.build.call(publisher: publisher)
+  end
 end
