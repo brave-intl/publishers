5f38dc5121134ff172adce8635f26fcaf8ead85a
Tyler Smart <tsmart@brave.com>
2021-11-11 12:56:19 -0600

    test fixes

--- test/services/util/wallet/connection_syncer_test.rb
+++ test/services/util/wallet/connection_syncer_test.rb
@@ -2,19 +2,19 @@
 
 require "test_helper"
 
-class Util::Wallet::ConnectionSyncerTest < RDoc::Options::Template
+class Util::Wallet::ConnectionSyncerTest < ActiveSupport::TestCase
   test "with no wallet does nothing" do
-    Exception.expects("perform").never.returns(1)
+    LogException.expects("perform").never.returns(1)
     refute Util::Wallet::ConnectionSyncer.new.call(publisher: publishers(:notes))
   end
 
   test "Uphold" do
-    Payout::UpholdService.any_instance.expects("sync_connection!").at_least_once.returns(1)
+    UpholdConnection.any_instance.expects("sync_connection!").at_least_once.returns(1)
     Util::Wallet::ConnectionSyncer.new.call(publisher: publishers(:verified))
   end
 
   test "Gemini" do
-    Bundler::Definition.any_instance.expects("sync_connection!").at_least_once.returns(1)
+    GeminiConnection.any_instance.expects("sync_connection!").at_least_once.returns(1)
     Util::Wallet::ConnectionSyncer.new.call(publisher: publishers(:top_referrer_gemini))
   end
 
