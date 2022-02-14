1c70c77f000a7b1f822b3970036c537be96fbbf8
Jon Staples <jstaples@brave.com>
2022-02-14 11:25:26 -0500

    task: Remove them cookies and maybe disable them?

--- app/javascript/analytics.js
+++ app/javascript/analytics.js
@@ -1,22 +1,4 @@
 if (document.head.dataset.piwikHost) {
   window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  window._paq.push(['trackPageView']);
-  window._paq.push(['enableLinkTracking']);
-  (function() {
-    var u=document.head.dataset.piwikHost;
-    window._paq.push(['setTrackerUrl', u+'piwik.php']);
-    window._paq.push(['setSiteId', '6']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-
-  document.addEventListener('click', function (e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement;
-
-    if (target.dataset.piwikAction) {
-      window._paq.push(['trackEvent', target.dataset.piwikAction, target.dataset.piwikName, target.dataset.piwikValue]);
-    }
-  }, false);
+  window._paq.push(['disableCookies']);
 }
