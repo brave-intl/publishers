a0a5c4f4822dc2820568f00e137bab3dbf5a4489
tsmartt <81187695+tsmartt@users.noreply.github.com>
2022-02-17 11:42:07 -0600

    Only retry in CI (#3556)
    * Only retry in CI
    
    * NPM audit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,13 +19,15 @@ require "capybara/minitest"
 require "minitest/rails"
 require "minitest/retry"
 
-Minitest::Retry.use!(
-  retry_count: 3, # The number of times to retry. The default is 3.
-  verbose: true, # Whether or not to display the message at the time of retry. The default is true.
-  io: $stdout, # Display destination of retry when the message. The default is stdout.
-  exceptions_to_retry: [], # List of exceptions that will trigger a retry (when empty, all exceptions will).
-  methods_to_retry: [] # List of methods that will trigger a retry (when empty, all methods will).
-)
+if ENV["USE_MINITEST_RETRY"]
+  Minitest::Retry.use!(
+    retry_count: 3, # The number of times to retry. The default is 3.
+    verbose: true, # Whether or not to display the message at the time of retry. The default is true.
+    io: $stdout, # Display destination of retry when the message. The default is stdout.
+    exceptions_to_retry: [], # List of exceptions that will trigger a retry (when empty, all exceptions will).
+    methods_to_retry: [] # List of methods that will trigger a retry (when empty, all methods will).
+  )
+end
 
 Webpacker.compile
 
