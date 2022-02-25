20ed5544275ce6ec627941c0a9cb332bcd2cd4db
tsmartt <81187695+tsmartt@users.noreply.github.com>
2022-02-25 10:11:38 -0600

    Referral Email and Sidekiq updates (#3566)
    * Referral Email and Sidekiq updates
    
    Top referrers will now receive a monthly email with the last 4 months worth
    of data. Also while adding this, I noticed some duplicate keys in the
    sidekiq.yml and added a the scheduled jobs web interface.
    
    * Move Email breakdown business logic to service
    
    * Lint Fixes

--- test/jobs/sidekiq_configuration_test.rb
+++ test/jobs/sidekiq_configuration_test.rb
@@ -10,12 +10,12 @@ class SidekiqConfigurationTest < ActiveJob::TestCase
 
     assert configuration.present?
 
-    jobs = configuration.dig(:schedule).keys
-    jobs.each do |job|
+    jobs = configuration.dig(:schedule).each do |job_key, job|
       # Rails can convert a string to a class with constantize
       # https://api.rubyonrails.org/classes/ActiveSupport/Inflector.html#method-i-constantize
       # This raises a NameError if the configuration isn't valid
-      job.constantize
+      to_constantize = job["class"] || job_key
+      to_constantize.constantize
     end
   end
 end
