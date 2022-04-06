363f7732abc8b36155dc872d58912c3594a417de
Kyle Den Hartog <kdenhartog@users.noreply.github.com>
2022-04-07 11:55:20 +1200

    chore: update Webauthn config to align supported algorithms with policy (#3605)
    Co-authored-by: Kyle Den Hartog <kdenhartog@brave.com>

---
+++ test/webauthn_config_test.rb
@@ -0,0 +1,15 @@
+# typed: false
+require "test_helper"
+
+class WebauthnConfigurationTest < ActiveSupport::TestCase
+
+  test "Webauthn supported verification algorithms are correctly configured" do
+    assert WebAuthn.configuration.algorithms.include?("ES256")
+    assert WebAuthn.configuration.algorithms.include?("ES384")
+    assert WebAuthn.configuration.algorithms.include?("ES512")
+    assert WebAuthn.configuration.algorithms.include?("PS256")
+    assert WebAuthn.configuration.algorithms.include?("PS384")
+    assert WebAuthn.configuration.algorithms.include?("PS512")
+    assert !WebAuthn.configuration.algorithms.include?("RS256")
+  end
+end
