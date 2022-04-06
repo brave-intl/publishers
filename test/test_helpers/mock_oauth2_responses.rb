7ce5b18d2e1328107d73c64c0ec51ad4d98e48f3
Jon Staples <8813557+OAuthBringer@users.noreply.github.com>
2022-04-06 16:00:55 -0400

    Feat/refresher service (#3609)
    * feat: Abstract concern for Oauth2 providers such as wallets
    * feat: Oauth2::ClientCredentials
    * feat: Oauth2RefresherService

---
+++ test/test_helpers/mock_oauth2_responses.rb
@@ -0,0 +1,13 @@
+module MockOauth2Responses
+  include Oauth2::Structs
+
+  def mock_token_failure(token_url)
+    stub_request(:post, token_url)
+      .to_return(status: 400, body: {error: "invalid_grant", error_description: "Ann error occurred"}.to_json)
+  end
+
+  def mock_refresh_token_success(token_url)
+    stub_request(:post, token_url)
+      .to_return(status: 200, body: {access_token: "access_token", expires_in: 10.minutes.to_i, refresh_token: "refresh_token", token_type: "example"}.to_json)
+  end
+end
