1d5053f9e2b71685b4ddf6d4df97ec059ecd2579
Jon Staples <jstaples@brave.com>
2022-04-06 12:46:54 -0400

    feat: Oauth2RefresherService

--- lib/oauth2/client_credentials.rb
+++ lib/oauth2/client_credentials.rb
@@ -18,34 +18,9 @@ class Oauth2::ClientCredentials
     @options = {use_ssl: true}
   end
 
-  #      def access_token(scope = nil)
-  #        # Below is a general flow of how this would normally work
-  #        # for generating an access token from client credentials but it isn't tested
-  #        raise NotImplementedError
-  #        uri = URI.parse(@token_url)
-  #        request = Net::HTTP::Post.new(uri)
-  #        form_data = {"grant_type" => "client_credentials"}
-  #
-  #        if scope:
-  #            formdata.merge("scope" => scope)
-  #
-  #        request.basic_auth(@client_id, @client_secret)
-  #        request.content_type = "application/x-www-form-urlencoded"
-  #        request.set_form_data(formdata)
-  #
-  #        response = Net::HTTP.start(uri.hostname, uri.port, @options) do |http|
-  #          http.request(request)
-  #        end
-  #
-  #        if response.is_a? Net::HttpSuccess
-  #          struct = AccessTokenResponse
-  #        else
-  #          struct = ErrorResponse
-  #        end
-  #
-  #        # To spec should always return a json response body.
-  #        struct.new(JSON.parse(response.body))
-  #      end
+  def access_token(scope = nil)
+    raise NotImplementedError
+  end
 
   sig { params(refresh_token: String).returns(T.any(RefreshTokenResponse, ErrorResponse)) }
   def refresh_token(refresh_token)
