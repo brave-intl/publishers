dbf58a2377608371630b057a24223b4a3784327f
Jon Staples <jstaples@brave.com>
2022-04-28 09:24:23 -0400

    task: Update rails to 6.1.5.1

--- app/controllers/admin/stats/top_balances_controller.rb
+++ app/controllers/admin/stats/top_balances_controller.rb
@@ -3,12 +3,12 @@ class Admin::Stats::TopBalancesController < AdminController
   def index
     @limit = params[:limit].present? ? params[:limit].to_i : 25
     @result = JSON.parse(case params[:type]
-    when Eyeshade::TopBalances::CHANNEL
-      Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_CHANNEL_BALANCES) || "[]"
-    when Eyeshade::TopBalances::OWNER
-      Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_PUBLISHER_BALANCES) || "[]"
-    when Eyeshade::TopBalances::UPHOLD
-      Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_UPHOLD_BALANCES) || "[]"
+              when Eyeshade::TopBalances::CHANNEL
+                Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_CHANNEL_BALANCES) || "[]"
+              when Eyeshade::TopBalances::OWNER
+                Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_PUBLISHER_BALANCES) || "[]"
+              when Eyeshade::TopBalances::UPHOLD
+                Rails.cache.fetch(Cache::EyeshadeStatsJob::EYESHADE_TOP_UPHOLD_BALANCES) || "[]"
               end)
     if @result.present?
       @result = @result.take(@limit)
