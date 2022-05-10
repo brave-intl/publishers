eb9dfb0e3250feec306af074113efebfc59935ab
Jon Staples <jstaples@brave.com>
2022-05-10 09:34:36 -0400

    improvement: Utilize token refresh implementations to handle deposit id updates

--- app/jobs/sync/bitflyer/update_missing_deposit_job.rb
+++ app/jobs/sync/bitflyer/update_missing_deposit_job.rb
@@ -3,26 +3,32 @@ class Sync::Bitflyer::UpdateMissingDepositJob
   include Sidekiq::Worker
   sidekiq_options queue: :default, retry: true
 
-  def perform(channel_id)
-    # Temporary disable
+  def perform(channel_id, notify: false)
+    channel = channel.find(channel_id)
+    return if channel.deposit_id.present?
 
-    # channel = Channel.find(channel_id)
-    # return if channel.deposit_id.present?
+    conn = channel.publisher.bitflyer_connection
 
-    # access_token = channel.publisher.bitflyer_connection.access_token
-    # raise "Bitflyer access token is nil!" unless access_token
-    #
-    # # Request a deposit id from bitFlyer.
-    # url = URI.parse(Rails.application.secrets[:bitflyer_host] + "/api/link/v1/account/create-deposit-id?request_id=" + SecureRandom.uuid)
-    # request = Net::HTTP::Get.new(url.to_s)
-    #
+    # Check all possible refresh options, notify user if  failure is known type
+    # and they need to refresh their wallets.
+    result = Wallet::RefreshFailutreNotificationService.build.call(conn, notify: notify)
 
-    # request["Authorization"] = "Bearer " + access_token
-    # response = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-    #   http.request(request)
-    # end
-    #
-    # deposit_id = JSON.parse(response.body)["deposit_id"]
-    # channel.update(deposit_id: deposit_id)
+    # If the result is a failure, the user's connection has been marked as failed and the deposit id cannot be retrieve
+    case result
+    when Oauth2::Responses::ErrorResponse
+      return
+    end
+
+    # request a deposit id from bitflyer.
+    url = uri.parse(rails.application.secrets[:bitflyer_host] + "/api/link/v1/account/create-deposit-id?request_id=" + securerandom.uuid)
+    request = net.http.get.new(url.to_s)
+
+    request["authorization"] = "bearer " + conn.access_token
+    response = net.http.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+      http.request(request)
+    end
+
+    deposit_id = json.parse(response.body)["deposit_id"]
+    channel.update(deposit_id: deposit_id)
   end
 end
--- app/jobs/sync/bitflyer/update_missing_deposits_job.rb
+++ app/jobs/sync/bitflyer/update_missing_deposits_job.rb
@@ -9,6 +9,8 @@ class Sync::Bitflyer::UpdateMissingDepositsJob < ApplicationJob
       .where(selected_wallet_provider_type: BitflyerConnection.name)
       .where.not(selected_wallet_provider_id: nil)
       .where(channels: {deposit_id: nil})
+      # For now I'm only allowing this to be sent once so we don't spam users, it's unclear to me how often this gets run
+      .where(:bitflyer_connection, {oauth_refresh_failed: false, oauth_failure_email_sent: false}) # There is no point in attempting this on failed connections
       .select("channels.id")
       .each do |result|
       channel_id = result["id"]
