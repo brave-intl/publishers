6ef3c98dd94a8d22b464e5b57c077df0b676e91d
Jon Staples <8813557+OAuthBringer@users.noreply.github.com>
2022-05-11 13:31:12 -0400

    refactor: Abstract deposit id service (#3664)

--- app/services/builder_base_service.rb
+++ app/services/builder_base_service.rb
@@ -13,12 +13,33 @@ class BuilderBaseService
   def call(args)
   end
 
+  sig { params(val: T.untyped).returns(BSuccess) }
   def pass(val = true)
     T.must(val)
     BSuccess.new(result: val)
   end
 
+  sig { params(e: T.any(String, T::Array[T.untyped])).returns(BFailure) }
   def problem(e)
-    BFailure.new(errors: [e])
+    case e
+    when String
+      BFailure.new(errors: [e])
+    when Array
+      BFailure.new(errors: e)
+    else
+      T.absurd(e)
+    end
+  end
+
+  sig { params(result: T.any(String, T::Array[T.untyped])).returns(BIndeterminate) }
+  def shrug(result)
+    case result
+    when String
+      BIndeterminate.new(result: [result])
+    when Array
+      BIndeterminate.new(result: result)
+    else
+      T.absurd(result)
+    end
   end
 end
