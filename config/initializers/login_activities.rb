59e1d204d7881cf148b7238ef6bbb0a0dab35300
Jon Staples <8813557+OAuthBringer@users.noreply.github.com>
2022-05-11 17:28:56 -0400

    fix: Remove this, redundant, shouldn't be here, causing a fail (#3666)

--- config/initializers/login_activities.rb
+++ config/initializers/login_activities.rb
@@ -10,6 +10,4 @@ Warden::Manager.after_authentication except: :fetch do |publisher, auth, _opts|
   auth.cookies[:_publisher_id] = {value: publisher.id, same_site: :lax, secure: true, httponly: true, expires: 20.year.from_now}
 
   LoginActivity.create!(params)
-
-  publisher.&selected_wallet_provider.sync_connection! if publisher && !Rails.env.test?
 end
