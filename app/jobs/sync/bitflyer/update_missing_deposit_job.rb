ed75a091828b996325fa327819bdb11a2918c1c8
Jon Staples <jstaples@brave.com>
2022-05-13 09:15:53 -0400

    task: Disable Retry
    In an effort to avoid potential blacklisting due to unknown failures I'm
    disabling retries on this for the time being.

--- app/jobs/sync/bitflyer/update_missing_deposit_job.rb
+++ app/jobs/sync/bitflyer/update_missing_deposit_job.rb
@@ -2,7 +2,7 @@
 
 class Sync::Bitflyer::UpdateMissingDepositJob
   include Sidekiq::Worker
-  sidekiq_options queue: :default, retry: true
+  sidekiq_options queue: :default, retry: false
 
   def perform(channel_id)
     Wallet::UpdateBitflyerDepositIdService.build.call(Channel.find_by_id!(channel_id))
