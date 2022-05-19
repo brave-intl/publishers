a72dffbac6637c14521b33ba7e195cd8fce63b1a
Jon Staples <8813557+OAuthBringer@users.noreply.github.com>
2022-05-19 12:41:25 -0400

    fix: Execute a functional job and not garbage (#3687)

--- app/jobs/sync/bitflyer/update_missing_deposits_job.rb
+++ app/jobs/sync/bitflyer/update_missing_deposits_job.rb
@@ -1,11 +1,16 @@
 # typed: ignore
-class Sync::Bitflyer::UpdateMissingDepositsJob < ApplicationJob
-  queue_as :low
+class Sync::Bitflyer::UpdateMissingDepositsJob
+  include Sidekiq::Worker
+  sidekiq_options queue: :default, retry: false
 
-  def perform(notify: false)
+  def perform(async: true)
     Channel.missing_deposit_id.using_active_bitflyer_connection.select(:id).find_in_batches do |batch|
       batch.each do |id|
-        Sync::Bitflyer::UpdateMissingDepositJob.perform_async(id, notify: notify)
+        if async
+          Sync::Bitflyer::UpdateMissingDepositJob.perform_async(id)
+        else
+          Sync::Bitflyer::UpdateMissingDepositJob.new.perform(id)
+        end
       end
     end
   end
