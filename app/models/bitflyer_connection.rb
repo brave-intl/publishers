187dcef11a234acaa8ea100d3b43d6c86a28293c
Jon Staples <jstaples@brave.com>
2022-05-20 13:35:10 -0400

    task: Guard against expiry

--- app/models/bitflyer_connection.rb
+++ app/models/bitflyer_connection.rb
@@ -57,6 +57,16 @@ class BitflyerConnection < Oauth2::AuthorizationCodeBase
     self
   end
 
+  # This is a temporary work around to gaurd against potential
+  # race conditions that have caused broken conditions in other connections such as Gemini
+  # Bitflyer is the first connection that is going to run a scheduled job running refresh
+  # so I am trying to be very cautious.  Updating this in the base class method
+  # broke too many specs to deal with right now so for the sake of forward progress
+  # this is my resolution.
+  def refresh_authorization!
+    access_token_expired? || !access_token ? super : self
+  end
+
   def sync_connection!
     refresh_authorization!
   end
