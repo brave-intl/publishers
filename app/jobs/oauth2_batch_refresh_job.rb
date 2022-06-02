4d1aed9feeb93b662fd40685a1286c75790788b6
Jon Staples <8813557+OAuthBringer@users.noreply.github.com>
2022-06-02 13:41:49 -0400

    improvement: Add clear scopes for refreshable gemini connections and enable limited job (#3712)

--- app/jobs/oauth2_batch_refresh_job.rb
+++ app/jobs/oauth2_batch_refresh_job.rb
@@ -1,18 +1,17 @@
 class Oauth2BatchRefreshJob < ApplicationJob
   queue_as :scheduler
 
-  def perform(wait: 0, limit: nil, notify: false)
+  # https://docs.gemini.com/rest-api/#rate-limits
+  # 0.1 requests/second
+  def perform(wait: 0.1, limit: 5000, notify: false)
     count = 0
-    [UpholdConnection, GeminiConnection, BitflyerConnection].each do |klass|
+    [GeminiConnection].each do |klass|
       base_query = limit.present? ? klass.limit(limit) : klass
 
-      # The idea here is I'm filling a queue to refresh tokens from each connection type
-      # while adding a delay so I do not overwhelm the API rate limit of a given provider
-      base_query.select(:id).find_in_batches do |batch|
+      base_query.refreshable.select(:id).find_in_batches do |batch|
         batch.each do |connection|
           count += 1
-          # I'm using this job to call individual jobs so I can take advantage of retries/failover due to thinks like rate limits
-          # I've used exactly this pattern before to handle mass email jobs, though I don't always wait between enqueing tasks.
+
           Oauth2RefreshJob.perform_later(connection.id, klass.name, notify: notify)
           sleep(wait) if wait
         end
