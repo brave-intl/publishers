abb628c0695860be7ab23d84134e1f866ce90f01
Jon Staples <jstaples@brave.com>
2022-06-07 12:12:25 -0400

    improvement: Add rate limit to deposits

--- app/jobs/sync/bitflyer/update_missing_deposits_job.rb
+++ app/jobs/sync/bitflyer/update_missing_deposits_job.rb
@@ -3,7 +3,9 @@ class Sync::Bitflyer::UpdateMissingDepositsJob
   include Sidekiq::Worker
   sidekiq_options queue: :default, retry: false
 
-  def perform(async: true)
+  # THere is definitely a rate limit here despite being told there wasn't.  I manually
+  # executed the service because the wrong id value was being passed to the job and it succeeded and eventually hit a 429.
+  def perform(async: true, wait: 0.1)
     Channel.missing_deposit_id.using_active_bitflyer_connection.select(:id).find_in_batches do |batch|
       batch.each do |channel|
         # FML
@@ -12,6 +14,8 @@ class Sync::Bitflyer::UpdateMissingDepositsJob
         else
           Sync::Bitflyer::UpdateMissingDepositJob.new.perform(channel.id)
         end
+
+        sleep(wait) if wait
       end
     end
   end
