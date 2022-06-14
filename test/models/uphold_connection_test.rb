0943fb25c0f3a7e781e55e9303bb5f9d357b03b0
Jon Staples <jstaples@brave.com>
2022-06-14 17:13:35 -0400

    fixup! fixup! fixup! task: Remove artifacts?

--- test/models/uphold_connection_test.rb
+++ test/models/uphold_connection_test.rb
@@ -55,7 +55,7 @@ class UpholdConnectionTest < ActiveSupport::TestCase
         let(:status) { "blocked" }
 
         before do
-          stub_get_user(id: "any unique value", status: status)
+          stub_get_user(id: "any unique value", user_status: status)
         end
 
         describe "if it is not ok" do
@@ -173,109 +173,6 @@ class UpholdConnectionTest < ActiveSupport::TestCase
     end
   end
 
-  describe "validations" do
-    let(:uphold_connection) { uphold_connections(:default_connection) }
-
-    describe "when uphold_access_parameters is nil" do
-      before do
-        uphold_connection.uphold_access_parameters = nil
-      end
-
-      describe "when uphold_code is present" do
-        before do
-          uphold_connection.uphold_code = "foo"
-        end
-
-        it "is valid" do
-          assert uphold_connection.valid?
-        end
-
-        describe "when uphold_verified is true" do
-          before do
-            uphold_connection.uphold_verified = true
-          end
-
-          it "is not valid" do
-            refute uphold_connection.valid?
-          end
-        end
-      end
-
-      describe "when uphold_code is missing" do
-        before do
-          uphold_connection.uphold_code = nil
-        end
-
-        it "is valid" do
-          assert uphold_connection.valid?
-        end
-
-        describe "when uphold_verified is true" do
-          before do
-            uphold_connection.uphold_verified = true
-          end
-
-          it "is valid" do
-            assert uphold_connection.valid?
-          end
-        end
-      end
-    end
-
-    describe "when uphold_access_parameters are present" do
-      before do
-        uphold_connection.uphold_access_parameters = "bar"
-      end
-
-      describe "when uphold_code is present" do
-        before do
-          uphold_connection.uphold_code = "foo"
-        end
-
-        it "is not valid" do
-          refute uphold_connection.valid?
-        end
-      end
-
-      describe "when uphold_code is missing" do
-        before do
-          uphold_connection.uphold_code = nil
-        end
-
-        it "is valid" do
-          assert uphold_connection.valid?
-        end
-      end
-    end
-  end
-
-  describe "receive_uphold_code" do
-    uphold_connection = nil
-    let(:subject) { uphold_connection.receive_uphold_code("secret!") }
-
-    before do
-      uphold_connection = uphold_connections(:verified_connection)
-      uphold_connection.uphold_state_token = "1234"
-      uphold_connection.uphold_code = nil
-      uphold_connection.uphold_access_parameters = "1234"
-      uphold_connection.uphold_verified = false
-
-      subject
-    end
-
-    it "it sets uphold_code" do
-      assert uphold_connection.uphold_code
-    end
-
-    it "clears the uphold_access_parameters field" do
-      assert_nil uphold_connection.uphold_access_parameters
-    end
-
-    it "clears the uphold_state_token field" do
-      assert_nil uphold_connection.uphold_state_token
-    end
-  end
-
   describe "verify_uphold_status" do
     uphold_connection = nil
 
@@ -285,7 +182,6 @@ class UpholdConnectionTest < ActiveSupport::TestCase
 
     describe "when uphold_code, access_parameters, and uphold_verified are nil" do
       before do
-        uphold_connection.uphold_code = nil
         uphold_connection.uphold_access_parameters = nil
         uphold_connection.uphold_verified = false
       end
@@ -306,42 +202,6 @@ class UpholdConnectionTest < ActiveSupport::TestCase
 
       it "should update" do
         assert verified_connection.oauth_refresh_failed
-      end
-    end
-
-    describe "when uphold_code is set but the other parameters are nil" do
-      before do
-        uphold_connection.uphold_code = "foo"
-        uphold_connection.uphold_access_parameters = nil
-        uphold_connection.uphold_verified = false
-      end
-
-      it "returns code_acquired" do
-        assert_equal :code_acquired, uphold_connection.uphold_status
-      end
-    end
-
-    describe "when uphold_code is nil but there are access parameters" do
-      before do
-        uphold_connection.uphold_code = nil
-        uphold_connection.uphold_access_parameters = "foo"
-        uphold_connection.uphold_verified = false
-      end
-
-      it "returns unconnected" do
-        assert_equal :unconnected, uphold_connection.uphold_status
-      end
-    end
-
-    describe "when uphold_code and access_parameters are nil" do
-      before do
-        uphold_connection.uphold_code = nil
-        uphold_connection.uphold_access_parameters = nil
-        uphold_connection.uphold_verified = true
-      end
-
-      it "returns reauthorize" do
-        assert_equal :reauthorization_needed, uphold_connection.uphold_status
       end
     end
 
