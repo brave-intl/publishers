84c6eb0fb364e9da93ebe365bc4f54d47ef644a1
Jon Staples <jstaples@brave.com>
2022-06-27 14:39:10 -0400

    fix: Fix 500 error for invalid csp reports

--- app/controllers/csp_violations_report_controller.rb
+++ app/controllers/csp_violations_report_controller.rb
@@ -16,10 +16,10 @@ class CspViolationsReportController < ApplicationController
     #   "status-code"=>0,
     #   "script-sample"=>""}
     # We only care about the document-uri, blocked-uri, and directives for now
-    report = JSON.parse(request.body.read)["csp-report"]
-    sliced_report = report.slice("document-uri", "violated-directive", "effective-directive", "blocked-uri")
     begin
-      CspViolationReport.create(report: sliced_report)
+      report = JSON.parse(request.body.read)["csp-report"]
+      sliced_report = report.slice("document-uri", "violated-directive", "effective-directive", "blocked-uri")
+      CspViolationReport.create!(report: sliced_report)
     rescue
       nil
     end
