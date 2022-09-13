8593e89e3df7e71e15b809211e29cb51b444778f
Jon Staples <jstaples@brave.com>
2022-09-13 12:36:01 -0400

    perf: Hopefully make this job not crash all the things

--- app/jobs/sync/promo_registration_stats_job.rb
+++ app/jobs/sync/promo_registration_stats_job.rb
@@ -2,6 +2,7 @@
 
 class Sync::PromoRegistrationStatsJob
   include Sidekiq::Worker
+  sidekiq_options queue: :low, retry: false
 
   def perform(promo_registration_ids)
     Promo::RegistrationsStatsFetcher.new(promo_registrations: PromoRegistration.where(id: promo_registration_ids), update_only: true).perform
