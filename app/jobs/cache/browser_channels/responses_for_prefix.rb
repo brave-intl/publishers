e8876d62648b1108b6702523ca5b984dc734fec6
Jennifer Byrne <jenniferbyrne@Jennifers-MacBook-Pro.local>
2022-09-14 14:33:25 -0500

    add in production flag

--- app/jobs/cache/browser_channels/responses_for_prefix.rb
+++ app/jobs/cache/browser_channels/responses_for_prefix.rb
@@ -24,14 +24,16 @@ class Cache::BrowserChannels::ResponsesForPrefix
     @site_banner_lookups = SiteBannerLookup.where("sha2_base16 LIKE ?", prefix + "%")
     channel_responses = PublishersPb::ChannelResponseList.new
 
-    parameters = Rewards::Parameters.new.get_parameters
+    unless Rails.env.production?
+      parameters = Rewards::Parameters.new.get_parameters
 
-    case parameters
-    when Rewards::Types::ParametersResponse
-      allowed_regions = parameters.custodianRegions
-    else
-      LogException.perform(parameters)
-      raise StandardError.new("Could not load allowed regions")
+      case parameters
+      when Rewards::Types::ParametersResponse
+        allowed_regions = parameters.custodianRegions
+      else
+        LogException.perform(parameters)
+        raise StandardError.new("Could not load allowed regions")
+      end
     end
 
     @site_banner_lookups.includes(publisher: [:uphold_connection, :bitflyer_connection, :gemini_connection]).each do |site_banner_lookup|
@@ -45,7 +47,11 @@ class Cache::BrowserChannels::ResponsesForPrefix
           connection = site_banner_lookup.publisher.uphold_connection
           uphold_wallet.wallet_state = get_uphold_wallet_state(uphold_connection: connection)
 
-          if connection.country && allowed_regions[:uphold][:allow].include?(connection.country.upcase)
+          unless Rails.env.production?
+            if connection.country && allowed_regions[:uphold][:allow].include?(connection.country.upcase)
+              uphold_wallet.address = site_banner_lookup.channel.uphold_connection&.address || ""
+            end
+          else
             uphold_wallet.address = site_banner_lookup.channel.uphold_connection&.address || ""
           end
 
@@ -58,8 +64,12 @@ class Cache::BrowserChannels::ResponsesForPrefix
           connection = site_banner_lookup.publisher.bitflyer_connection
           bitflyer_wallet.wallet_state = get_bitflyer_wallet_state(bitflyer_connection: connection)
 
-          if connection.country && allowed_regions[:bitflyer][:allow].include?(connection.country.upcase)
-            bitflyer_wallet.address = site_banner_lookup.channel.deposit_id
+          unless Rails.env.production?
+            if connection.country && allowed_regions[:bitflyer][:allow].include?(connection.country.upcase)
+              bitflyer_wallet.address = site_banner_lookup.channel.deposit_id
+            end
+          else
+            gemini_wallet.address = site_banner_lookup.channel.gemini_connection&.recipient_id || connection.recipient_id
           end
 
           wallet.bitflyer_wallet = bitflyer_wallet
@@ -71,7 +81,11 @@ class Cache::BrowserChannels::ResponsesForPrefix
           connection = site_banner_lookup.publisher.gemini_connection
           gemini_wallet.wallet_state = get_gemini_wallet_state(gemini_connection: connection)
 
-          if connection.country && allowed_regions[:gemini][:allow].include?(connection.country.upcase)
+          unless Rails.env.production?
+            if connection.country && allowed_regions[:gemini][:allow].include?(connection.country.upcase)
+              gemini_wallet.address = site_banner_lookup.channel.gemini_connection&.recipient_id || connection.recipient_id
+            end
+          else
             gemini_wallet.address = site_banner_lookup.channel.gemini_connection&.recipient_id || connection.recipient_id
           end
 
