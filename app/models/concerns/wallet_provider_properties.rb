576f3ae6a68bb1cc85a2faa5c7c15462275effad
Jennifer Byrne <jbyrne@brave.com>
2022-09-29 21:14:02 -0500

    standardrb fix

--- app/models/concerns/wallet_provider_properties.rb
+++ app/models/concerns/wallet_provider_properties.rb
@@ -25,9 +25,9 @@ module WalletProviderProperties
       raise StandardError.new("Could not load allowed regions")
     end
     unless allowed_regions[provider_sym][:allow].include?(country_code.upcase)
-      raise BlockedCountryError.new("Your #{provider_sym.to_s.capitalize} account can't be connected to your Brave Creators"\
-        " profile at this time. Your #{provider_sym.to_s.capitalize} account is registered in a country that's not currently "\
-        "supported for connecting to Brave Creators.</br>See the <a target='_blank' href='https://support.brave.com/hc/en-us/articles/6539887971469'>"\
+      raise BlockedCountryError.new("Your #{provider_sym.to_s.capitalize} account can't be connected to your Brave Creators" \
+        " profile at this time. Your #{provider_sym.to_s.capitalize} account is registered in a country that's not currently " \
+        "supported for connecting to Brave Creators.</br>See the <a target='_blank' href='https://support.brave.com/hc/en-us/articles/6539887971469'>" \
         "current list of supported regions and learn more</a> about connecting a custodial account to Brave Rewards.")
     end
   end
