1e600db81384dc2e6e2ac547e09e65bd26e1682a
Jennifer Byrne <jbyrne@brave.com>
2022-10-25 10:27:57 -0500

    move allowed countries endpoint out of public api

---
+++ app/controllers/api/v3/channels_controller.rb
@@ -0,0 +1,28 @@
+# typed: ignore
+
+class Api::V3::ChannelsController < Api::BaseController
+  # Takes an array of channel identifiers and retuns a dictionary of channel identifiers as keys and true/false as values
+  def allowed_countries
+    channels = params[:channel_ids].map do |id|
+      {channel: Channel.find_by_channel_identifier(id), channel_identifier: id}
+    end
+
+    allowed_regions = Rewards::Parameters.new.fetch_allowed_regions
+
+    response = {}
+    channels.each do |channel_obj|
+      publisher = channel_obj[:channel].publisher
+
+      response[channel_obj[:channel_identifier]] = if publisher.uphold_connection.present?
+        allowed_regions[:uphold][:allow].include?(publisher.uphold_connection.country)
+      elsif publisher.gemini_connection.present?
+        allowed_regions[:gemini][:allow].include?(publisher.gemini_connection.country)
+      else
+        publisher.bitflyer_connection.present?
+      end
+    end
+
+    # is there any metadata we need here?
+    render(json: response.to_json, status: 200)
+  end
+end
