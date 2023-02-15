a79f79701e8be9a7040f533aec4f838e48030ac5
Tyler Smart <tsmart@brave.com>
2023-02-15 10:40:55 -0800

    Update defaults to 7

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module Publishers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     config.middleware.use HttpHeaderMiddleware
     config.middleware.use Rack::Deflater
