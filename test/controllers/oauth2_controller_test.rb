60bd23c13b140d43f838ba7cb1d683468fc34a73
Tyler Smart <tsmart@brave.com>
2023-02-22 10:34:55 -0800

    Add test for uphold_connection#create

--- test/controllers/oauth2_controller_test.rb
+++ test/controllers/oauth2_controller_test.rb
@@ -12,13 +12,20 @@ class Oauth2ControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "code redirects" do
-
     publisher = publishers(:completed)
     sign_in publisher
-
     Oauth2Controller.any_instance.stubs(:state_verified?).returns(true)
-
     get(publishers_uphold_verified_path(code: '123'))
     assert_response :redirect
   end
+
+  test "uphold connection create" do
+    publisher = publishers(:completed)
+    sign_in publisher
+    Oauth2Controller.any_instance.stubs(:state_verified?).returns(true)
+    post(connection_uphold_connection_path(code: '123'))
+    assert_response :redirect
+  end
+
+
 end
