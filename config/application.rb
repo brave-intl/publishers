b6b230a1aa3dec6cfe8c264aa4fe124cffac4ea3
Tyler Smart <tsmart@brave.com>
2023-03-16 11:03:26 -0700

    Pull rails 7 column enc keys from env

--- config/application.rb
+++ config/application.rb
@@ -63,5 +63,9 @@ module Publishers
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.encryption.primary_key = ENV['ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY']
+    config.active_record.encryption.deterministic_key = ENV['ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY']
+    config.active_record.encryption.key_derivation_salt = ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT']
   end
 end
