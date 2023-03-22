db34ca2fa3979da263731b337b1770894bb376a1
Tyler Smart <tsmart@brave.com>
2023-03-22 10:20:27 -0700

    dont load DB tables before they exist in CI

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -7,18 +7,20 @@ class ApplicationRecord < ActiveRecord::Base
 
   # From https://pagertree.com/blog/migrate-attr_encrypted-to-rails-7-active-record-encrypts
   def self.encrypt_column_transition(name)
-    attr_encrypted_options[:key] = proc { |record| record.class.encryption_key }
+    if !ENV["PREPARING_DATABASE"].present?
+      attr_encrypted_options[:key] = proc { |record| record.class.encryption_key }
 
-    if column_names.include? "encrypted_#{name}"
-      attr_encrypted name.to_sym
-    end
+      if column_names.include? "encrypted_#{name}"
+        attr_encrypted name.to_sym
+      end
 
-    if column_names.include? "encrypted_#{name}_2"
-      attr_encrypted "#{name}_2".to_sym
-    end
+      if column_names.include? "encrypted_#{name}_2"
+        attr_encrypted "#{name}_2".to_sym
+      end
 
-    if column_names.include? name
-      encrypts name.to_sym
+      if column_names.include? name
+        encrypts name.to_sym
+      end
     end
   end
 end
