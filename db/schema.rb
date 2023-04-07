25000eb85f97916a8c32f843e41b475f48a78cad
tsmartt <81187695+tsmartt@users.noreply.github.com>
2023-04-07 16:26:11 -0700

    Add index to uphold_id (#4042)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_184226) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_07_231600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "pg_stat_statements"
@@ -755,6 +755,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_184226) do
     t.text "uphold_access_parameters"
     t.index ["card_id"], name: "index_uphold_connections_on_card_id"
     t.index ["publisher_id"], name: "index_uphold_connections_on_publisher_id", unique: true
+    t.index ["uphold_id"], name: "index_uphold_connections_on_uphold_id"
   end
 
   create_table "uphold_status_reports", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
