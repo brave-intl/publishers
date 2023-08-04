d1da8b9c718ad266abcd41370808b343aeed9c56
Tyler Smart <tsmart@brave.com>
2023-08-04 13:15:12 -0700

    Most tests passing

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,8 +3,8 @@
 class ApplicationJob < ActiveJob::Base
   # Send handled exceptions to Sentry and New Relic (which normally only sends unhandled exceptions).
   require "error_handler_delegator"
-  def self.new(*args, &block)
-    ErrorHandlerDelegator.new(super)
+  def self.new(...)
+    ErrorHandlerDelegator.new(super(...))
   end
 
   rescue_from(ActiveRecord::RecordNotFound) do |exception|
