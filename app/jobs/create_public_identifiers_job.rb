507b538c588eea55934d948b206394454aa2950f
Jennifer Byrne <jbyrne@brave.com>
2023-09-20 14:49:12 -0500

    update task to run no more than 2 jobs at once

--- app/jobs/create_public_identifiers_job.rb
+++ app/jobs/create_public_identifiers_job.rb
@@ -1,5 +1,8 @@
 class CreatePublicIdentifiersJob < ApplicationJob
   queue_as :low
+  include Sidekiq::Throttled::Job
+
+  sidekiq_throttle(concurrency: {limit: 2})
 
   def perform(channel_ids)
     channel_ids.each do |id|
