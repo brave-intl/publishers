6a3a8a2eaf7cb749d24e1412a01f854cc59f9fde
Tyler Smart <tsmart@brave.com>
2023-09-28 08:53:24 -0700

    Remove unneeded code

--- nextjs/scripts/create-local-server.js
+++ nextjs/scripts/create-local-server.js
@@ -76,45 +76,13 @@ app
     return server.listen(PORT, (err) => {
       if (err) throw err;
 
-      console.log('> Ready on https://localhost:5000');
+    console.log(
+      chalk.green(
+        `> Server started on ${chalk.bold.green(`https://localhost:${PORT}`)}`,
+      ),
+    );
     });
   })
   .catch((err) => {
     console.log('Error:::::', err);
   });
-
-
-
-
-// const { createServer } = require('https');
-// const httpProxy = require('http-proxy');
-// const chalk = require('chalk');
-// const { parse } = require('url');
-// const next = require('next');
-// const path = require('path');
-// const fs = require('fs');
-//
-// const PORT = 5001;
-// const dev = process.env.NODE_ENV !== 'production';
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
-//
-// const httpsOptions = {
-//   key: fs.readFileSync(path.join(__dirname, '..', '..', 'ssl', 'server.key')),
-//   cert: fs.readFileSync(path.join(__dirname, '..', '..', 'ssl', 'server.crt')),
-// };
-//
-// app.prepare().then(() => {
-//   createServer(httpsOptions, (req, res) => {
-//     const parsedUrl = parse(req.url, true);
-//
-//     handle(req, res, parsedUrl);
-//   }).listen(PORT, (err) => {
-//     if (err) throw err;
-//     console.log(
-//       chalk.green(
-//         `> Server started on ${chalk.bold.green(`https://localhost:${PORT}`)}`,
-//       ),
-//     );
-//   });
-// });
