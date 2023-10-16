0911284f1626c54c8ae20eb0c0695075730403b5
Jennifer Byrne <jbyrne@brave.com>
2023-10-16 17:25:13 -0500

    add solana bat transfers, move addresses to env variables

--- app/controllers/public_channel_controller.rb
+++ app/controllers/public_channel_controller.rb
@@ -6,6 +6,13 @@ class PublicChannelController < ApplicationController
     @site_banner = channel.site_banner&.read_only_react_property || SiteBanner.new_helper(current_publisher.id, channel.id)
     @crypto_addresses = channel.crypto_addresses.pluck(:address, :chain)
 
+    @crypto_constants = {
+      solana_test_url: ENV["SOLANA_TEST_URL"],
+      solana_main_url: ENV["SOLANA_MAIN_URL"],
+      solana_bat_address: ENV["SOLANA_BAT_ADDRESS"],
+      eth_bat_address: ENV["ETH_GOERLI_BAT_ADDRESS"]
+    }
+
     # Handle the case when the resource is not found
     if channel.nil? || @crypto_addresses.empty? || current_publisher != channel.publisher || !current_publisher.feature_flags["p2p_enabled"]
       redirect_to root_path, alert: "Channel not found"
