3ef3ce451470a9685edd7088de8a5f34cc37b1a5
Jennifer Byrne <jbyrne@brave.com>
2023-11-21 10:49:04 -0600

    add privacy modal when address is in use

--- app/javascript/packs/CryptoWalletServices.jsx
+++ app/javascript/packs/CryptoWalletServices.jsx
@@ -21,9 +21,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const channels = document.querySelectorAll(".crypto-wallet-services")
 
+  const createStore = (reducer, initialState) => {
+    const store = {};
+
+    store.state = initialState;
+    store.listeners = [];
+    store.subscribe = listener => store.listeners.push(listener);
+    store.dispatch = async action => {
+      store.state = await reducer(store.state, action);
+      store.listeners.forEach(listener => listener(action));
+    };
+    store.getState = () => store.state;
+
+    return store;
+  };
+
+  const initialState = {
+    addressesInUse: []
+  };
+
+  const reducer = async (state = initialState, action) => {
+    if ( action.type === "ADD_ADDRESS" ) {
+      state.addressesInUse.push(action.payload.newAddress);
+      return state;
+    } else if (action.type === "REMOVE_ADDRESS") {
+      const idx = state.addressesInUse.findIndex(address => address.id === action.payload.addressId);
+      return idx > -1 ? { addressesInUse: state.addressesInUse.splice(idx, 1) } : state;
+    }
+  }
+
+  const store = createStore(reducer, initialState);
+
   channels.forEach((channel) => {
-    const channelId = channel.dataset.channelId;
     const props = JSON.parse(channel.dataset.props);
+    props.store = store;
     
     ReactDOM.render(
       <IntlProvider locale={locale} messages={flattenMessages(localePackage)}>
