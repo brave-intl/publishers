b5d60bb68c774fc2036a3d5fb748289c8f4e3659
Jennifer Byrne <jbyrne@brave.com>
2024-02-28 12:41:51 -0600

    fix 500 error where channel is not found

--- app/controllers/public_channel_controller.rb
+++ app/controllers/public_channel_controller.rb
@@ -4,12 +4,13 @@ class PublicChannelController < ApplicationController
   def show
     channel = Channel.includes(:site_banner).find_by(public_identifier: params[:public_identifier])
     # channel_title is used in the meta tags
-    @channel_title = channel.publication_title
+    @channel_title = channel&.publication_title
     @crypto_addresses = channel&.crypto_addresses&.pluck(:address, :chain)
 
     # Handle the case when the resource is not found
     if channel.nil? || @crypto_addresses.empty?
       redirect_to root_path, alert: "Channel not found"
+      return
     end
     @url = channel.details&.url
     @site_banner = channel.site_banner&.read_only_react_property || SiteBanner.new_helper(current_publisher.id, channel.id)
