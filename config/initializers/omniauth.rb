0feb8540cccce05f7494ea479de9d05c6dc532ca
tsmartt <tsmart@brave.com>
2024-04-11 06:39:27 -0700

    Only set the FULL_HOST in non-test envs (#4412)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
-OmniAuth.config.full_host = lambda do |env|
-  Rails.configuration.pub_secrets[:creators_full_host]
+if !Rails.env.test?
+  OmniAuth.config.full_host = lambda do |env|
+    Rails.configuration.pub_secrets[:creators_full_host]
+  end
 end
