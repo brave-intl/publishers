e288562a6528fddd2edcd09fa4847ca5fa6f0a93
Tyler Smart <tsmart@brave.com>
2024-08-06 19:52:12 -0400

    Bundle exec

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -6,7 +6,7 @@ echo "Running the Next.js server in test mode"
 (cd nextjs && NODE_TLS_REJECT_UNAUTHORIZED=0 TEST_MODE=true NODE_ENV=development npm run dev 2>&1 > ../log/nextjstest.log) &
 
 echo "Running only the tests in the system/nextjs folder"
-rails test test/system/nextjs
+bundle exec rails test test/system/nextjs
 
 echo "Running all tests except those in the system/nextjs folder"
 bundle exec rails test --exclude /test/system/nextjs/  # the / / around the folder path are for regex
