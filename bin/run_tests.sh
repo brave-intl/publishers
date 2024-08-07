6d900f426a96cc8234af193ce1a4158ecb2e6ef1
Tyler Smart <tsmart@brave.com>
2024-08-06 20:03:47 -0400

    Add necessary env vars for CI

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo "Running the Next.js server in test mode"
-(cd nextjs && NODE_TLS_REJECT_UNAUTHORIZED=0 TEST_MODE=true NODE_ENV=development npm run dev 2>&1 > ../log/nextjstest.log) &
+(cd nextjs && NODE_TLS_REJECT_UNAUTHORIZED=0 TEST_MODE=true TEST_MODE_PUBLISHERS_HOST=localhost:4000 TEST_MODE_NEXT_HOST=localhost:5001 NODE_ENV=development npm run dev 2>&1 > ../log/nextjstest.log) &
 
 echo "Running only the tests in the system/nextjs folder"
-bundle exec rails test test/system/nextjs
+NEXT_HOST=localhost:5001 bundle exec rails test test/system/nextjs
 
 echo "Running all tests except those in the system/nextjs folder"
 bundle exec rails test --exclude /test/system/nextjs/  # the / / around the folder path are for regex
