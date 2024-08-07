54ddc0bf3f94da3e1bb7ec2bea1160324545a01f
Tyler Smart <tsmart@brave.com>
2024-08-06 20:19:19 -0400

    Remove eating output

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Running the Next.js server in test mode"
-(cd nextjs && NODE_TLS_REJECT_UNAUTHORIZED=0 TEST_MODE=true TEST_MODE_PUBLISHERS_HOST=localhost:4000 TEST_MODE_NEXT_HOST=localhost:5001 NODE_ENV=development npm run dev 2>&1 > ../log/nextjstest.log) &
+(cd nextjs && NODE_TLS_REJECT_UNAUTHORIZED=0 TEST_MODE=true TEST_MODE_PUBLISHERS_HOST=localhost:4000 TEST_MODE_NEXT_HOST=localhost:5001 NODE_ENV=development npm run dev) &
 
 echo "Running only the tests in the system/nextjs folder"
 NEXT_HOST=localhost:5001 bundle exec rails test test/system/nextjs
