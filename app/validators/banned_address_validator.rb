ff1d321027f22060b7e0bf40beff8aabacc84422
jlbyrne <jbyrne@brave.com>
2025-04-22 15:57:08 -0500

    ran standardb fix

--- app/validators/banned_address_validator.rb
+++ app/validators/banned_address_validator.rb
@@ -6,7 +6,7 @@ class BannedAddressValidator < ActiveModel::EachValidator
     banned_addresses.each do |banned_address|
       stripped_ba = banned_address.to_s.strip.downcase
       stripped_val = value.to_s.strip.downcase
-      if (stripped_ba.present? && stripped_val.present?) && (stripped_val.include?(stripped_ba) || stripped_ba.include?(stripped_val))
+      if stripped_ba.present? && stripped_val.present? && (stripped_val.include?(stripped_ba) || stripped_ba.include?(stripped_val))
         record.errors.add attribute.to_sym, MSG
       end
     end
