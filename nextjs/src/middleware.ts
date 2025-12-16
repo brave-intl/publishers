a5970c70a6deb15d5a43c1a484a662dd6a1aa92f
Jenny Byrne <jbyrne@brave.com>
2025-12-16 14:03:33 -0600

    add logging everywhere

--- nextjs/src/middleware.ts
+++ nextjs/src/middleware.ts
@@ -6,28 +6,44 @@ function secureUrl(url: string): string {
 }
 
 export function middleware(request) {
+  console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+  console.log("request: ", request)
   const pathname = request.nextUrl.pathname;
   const locale = request.nextUrl.searchParams.get('locale');
-
+  console.log('***************************************************************')
+  console.log('pathname: ', pathname)
+  console.log('locale: ', locale)
   if (locale === 'ja') {
+    console.log('japanese locale param detected')
     return NextResponse.redirect(new URL(`/ja/${pathname}`, request.url));
   }
 
   // next-intl middleware
   const defaultLocale = request.headers.get('x-default-locale') || 'en';
-
+  console.log("defaultLocale: ", defaultLocale)
+  console.log("pathname match: ", pathname === '/')
   if (pathname === '/' ) {
+    console.log("root path detected in middleware")
     const rootLocale = ['en', 'ja'].includes(defaultLocale) ? defaultLocale : null;
+    console.log("root locale: ", rootLocale)
+    console.log('redirect url: ', new URL(`/${rootLocale}`, request.url))
     return NextResponse.redirect(new URL(`/${rootLocale}`, request.url));
   }
+
+  console.log("testing statements next intl middleware will run:")
+  const unsafeExternalPathname = decodeURI(request.nextUrl.pathname);
+  console.log(unsafeExternalPathname)
+  console.log("base path: ", request.nextUrl.basePath)
 
   const handleI18nRouting = createMiddleware({
     locales: ['en', 'ja'],
     defaultLocale: 'en',
   });
 
+  console.log("handleI18nRouting: ", handleI18nRouting)
   const response = handleI18nRouting(request);
   response.headers.set('x-default-locale', defaultLocale);
+  console.log("response: ", response)
 
   // NextJS will pre-compile middleware routes in HTTP. To use SSL, we need to set all rewrites to
   // the appropriate url scheme
@@ -48,5 +64,5 @@ export function middleware(request) {
 export const config = {
   // Skip all paths that should not be internationalized. This example skips the
   // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|.*\\..*).*)'],
+  matcher: ['/((?!api|_next|.*\\..*).*)', '/'],
 };
